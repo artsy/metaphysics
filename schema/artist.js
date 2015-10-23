52f16dd04b419ea250b4ff05b8258010347d5501
dzucconi <mail@damonzucconi.com>
2015-10-23 13:40:31 -0400

    Clean up and add artwork sorts

--- schema/artist.js
+++ schema/artist.js
@@ -6,26 +6,54 @@ import {
   GraphQLString,
   GraphQLNonNull,
   GraphQLList,
-  GraphQLInt
+  GraphQLInt,
+  GraphQLEnumType
 } from 'graphql';
 
 let ArtistType = new GraphQLObjectType({
   name: 'Artist',
   fields: () => ({
-    id: { type: GraphQLString },
+    id: {
+      type: GraphQLString
+    },
     sortable_id: {
       type: GraphQLString,
       description: 'Use this attribute to sort by when sorting a collection of Artists'
     },
-    name: { type: GraphQLString },
-    birthday: { type: GraphQLString },
+    name: {
+      type: GraphQLString
+    },
+    birthday: {
+      type: GraphQLString
+    },
     artworks: {
       type: new GraphQLList(Artwork.type),
-      args: { size: { type: GraphQLInt } },
-      resolve: ({ id }, { size }) => {
+      args: {
+        size: {
+          type: GraphQLInt,
+          description: 'The number of Artworks to return'
+        },
+        sort: {
+          type: new GraphQLEnumType({
+            name: 'ArtworkSorts',
+            values: {
+              'title_asc': { value: 'title' },
+              'title_desc': { value: '-title' },
+              'created_at_asc': { value: 'created_at' },
+              'created_at_desc': { value: '-created_at' },
+              'iconicity_desc': { value: '-iconicity' },
+              'merchandisability_desc': { value: '-merchandisability' },
+              'published_at_asc': { value: 'published_at' },
+              'published_at_desc': { value: '-published_at' }
+            }
+          })
+        }
+      },
+      resolve: ({ id }, { size, sort }) => {
         return artsy(['artist', id, 'artworks'], {
           published: true,
-          size: size
+          size: size,
+          sort: sort
         });
       }
     }
