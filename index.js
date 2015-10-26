beacbb5a847396d17466540cf1f2f9b0d35a5ff0
dzucconi <mail@damonzucconi.com>
2015-10-26 17:11:16 -0400

    Update env variable names to be a little more explicit

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ require('./server.js');
 
 var xapp = require('artsy-xapp');
 xapp.on('error', process.exit);
-xapp.init({}, function() {
+xapp.init({
+  url: process.env.ARTSY_API_URL
+}, function() {
   require('./config').ARTSY_XAPP_TOKEN = xapp.token;
 });
