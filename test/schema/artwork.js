7d158e62cd87bbbce0021d812c8126a914c9e24a
Charles Broskoski <broskoski@gmail.com>
2015-11-03 17:34:36 -0500

    add spec for artwork.is_in_auction

--- test/schema/artwork.js
+++ test/schema/artwork.js
@@ -7,7 +7,11 @@ let artwork = {
   title: "Untitled (Portrait)",
   forsale: true,
   acquireable: false,
-  partner: [{id: 'galerie cabbie'}]
+  partner: [{id: 'galerie cabbie'}],
+  sales: [
+    {title: 'sale #1', is_auction: true},
+    {title: 'sale #2', is_auction: false}
+  ]
 }
 
 describe('Artwork', () => {
@@ -19,6 +23,14 @@ describe('Artwork', () => {
     it('sets an artwork as not contactable if it has related sales', () => {
       ArtworkPredicates.is_contactable(artwork, [{id: 'another auction by cab'}])
         .should.be.false();
+    });
+  });
+  describe('ArtworkPredicates.is_in_auction', () => {
+    it('if any artwork sale is an auction, then is_in_auction is true', () => {
+      ArtworkPredicates.is_in_auction(artwork, artwork.sales).should.be.true();
+    });
+    it('if no artwork sales are in an auction, then is_in_auction is false', () => {
+      ArtworkPredicates.is_in_auction(artwork, []).should.be.false();
     });
   });
 });
