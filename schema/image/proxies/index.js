c3b3b55bcd208f66a2a6e85f1b97d822d815e803
dzucconi <mail@damonzucconi.com>
2015-11-09 14:34:03 -0500

    Configure using an ENV variable

--- schema/image/proxies/index.js
+++ schema/image/proxies/index.js
@@ -1,3 +1,9 @@
+const { RESIZING_SERVICE } = process.env;
+
 module.exports = function() {
-  return require('./embedly').apply(null, arguments);
+  if (RESIZING_SERVICE === 'gemini') {
+    return require('./gemini').apply(null, arguments);
+  } else {
+    return require('./embedly').apply(null, arguments);
+  }
 };
