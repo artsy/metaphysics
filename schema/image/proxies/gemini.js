8274e31a6e8e6b150037fe56831e2579fddd57c3
dzucconi <mail@damonzucconi.com>
2015-11-09 13:57:00 -0500

    Easier to switch out underlying image resizing service; adds Embedly

--- schema/image/proxies/gemini.js
+++ schema/image/proxies/gemini.js
@@ -2,10 +2,14 @@ import qs from 'qs';
 
 const { GEMINI_ENDPOINT } = process.env;
 
-export default (src, mode, width, height) => `${GEMINI_ENDPOINT}/?${qs.stringify({
-  resize_to: mode,
-  height: height,
-  width: width,
-  quality: 95,
-  src: src
-})}`;
+export default (src, mode, width, height) => {
+  if (mode === 'resize') mode = 'fit';
+
+  return `${GEMINI_ENDPOINT}/?${qs.stringify({
+    resize_to: mode,
+    height: height,
+    width: width,
+    quality: 95,
+    src: src
+  })}`;
+};
---
+++ schema/image/proxies/index.js
@@ -0,0 +1,3 @@
+module.exports = function() {
+  return require('./embedly').apply(null, arguments);
+};
