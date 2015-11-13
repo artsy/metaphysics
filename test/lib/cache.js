8b7e3d9a9b567aabba400eb89370aa7231ef0dc5
dzucconi <mail@damonzucconi.com>
2015-11-13 11:53:59 -0500

    Pass through when Redis dies

--- test/lib/cache.js
+++ test/lib/cache.js
@@ -2,64 +2,82 @@ import fakeredis from 'fakeredis';
 import cache from '../../lib/cache';
 
 describe('Cache', () => {
-  let client = fakeredis.createClient();
-
-  before(() => {
-    cache.__Rewire__('client', client);
-  });
-
-  after(() => {
-    cache.__ResetDependency__('client');
-  });
-
-  afterEach((done) => {
-    client.flushdb(err => done());
-  });
-
-  describe('#get', () => {
-    beforeEach(() => cache.set('get_foo', { bar: 'baz'}));
-
-    it('parses the data and resolves the promise', (done) => {
-      let promise = cache.get('get_foo');
-
-      promise.should.be.instanceOf(Promise);
-
-      promise
-        .then(data => {
-          data.bar.should.equal('baz');
-          done();
-        });
+  describe('when connection to Redis fails', () => {
+    before(() => {
+      cache.__Rewire__('client', null);
     });
-  });
 
-  describe('#set', () => {
-    describe('with a plain Object', () => {
-      it('sets the cache and includes a timestamp', (done) => {
-        cache.set('set_foo', { bar: 'baz' });
+    after(() => {
+      cache.__ResetDependency__('client');
+    });
 
-        client.get('set_foo', (err, data) => {
-          let parsed = JSON.parse(data);
-
-          parsed.bar.should.equal('baz');
-          parsed.cached.should.be.instanceOf(Number)
-
-          done();
-        });
+    describe('#get', () => {
+      it('falls through with a rejection', () => {
+        return cache.get('foobar').should.be.rejected();
       });
     });
 
-    describe('with an Array', () => {
-      it('sets the cache and includes a timestamp', (done) => {
-        cache.set('set_bar', [{ baz: 'qux' }]);
+    describe('#set', () => {
+      it('returns false', () => {
+        cache.set('foo', {}).should.be.false();
+      })
+    });
+  });
 
-        client.get('set_bar', (err, data) => {
-          let parsed = JSON.parse(data);
+  describe('when successfully connected to Redis', () => {
+    let client = fakeredis.createClient();
 
-          parsed.should.have.lengthOf(1);
-          parsed[0].baz.should.equal('qux');
-          parsed[0].cached.should.be.instanceOf(Number)
+    before(() => {
+      cache.__Rewire__('client', client);
+    });
 
-          done();
+    after(() => {
+      cache.__ResetDependency__('client');
+    });
+
+    afterEach((done) => {
+      client.flushdb(err => done());
+    });
+
+    describe('#get', () => {
+      beforeEach(() => cache.set('get_foo', { bar: 'baz'}));
+
+      it('parses the data and resolves the promise', () => {
+        return cache.get('get_foo').then(data => {
+          data.bar.should.equal('baz')
+        }).should.be.fulfilled();
+      });
+    });
+
+    describe('#set', () => {
+      describe('with a plain Object', () => {
+        it('sets the cache and includes a timestamp', (done) => {
+          cache.set('set_foo', { bar: 'baz' });
+
+          client.get('set_foo', (err, data) => {
+            let parsed = JSON.parse(data);
+
+            parsed.bar.should.equal('baz');
+            parsed.cached.should.be.instanceOf(Number)
+
+            done();
+          });
+        });
+      });
+
+      describe('with an Array', () => {
+        it('sets the cache and includes a timestamp', (done) => {
+          cache.set('set_bar', [{ baz: 'qux' }]);
+
+          client.get('set_bar', (err, data) => {
+            let parsed = JSON.parse(data);
+
+            parsed.should.have.lengthOf(1);
+            parsed[0].baz.should.equal('qux');
+            parsed[0].cached.should.be.instanceOf(Number)
+
+            done();
+          });
         });
       });
     });
