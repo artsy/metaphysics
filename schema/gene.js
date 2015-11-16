5167a59cebbba817e406ef6b6ed60282bec46f6c
dzucconi <mail@damonzucconi.com>
2015-11-16 13:57:22 -0500

    Add Gene to root; add Gene artist relations

--- schema/gene.js
+++ schema/gene.js
@@ -1,9 +1,14 @@
+import _ from 'lodash';
 import gravity from '../lib/loaders/gravity';
 import cached from './fields/cached';
+import Artist from './artist';
 import Image from './image';
 import {
   GraphQLObjectType,
-  GraphQLString
+  GraphQLString,
+  GraphQLNonNull,
+  GraphQLList,
+  GraphQLInt
 } from 'graphql';
 
 let GeneType = new GraphQLObjectType({
@@ -13,19 +18,56 @@ let GeneType = new GraphQLObjectType({
     id: {
       type: GraphQLString
     },
+    href: {
+      type: GraphQLString,
+      resolve: ({ id }) => `gene/${id}`
+    },
     name: {
       type: GraphQLString
     },
     image: {
       type: Image.type,
       resolve: (gene) => gene
+    },
+    artists: {
+      type: new GraphQLList(Artist.type),
+      resolve: ({ id }) => {
+        return gravity(`gene/${id}/artists`, {
+          exclude_artists_without_artworks: true
+        });
+      }
+    },
+    trending_artists: {
+      type: new GraphQLList(Artist.type),
+      args: {
+        sample: {
+          type: GraphQLInt
+        }
+      },
+      resolve: ({ id }, options) => {
+        return gravity(`artists/trending`, {
+          gene: id
+        }).then(artists => {
+          if (_.has(options, 'sample')) {
+            return _.take(_.shuffle(artists), options.sample);
+          } else {
+            return artists;
+          }
+        });
+      }
     }
   }
 });
 
 let Gene = {
   type: GeneType,
-  resolve: ({ id }) => gravity(`gene/${id}`)
+  args: {
+    id: {
+      description: 'The slug or ID of the Gene',
+      type: new GraphQLNonNull(GraphQLString)
+    }
+  },
+  resolve: (root, { id }) => gravity(`gene/${id}`)
 };
 
 export default Gene;
