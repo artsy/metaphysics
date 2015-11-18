dc3fd619072682ceabe64f5779389db4715e167e
Charles Broskoski <broskoski@gmail.com>
2015-11-18 18:08:17 -0500

    updates position type

--- schema/sale_artwork.js
+++ schema/sale_artwork.js
@@ -24,7 +24,7 @@ let SaleArtworkType = new GraphQLObjectType({
         type: GraphQLInt
       },
       position: {
-        type: GraphQLString
+        type: GraphQLInt
       },
       lot_number: {
         type: GraphQLString
