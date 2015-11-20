43cf320010b0fa06729d1bb70d0062e1df2660a3
dzucconi <mail@damonzucconi.com>
2015-11-20 15:20:46 -0500

    Implements moment-date field

---
+++ schema/fields/date.js
@@ -0,0 +1,18 @@
+import moment from 'moment';
+import { GraphQLString } from 'graphql';
+
+export default {
+  type: GraphQLString,
+  args: {
+    format: {
+      type: GraphQLString
+    }
+  },
+  resolve: (show, { format }, { fieldName }) => {
+    if (!format) {
+      return show[fieldName];
+    } else {
+      return moment(show[fieldName]).format(format);
+    };
+  }
+};
