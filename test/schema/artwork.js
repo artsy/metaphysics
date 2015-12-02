336c3baf11f6415c79418421c03864cc29038561
dzucconi <mail@damonzucconi.com>
2015-12-01 21:22:59 -0500

    Clean up code; better specs

--- test/schema/artwork.js
+++ test/schema/artwork.js
@@ -1,36 +1,140 @@
 import _ from 'lodash';
-import Artwork from '../../schema/artwork';
-import { ArtworkPredicates } from '../../schema/artwork';
+import sinon from 'sinon';
+import { graphql } from 'graphql';
+import schema from '../../schema';
 
-let artwork = {
-  id: "richard-prince-untitled-portrait",
-  title: "Untitled (Portrait)",
-  forsale: true,
-  acquireable: false,
-  partner: [{id: 'galerie cabbie'}],
-  sales: [
-    {title: 'sale #1', is_auction: true},
-    {title: 'sale #2', is_auction: false}
-  ]
-}
+describe('Artwork type', () => {
+  let gravity;
+  let Artwork = schema.__get__('Artwork');
 
-describe('Artwork', () => {
-  describe('ArtworkPredicates.is_contactable', () => {
-    it('sets an artwork as contactable if it meets requirements', () => {
-      ArtworkPredicates.is_contactable(artwork, []).should.be.true();
+  let partner = { id: 'existy' };
+  let sale = { id: 'existy' };
+
+  let artwork = {
+    id: 'richard-prince-untitled-portrait',
+    title: 'Untitled (Portrait)',
+    forsale: true,
+    acquireable: false
+  };
+
+  beforeEach(() => {
+    gravity = sinon.stub();
+    Artwork.__Rewire__('gravity', gravity);
+  });
+
+  afterEach(() => {
+    Artwork.__ResetDependency__('gravity');
+  });
+
+  describe('#is_contactable', () => {
+    let query = `
+      {
+        artwork(id: "richard-prince-untitled-portrait") {
+          id
+          is_contactable
+        }
+      }
+    `;
+
+    it('is contactable if it meets all requirements', () => {
+      gravity
+        // Artwork
+        .onCall(0)
+        .returns(Promise.resolve(_.assign({}, artwork, {
+          partner: partner
+        })))
+        // Sales
+        .onCall(1)
+        .returns(Promise.resolve([]));
+
+      return graphql(schema, query)
+        .then(({ data }) => {
+          data.should.eql({
+            artwork: {
+              id: 'richard-prince-untitled-portrait',
+              is_contactable: true
+            }
+          });
+        });
     });
 
-    it('sets an artwork as not contactable if it has related sales', () => {
-      ArtworkPredicates.is_contactable(artwork, [{id: 'another auction by cab'}])
-        .should.be.false();
+    it('is not contactable if it has related sales', () => {
+      gravity
+        // Artwork
+        .onCall(0)
+        .returns(Promise.resolve(_.assign({}, artwork, {
+          partner: partner
+        })))
+        // Sales
+        .onCall(1)
+        .returns(Promise.resolve([sale]));
+
+      return graphql(schema, query)
+        .then(({ data }) => {
+          data.should.eql({
+            artwork: {
+              id: 'richard-prince-untitled-portrait',
+              is_contactable: false
+            }
+          });
+        });
     });
   });
-  describe('ArtworkPredicates.is_in_auction', () => {
-    it('if any artwork sale is an auction, then is_in_auction is true', () => {
-      ArtworkPredicates.is_in_auction(artwork, artwork.sales).should.be.true();
+
+  describe('#is_in_auction', () => {
+    let query = `
+      {
+        artwork(id: "richard-prince-untitled-portrait") {
+          id
+          is_in_auction
+        }
+      }
+    `;
+
+    it('is true if the artwork has any sales that are auctions', () => {
+      gravity
+        // Artwork
+        .onCall(0)
+        .returns(Promise.resolve(artwork))
+        // Sales
+        .onCall(1)
+        .returns(Promise.resolve([
+          _.assign({}, sale, { is_auction: false }),
+          _.assign({}, sale, { is_auction: true })
+        ]));
+
+      return graphql(schema, query)
+        .then(({ data }) => {
+          data.should.eql({
+            artwork: {
+              id: 'richard-prince-untitled-portrait',
+              is_in_auction: true
+            }
+          });
+        });
     });
-    it('if no artwork sales are in an auction, then is_in_auction is false', () => {
-      ArtworkPredicates.is_in_auction(artwork, []).should.be.false();
+
+    it('is false if the artwork is not in any sales that are auctions', () => {
+      gravity
+        // Artwork
+        .onCall(0)
+        .returns(Promise.resolve(artwork))
+        // Sales
+        .onCall(1)
+        .returns(Promise.resolve([
+          _.assign({}, sale, { is_auction: false }),
+          _.assign({}, sale, { is_auction: false })
+        ]));
+
+      return graphql(schema, query)
+        .then(({ data }) => {
+          data.should.eql({
+            artwork: {
+              id: 'richard-prince-untitled-portrait',
+              is_in_auction: false
+            }
+          });
+        });
     });
   });
 });
