dfca7368f71063e0f9d0eb6f0de225427b10da40
dzucconi <mail@damonzucconi.com>
2016-01-20 12:29:03 -0500

    Fix to work with API v1

--- schema/artwork/utilities.js
+++ schema/artwork/utilities.js
@@ -1,25 +1,21 @@
-import { mapValues } from 'lodash';
-
 export const isDimensional = (value) => parseFloat(value) > 0;
 
-export const isThreeDimensional = ({ dimensions }) => {
-  if (!dimensions.cm) return false;
-  const { depth, diameter } = dimensions.cm;
+export const isThreeDimensional = ({ depth, diameter }) => {
   return isDimensional(depth) && isDimensional(diameter);
 };
 
-export const isTwoDimensional = ({ dimensions }) => {
-  if (!dimensions.cm) return false;
-  const { width, height } = dimensions.cm;
+export const isTwoDimensional = ({ width, height, depth, diameter }) => {
   return (
     isDimensional(width) &&
     isDimensional(height) &&
-    !isThreeDimensional({ dimensions })
+    !isThreeDimensional({ depth, diameter })
   );
 };
 
-export const isTooBig = ({ dimensions }) => {
-  const LIMIT_IN_CM = 1500;
-  const { width, height } = mapValues(dimensions.cm, parseFloat);
-  return width > LIMIT_IN_CM || height > LIMIT_IN_CM;
+export const isTooBig = ({ width, height, metric }) => {
+  const LIMIT = { in: 600, cm: 1524 }; // 50 feet
+  return (
+    parseFloat(width) > LIMIT[metric] ||
+    parseFloat(height) > LIMIT[metric]
+  );
 };
