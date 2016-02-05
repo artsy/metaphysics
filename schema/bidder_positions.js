62c08b9ef328562953f3f1e42bdcbfcde72d62f5
Craig Spaeth <craigspaeth@gmail.com>
2016-02-05 14:17:13 -0500

    remove me flag

--- schema/bidder_positions.js
+++ schema/bidder_positions.js
@@ -2,17 +2,11 @@ import gravity from '../lib/loaders/gravity';
 import BidderPosition from './bidder_position';
 import {
   GraphQLList,
-  GraphQLBoolean,
 } from 'graphql';
 
 const BidderPositions = {
   type: new GraphQLList(BidderPosition.type),
   description: 'A list of bidder positions',
-  args: {
-    me: {
-      type: GraphQLBoolean,
-    },
-  },
   resolve: (root, options, { rootValue: { accessToken } }) => {
     return gravity.with(accessToken)('me/bidder_positions');
   },
