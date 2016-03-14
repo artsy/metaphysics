46fa0bdeabaa9789b7ff0357a40606aef0013c99
dzucconi <mail@damonzucconi.com>
2016-03-14 12:44:27 -0400

    Firm up TrendingArtists args

--- schema/trending.js
+++ schema/trending.js
@@ -8,6 +8,8 @@ import {
   GraphQLNonNull,
   GraphQLList,
   GraphQLInt,
+  GraphQLEnumType,
+  GraphQLBoolean,
 } from 'graphql';
 
 const TrendingArtistsType = new GraphQLObjectType({
@@ -25,6 +27,36 @@ const TrendingArtistsType = new GraphQLObjectType({
   }),
 });
 
+const TrendingMetricsType = new GraphQLEnumType({
+  name: 'TrendingMetrics',
+  values: {
+    ARTIST_FOLLOW: {
+      value: 'artist_follow',
+      description: 'Base time period: 2 weeks',
+    },
+    ARTIST_INQUIRY: {
+      value: 'artist_inquiry',
+      description: 'Base time period: 1 month',
+    },
+    ARTIST_SEARCH: {
+      value: 'artist_search',
+      description: 'Base time period: 2 weeks',
+    },
+    ARTIST_SAVE: {
+      value: 'artist_save',
+      description: 'Base time period: 1 month',
+    },
+    ARTIST_FAIR: {
+      value: 'artist_fair',
+      description: 'Number of artworks in fairs. Base time period: 12 weeks.',
+    },
+    ARTIST_AUCTION_LOT: {
+      value: 'artist_auction_lot',
+      description: 'Cumulative price achieved at auction. Base time period: 12 weeks',
+    },
+  },
+});
+
 const TrendingArtists = {
   type: TrendingArtistsType,
   description: 'Trending artists',
@@ -35,16 +67,26 @@ const TrendingArtists = {
       defaultValue: 'fetch',
     },
     name: {
-      type: new GraphQLNonNull(GraphQLString),
+      type: new GraphQLNonNull(TrendingMetricsType),
       description: 'Trending metric name',
     },
-    n: {
+    size: {
       type: GraphQLInt,
       description: 'Number of results to return',
       defaultValue: 40,
     },
+    double_time_period: {
+      type: GraphQLBoolean,
+      description: 'Fetch the top artists for each metric within double the base time period',
+      defaultValue: false,
+    },
   },
-  resolve: (root, options) => delta('/', options),
+  resolve: (root, { method, name, size, double_time_period }) =>
+    delta('/', {
+      method,
+      n: size,
+      name: name + (double_time_period ? '_2t' : ''),
+    }),
 };
 
 export default TrendingArtists;
