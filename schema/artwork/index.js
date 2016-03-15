ca3dffc9ca8ebcc490a90578aa881924dab644e1
Charles Broskoski <broskoski@gmail.com>
2016-03-15 12:53:46 -0400

    catch is_contactable if related sale cant be fetched

--- schema/artwork/index.js
+++ schema/artwork/index.js
@@ -114,7 +114,7 @@ const ArtworkType = new GraphQLObjectType({
                 !artwork.acquireable &&
                 !sales.length
               );
-            });
+            }).catch(() => false);
         },
       },
       is_in_auction: {
