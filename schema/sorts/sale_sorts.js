dccfa31c2e4de9e2c40febe92a5f428701e58a29
Laura Brown <laurabrown1113@gmail.com>
2016-03-18 19:46:10 +0100

    cover image for sale

---
+++ schema/sorts/sale_sorts.js
@@ -0,0 +1,45 @@
+import { GraphQLEnumType } from 'graphql';
+
+export default {
+  type: new GraphQLEnumType({
+    name: 'SaleSorts',
+    values: {
+      _ID_ASC: {
+        value: '_id',
+      },
+      _ID_DESC: {
+        value: '-_id',
+      },
+      NAME_ASC: {
+        value: 'name',
+      },
+      NAME_DESC: {
+        value: '-name',
+      },
+      CREATED_AT_ASC: {
+        value: 'created_at',
+      },
+      CREATED_AT_DESC: {
+        value: '-created_at',
+      },
+      END_AT_ASC: {
+        value: 'end_at',
+      },
+      END_AT_DESC: {
+        value: '-end_at',
+      },
+      START_AT_ASC: {
+        value: 'start_at',
+      },
+      START_AT_DESC: {
+        value: '-start_at',
+      },
+      ELIGIBLE_SALE_ARTWORKS_COUNT_ASC: {
+        value: 'eligible_sale_artworks_count',
+      },
+      ELIGIBLE_SALE_ARTWORKS_COUNT_DESC: {
+        value: '-eligible_sale_artworks_count',
+      },
+    },
+  }),
+};
