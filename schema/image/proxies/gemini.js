a99f4ac3e88d6e8c74afd99b8b2c8af73dd33695
dzucconi <mail@damonzucconi.com>
2016-03-22 12:15:32 -0400

    Get Gemini URLs working

--- schema/image/proxies/gemini.js
+++ schema/image/proxies/gemini.js
@@ -1,14 +1,28 @@
 import qs from 'qs';
+import { isEmpty } from 'lodash';
+import { isExisty } from '../../../lib/helpers';
+
 const { GEMINI_ENDPOINT } = process.env;
 
-export default (src, mode, width, height) => {
-  const resizeTo = (mode === 'resize') ? 'fit' : mode;
+function resizeTo(mode, width, height) {
+  if (mode === 'crop') {
+    return 'fill';
+  } else if (isExisty(width) && isEmpty(height)) {
+    return 'width';
+  } else if (isExisty(height) && isEmpty(width)) {
+    return 'height';
+  }
+  return 'fit';
+}
 
-  return `${GEMINI_ENDPOINT}/?${qs.stringify({
-    resize_to: resizeTo,
-    height,
+export default (src, mode, width, height) => {
+  const options = {
+    resize_to: resizeTo(mode, width, height),
     width,
+    height,
     quality: 95,
     src,
-  })}`;
+  };
+
+  return `${GEMINI_ENDPOINT}/?${qs.stringify(options)}`;
 };
