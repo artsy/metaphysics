2fe4f3bed67e09247a6816a1003f9ed7d4376123
dzucconi <mail@damonzucconi.com>
2016-04-04 15:54:52 -0400

    More complete Artwork#shows fetch

--- schema/partner.js
+++ schema/partner.js
@@ -1,8 +1,11 @@
+import {
+  assign,
+  omit,
+} from 'lodash';
 import gravity from '../lib/loaders/gravity';
 import cached from './fields/cached';
 import initials from './fields/initials';
 import Profile from './profile';
-import PartnerShow from './partner_show';
 import Location from './location';
 import {
   GraphQLString,
@@ -15,83 +18,88 @@ import {
 
 const PartnerType = new GraphQLObjectType({
   name: 'Partner',
-  fields: () => ({
-    cached,
-    _id: {
-      type: GraphQLString,
-    },
-    id: {
-      type: GraphQLString,
-    },
-    name: {
-      type: GraphQLString,
-      resolve: ({ name }) => name.trim(),
-    },
-    type: {
-      type: GraphQLString,
-    },
-    href: {
-      type: GraphQLString,
-      resolve: ({ default_profile_id }) => `/${default_profile_id}`,
-    },
-    is_linkable: {
-      type: GraphQLBoolean,
-      resolve: ({ default_profile_id, default_profile_public }) => {
-        return default_profile_id && default_profile_public;
+  fields: () => {
+    // Prevent circular dependency
+    const PartnerShows = require('./partner_shows').default;
+
+    return {
+      cached,
+      _id: {
+        type: GraphQLString,
       },
-    },
-    is_pre_qualify: {
-      type: GraphQLBoolean,
-      resolve: ({ pre_qualify }) => pre_qualify,
-    },
-    is_limited_fair_partner: {
-      type: GraphQLBoolean,
-      resolve: ({ has_limited_fair_partnership }) => has_limited_fair_partnership,
-    },
-    initials: initials('name'),
-    default_profile_id: {
-      type: GraphQLString,
-    },
-    profile: {
-      type: Profile.type,
-      resolve: ({ default_profile_id }) =>
-        gravity(`profile/${default_profile_id}`)
-          .catch(() => null),
-    },
-    shows: {
-      type: new GraphQLList(PartnerShow.type),
-      args: {
-        size: {
-          type: GraphQLInt,
+      id: {
+        type: GraphQLString,
+      },
+      name: {
+        type: GraphQLString,
+        resolve: ({ name }) => name.trim(),
+      },
+      type: {
+        type: GraphQLString,
+      },
+      href: {
+        type: GraphQLString,
+        resolve: ({ default_profile_id }) => `/${default_profile_id}`,
+      },
+      is_linkable: {
+        type: GraphQLBoolean,
+        resolve: ({ default_profile_id, default_profile_public }) => {
+          return default_profile_id && default_profile_public;
         },
       },
-      resolve: ({ id }, options) => gravity(`partner/${id}/shows`, options),
-    },
-    locations: {
-      type: new GraphQLList(Location.type),
-      args: {
-        size: {
-          type: GraphQLInt,
+      is_pre_qualify: {
+        type: GraphQLBoolean,
+        resolve: ({ pre_qualify }) => pre_qualify,
+      },
+      is_limited_fair_partner: {
+        type: GraphQLBoolean,
+        resolve: ({ has_limited_fair_partnership }) => has_limited_fair_partnership,
+      },
+      initials: initials('name'),
+      default_profile_id: {
+        type: GraphQLString,
+      },
+      profile: {
+        type: Profile.type,
+        resolve: ({ default_profile_id }) =>
+          gravity(`profile/${default_profile_id}`)
+            .catch(() => null),
+      },
+      shows: {
+        type: PartnerShows.type,
+        args: omit(PartnerShows.args, 'partner_id'),
+        resolve: ({ _id }, options) => {
+          return PartnerShows.resolve(null, assign({}, options, {
+            partner_id: _id,
+          }));
         },
       },
-      resolve: ({ id }, options) => gravity(`partner/${id}/locations`, options),
-    },
-    contact_message: {
-      type: GraphQLString,
-      resolve: ({ type }) => {
-        if (type === 'Auction') {
+      locations: {
+        type: new GraphQLList(Location.type),
+        args: {
+          size: {
+            type: GraphQLInt,
+          },
+        },
+        resolve: ({ id }, options) => gravity(`partner/${id}/locations`, options),
+      },
+      contact_message: {
+        type: GraphQLString,
+        resolve: ({ type }) => {
+          if (type === 'Auction') {
+            return [
+              'Hello, I am interested in placing a bid on this work.',
+              'Please send me more information.',
+            ].join(' ');
+          }
           return [
-            'Hello, I am interested in placing a bid on this work.',
-            'Please send me more information.',
+            'Hi, I’m interested in purchasing this work.',
+            'Could you please provide more information about the piece?',
           ].join(' ');
-        }
-        return [
-          'Hi, I’m interested in purchasing this work.',
-          'Could you please provide more information about the piece?',
-        ].join(' ');
+        },
       },
-    },
-  }),
+    };
+  },
 });
 
 const Partner = {
