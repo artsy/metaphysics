538db62d93ca2f4220a1097c4cd3eb49ae0a1c7b
dzucconi <mail@damonzucconi.com>
2016-04-14 13:17:43 -0400

    Adds ArtworkLayers

---
+++ schema/artwork_layer.js
@@ -0,0 +1,38 @@
+import Artwork from './artwork';
+import gravity from '../lib/loaders/gravity';
+import {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLList,
+} from 'graphql';
+
+const ArtworkLayerType = new GraphQLObjectType({
+  name: 'ArtworkLayer',
+  fields: () => ({
+    id: {
+      type: GraphQLString,
+    },
+    type: {
+      type: GraphQLString,
+    },
+    name: {
+      type: GraphQLString,
+    },
+    description: {
+      type: GraphQLString,
+    },
+    more_info_url: {
+      type: GraphQLString,
+    },
+    artworks: {
+      type: new GraphQLList(Artwork.type),
+      resolve: ({ id, type, artwork_id }) => {
+        return gravity(`related/layer/${type}/${id}/artworks`, { artwork: [artwork_id] });
+      },
+    },
+  }),
+});
+
+export default {
+  type: ArtworkLayerType,
+};
