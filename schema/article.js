ed4b047960358dd9161f9c7e68e2364582b67744
Laura Brown <laurabrown1113@gmail.com>
2016-05-24 21:22:47 +0200

    use date

--- schema/article.js
+++ schema/article.js
@@ -2,6 +2,7 @@ import positron from '../lib/loaders/positron';
 import cached from './fields/cached';
 import AuthorType from './author';
 import Image from './image';
+import date from './fields/date';
 import {
   GraphQLString,
   GraphQLObjectType,
@@ -18,12 +19,8 @@ const ArticleType = new GraphQLObjectType({
     title: {
       type: GraphQLString,
     },
-    published_at: {
-      type: GraphQLString,
-    },
-    updated_at: {
-      type: GraphQLString,
-    },
+    published_at: date,
+    updated_at: date,
     thumbnail_title: {
       type: GraphQLString,
     },
