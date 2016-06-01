273b2ca9da31890daded7f5810e97b6eebaeadf4
Laura Brown <laurabrown1113@gmail.com>
2016-06-01 16:10:48 +0200

    some clean up and ability to sort articles

--- schema/artist/index.js
+++ schema/artist/index.js
@@ -3,8 +3,6 @@ import {
   compact,
   concat,
   take,
-  sortBy,
-  reverse,
 } from 'lodash';
 import { exclude } from '../../lib/helpers';
 import cached from '../fields/cached';
@@ -18,6 +16,7 @@ import Meta from './meta';
 import PartnerShow from '../partner_show';
 import Sale from '../sale/index';
 import ArtworkSorts from '../sorts/artwork_sorts';
+import ArticleSorts from '../sorts/article_sorts';
 import PartnerShowSorts from '../sorts/partner_show_sorts';
 import SaleSorts from '../sale/sorts';
 import ArtistCarousel from './carousel';
@@ -381,6 +380,7 @@ const ArtistType = new GraphQLObjectType({
 
       articles: {
         args: {
+          sort: ArticleSorts,
           limit: {
             type: GraphQLInt,
           },
