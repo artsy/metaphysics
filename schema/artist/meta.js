6964c4943915f5169f6b761cef7bccb5a6408cad
Charles Broskoski <broskoski@gmail.com>
2016-07-20 16:39:40 -0400

    Remove logic for artist title tag test

--- schema/artist/meta.js
+++ schema/artist/meta.js
@@ -1,4 +1,3 @@
-import { slugs } from './maps/artist_title_slugs';
 import descriptions from './maps/artist_meta_descriptions';
 import { stripTags, truncate, markdownToText } from '../../lib/helpers';
 import { compact } from 'lodash';
@@ -6,16 +5,6 @@ import {
   GraphQLString,
   GraphQLObjectType,
 } from 'graphql';
-
-
-export const countDisplay = (artist) => {
-  const count = artist.published_artworks_count;
-
-  if (count < 10) { return ''; }
-  if (count < 100) { return `${Math.floor(count / 10) * 10}+ `; }
-  if (count < 10000) { return `${Math.floor(count / 100) * 100}+ `; }
-  return `${Math.floor(count / 10000) * 10000}+ `;
-};
 
 export const metaName = (artist) => {
   if (artist.name) return stripTags(artist.name);
@@ -28,9 +17,6 @@ const ArtistMetaType = new GraphQLObjectType({
     title: {
       type: GraphQLString,
       resolve: (artist) => {
-        if (slugs.indexOf(artist.id) !== -1) {
-          return `${artist.name} Art - ${countDisplay(artist)}Works, Bio, News | Artsy`;
-        }
         const count = artist.published_artworks_count;
         return `${metaName(artist)} - ${count} Artworks, Bio & Shows on Artsy`;
       },
