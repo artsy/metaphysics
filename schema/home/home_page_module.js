5e2b51aadf75125e27238b5c0b1bace9be7d6b25
Charles Broskoski <broskoski@gmail.com>
2016-07-27 17:40:23 -0400

    Deprecate old homepage query style

--- schema/home/home_page_module.js
+++
@@ -1,9 +0,0 @@
-// NOTE: This type and file are deprecated, edit home_page_artwork_module.js instead.
-//       These should be removed in the future once Force has migrated to the new type.
-//
-//       In addition, you should not try to use this type through the Node interface.
-
-import { createHomePageArtworkModule } from './home_page_artwork_module';
-
-const HomePageModule = createHomePageArtworkModule('HomePageModule');
-export default HomePageModule;
