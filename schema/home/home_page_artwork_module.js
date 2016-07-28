d8197d85b54163b8f9512083f92a0c55fbcc795b
Charles Broskoski <broskoski@gmail.com>
2016-07-28 10:58:35 -0400

    Remove create module function

--- schema/home/home_page_artwork_module.js
+++ schema/home/home_page_artwork_module.js
@@ -17,67 +17,62 @@ import {
   GraphQLNonNull,
 } from 'graphql';
 
-export function createHomePageArtworkModule(moduleTypeName) {
-  const type = new GraphQLObjectType({
-    name: moduleTypeName,
-    interfaces: [NodeInterface],
-    isTypeOf: (obj) => has(obj, 'key') && has(obj, 'display'),
-    fields: () => ({
-      __id: {
-        type: new GraphQLNonNull(GraphQLID),
-        description: 'A globally unique ID.',
-        resolve: ({ key, params }) => {
-          // Compose this ID from params that `resolve` uses to identify a rail later on.
-          const payload = { key };
-          if (params) {
-            payload.id = params.id;
-          }
-          return toGlobalId(moduleTypeName, JSON.stringify(payload));
-        },
-      },
-      key: {
-        type: GraphQLString,
-      },
-      display: {
-        type: GraphQLString,
-        deprecationReason: 'Favor `is_`-prefixed Booleans (*and* this should be a Boolean)',
-      },
-      is_displayable: {
-        type: GraphQLBoolean,
-        resolve: ({ display }) => display,
-      },
-      params: Params,
-      context: Context,
-      title: Title,
-      results: Results,
-    }),
-  });
-
-  const mod = {
-    type,
-    description: 'Single artwork module to show on the home screen',
-    args: {
-      key: {
-        type: GraphQLString,
-        description: 'Module key',
-      },
-      id: {
-        type: GraphQLString,
-        description: 'ID of generic gene rail to target',
+const HomePageArtworkModuleType = new GraphQLObjectType({
+  name: 'HomePageArtworkModule',
+  interfaces: [NodeInterface],
+  isTypeOf: (obj) => has(obj, 'key') && has(obj, 'display'),
+  fields: () => ({
+    __id: {
+      type: new GraphQLNonNull(GraphQLID),
+      description: 'A globally unique ID.',
+      resolve: ({ key, params }) => {
+        // Compose this ID from params that `resolve` uses to identify a rail later on.
+        const payload = { key };
+        if (params) {
+          payload.id = params.id;
+        }
+        return toGlobalId('HomePageArtworkModule', JSON.stringify(payload));
       },
     },
-    resolve: (root, { key, id }) => {
-      // is id a generic gene?
-      const params = find(genericGenes, ['id', id]);
-      if (params) {
-        return { key, params, display: true };
-      }
-      return { key, display: true };
+    key: {
+      type: GraphQLString,
     },
-  };
+    display: {
+      type: GraphQLString,
+      deprecationReason: 'Favor `is_`-prefixed Booleans (*and* this should be a Boolean)',
+    },
+    is_displayable: {
+      type: GraphQLBoolean,
+      resolve: ({ display }) => display,
+    },
+    params: Params,
+    context: Context,
+    title: Title,
+    results: Results,
+  }),
+});
 
-  return mod;
-}
+const HomePageArtworkModule = {
+  type: HomePageArtworkModuleType,
+  description: 'Single artwork module to show on the home screen',
+  args: {
+    key: {
+      type: GraphQLString,
+      description: 'Module key',
+    },
+    id: {
+      type: GraphQLString,
+      description: 'ID of generic gene rail to target',
+    },
+  },
+  resolve: (root, { key, id }) => {
+    // is id a generic gene?
+    const params = find(genericGenes, ['id', id]);
+    if (params) {
+      return { key, params, display: true };
+    }
+    return { key, display: true };
+  },
+};
 
-const HomePageArtworkModule = createHomePageArtworkModule('HomePageArtworkModule');
 export default HomePageArtworkModule;
