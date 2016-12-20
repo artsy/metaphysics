dc53e409b9dc5a3ec4be9f951a7e7768aac0dc79
orta <orta.therox@gmail.com>
2016-12-20 12:46:21 -0500

    Adds a check for the assignee

--- dangerfile.js
+++ dangerfile.js
@@ -3,6 +3,13 @@
 import { danger, fail, warn, markdown } from 'danger';
 const fs = require('fs');
 
+// Make sure we're using the assignee system so that
+// our slack bot works correctly.
+const someoneAssigned = danger.github.pr.assignee;
+if (someoneAssigned === null) {
+  fail('Please set a reviewer');
+}
+
 // Move all JS files towards using flow
 const changedFiles = danger.git.created_files.concat(danger.git.modified_files);
 const unFlowedFiles = changedFiles.filter(path => !path.startsWith('test/') && path.endsWith('js'))
@@ -22,7 +29,7 @@ if (unFlowedFiles.length > 0) {
   warn(`These new JS files do not have Flow enabled: ${unFlowedFiles.join(', ')}`);
 }
 
-// Request a CHANGELOG entry
+// Request a CHANGELOG entry, but allow a PR to say it doesn't neeed one
 const hasChangelog = changedFiles.includes('changelog.md');
 const declaredTrivial = danger.pr.title.indexOf('trivial') !== -1;
 
@@ -30,8 +37,8 @@ if (!hasChangelog && !declaredTrivial) {
   fail('Please add a changelog entry noting your changes.');
 }
 
+// Politely ask for their name on the entry too
 if (hasChangelog) {
-  // Politely ask for their name on the entry too
   const changelogDiff = danger.git.diffForFile('changelog.md');
   const contributorName = danger.github.pr.user.login;
   if (changelogDiff && changelogDiff.indexOf(contributorName) === -1) {
