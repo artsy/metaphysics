65ac1ccd8658eeddc79b37476bcb50db43195df4
kanaabe <kanakoabe5@gmail.com>
2017-03-13 18:01:04 -0400

    Adds count to artist articles resolve

--- schema/artist/statuses.js
+++ schema/artist/statuses.js
@@ -59,6 +59,7 @@ const ArtistStatusesType = new GraphQLObjectType({
           artist_id: _id,
           published: true,
           limit: 0,
+          count: true,
         }).then(({ count }) => count > 0);
       },
     },
