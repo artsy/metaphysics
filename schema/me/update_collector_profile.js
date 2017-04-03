6fc1b1ddbca679e617dd5b0b9a730ccc7895736b
Matthew Zikherman <matt@bestvendor.com>
2017-04-03 14:27:40 -0400

    Save self_reported_purchaes as well as loyalty applicant status

--- schema/me/update_collector_profile.js
+++ schema/me/update_collector_profile.js
@@ -2,6 +2,7 @@ import gravity from '../../lib/loaders/gravity';
 import { CollectorProfileType } from './collector_profile';
 import {
   GraphQLBoolean,
+  GraphQLString,
 } from 'graphql';
 
 export default {
@@ -14,14 +15,18 @@ export default {
     professional_buyer: {
       type: GraphQLBoolean,
     },
+    self_reported_purchases: {
+      type: GraphQLString,
+    },
   },
   resolve: (root, {
     loyalty_applicant,
     professional_buyer,
+    self_reported_purchases,
   }, request, { rootValue: { accessToken } }) => {
     if (!accessToken) return null;
     return gravity.with(accessToken, {
       method: 'PUT',
-    })('me/collector_profile', { loyalty_applicant, professional_buyer });
+    })('me/collector_profile', { loyalty_applicant, professional_buyer, self_reported_purchases });
   },
 };
