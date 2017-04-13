f24652f77f249adb8967f011330b6961159b25c6
Charles Broskoski <broskoski@gmail.com>
2017-04-13 12:10:34 -0400

    Adds additonal spec to return is_saved as false if artwork is not returned

--- test/lib/loaders/saved_artwork.js
+++ test/lib/loaders/saved_artwork.js
@@ -27,4 +27,27 @@ describe('savedArtwork', () => {
           .toBe(true);
       });
   });
+
+  it('marks is_saved as false if artwork is not returned', () => {
+    const userID = 'fake';
+    const accessToken = 'hello';
+    const id = 'cab';
+
+    const gravity = sinon.stub()
+      .returns(Promise.resolve({
+        body: [],
+      }));
+
+    savedArtwork.__Rewire__('gravity', gravity);
+
+    return savedArtwork.load(JSON.stringify({ id, userID, accessToken }))
+      .then(artwork => {
+        expect(gravity.args[0][0])
+          .toBe('collection/saved-artwork/artworks?artworks%5B%5D=cab&private=true&user_id=fake');
+        expect(gravity.args[0][1])
+          .toBe('hello');
+        expect(artwork.is_saved)
+          .toBe(false);
+      });
+  });
 });
