c4402f79f298cd9c6d56ac04ef32a3121dc394d7
xtina-starr <c.thompsonms@gmail.com>
2017-04-17 15:21:48 -0400

    stub the request for the fair organizer profile

--- test/schema/fair.js
+++ test/schema/fair.js
@@ -11,10 +11,6 @@ describe('Fair type', () => {
       name: 'The Armory Show 2017',
       organizer: {
         profile_id: 'the-armory-show',
-        profile: {
-          published: true,
-          private: false,
-        },
       },
     };
 
@@ -43,6 +39,16 @@ describe('Fair type', () => {
   `;
 
   it('is_publically_visible returns true when profile is published', () => {
+    const profileData = {
+      id: 'the-armory-show',
+      published: true,
+      private: false,
+    };
+
+    gravity
+      .onCall(1)
+      .returns(Promise.resolve(profileData));
+
     return runQuery(query)
       .then(data => {
         expect(data).toEqual({
@@ -59,53 +65,18 @@ describe('Fair type', () => {
         });
       });
   });
-});
-
-describe('Fair unpublished organizer profile', () => {
-  const Fair = schema.__get__('Fair');
-  let gravity = null;
-  let fairData = null;
-
-  beforeEach(() => {
-    gravity = sinon.stub();
-
-    fairData = {
-      id: 'the-armory-show-2017',
-      name: 'The Armory Show 2017',
-      organizer: {
-        profile_id: 'the-armory-show',
-        profile: {
-          published: false,
-          private: false,
-        },
-      },
-    };
-
-    gravity.returns(Promise.resolve(fairData));
-
-    Fair.__Rewire__('gravity', gravity);
-  });
-
-  afterEach(() => {
-    Fair.__ResetDependency__('gravity');
-  });
-
-  const query = `
-    {
-      fair(id: "the-armory-show-2017") {
-        id
-        name
-        organizer {
-          profile_id
-          profile {
-            is_publically_visible
-          }
-        }
-      }
-    }
-  `;
 
   it('is_publically_visible returns false when profile is not published', () => {
+    const unpublishedProfileData = {
+      id: 'context',
+      published: false,
+      private: false,
+    };
+
+    gravity
+      .onCall(1)
+      .returns(Promise.resolve(unpublishedProfileData));
+
     return runQuery(query)
       .then(data => {
         expect(data).toEqual({
@@ -122,53 +93,18 @@ describe('Fair unpublished organizer profile', () => {
         });
       });
   });
-});
 
-describe('Fair private organizer profile', () => {
-  const Fair = schema.__get__('Fair');
-  let gravity = null;
-  let fairData = null;
-
-  beforeEach(() => {
-    gravity = sinon.stub();
-
-    fairData = {
-      id: 'the-armory-show-2017',
-      name: 'The Armory Show 2017',
-      organizer: {
-        profile_id: 'the-armory-show',
-        profile: {
-          published: true,
-          private: true,
-        },
-      },
+  it('is_publically_visible returns false when profile is not published', () => {
+    const privateProfileData = {
+      id: 'context',
+      published: false,
+      private: false,
     };
 
-    gravity.returns(Promise.resolve(fairData));
+    gravity
+      .onCall(1)
+      .returns(Promise.resolve(privateProfileData));
 
-    Fair.__Rewire__('gravity', gravity);
-  });
-
-  afterEach(() => {
-    Fair.__ResetDependency__('gravity');
-  });
-
-  const query = `
-    {
-      fair(id: "the-armory-show-2017") {
-        id
-        name
-        organizer {
-          profile_id
-          profile {
-            is_publically_visible
-          }
-        }
-      }
-    }
-  `;
-
-  it('is_publically_visible returns false when profile is private', () => {
     return runQuery(query)
       .then(data => {
         expect(data).toEqual({
