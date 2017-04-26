d2747ac75a4cbb99b5f0c451eb077c831de5074d
Charles Broskoski <broskoski@gmail.com>
2017-04-26 19:47:07 -0400

    Guess danger hates me

--- dangerfile.js
+++ dangerfile.js
@@ -13,18 +13,18 @@ if (someoneAssigned === null) {
 const changedFiles = danger.git.created_files.concat(danger.git.modified_files)
 
 // Request a CHANGELOG entry, but allow a PR to say it doesn't neeed one
-const hasChangelog = changedFiles.includes("changelog.md")
-const declaredTrivial = danger.pr.title.indexOf("trivial") !== -1
+// const hasChangelog = changedFiles.includes("changelog.md")
+// const declaredTrivial = danger.pr.title.indexOf("trivial") !== -1
 
-if (!hasChangelog && !declaredTrivial) {
-  fail("Please add a changelog entry noting your changes.")
-}
+// if (!hasChangelog && !declaredTrivial) {
+//   fail("Please add a changelog entry noting your changes.")
+// }
 
 // Politely ask for their name on the entry too
-if (hasChangelog) {
-  const changelogDiff = danger.git.diffForFile("changelog.md")
-  const contributorName = danger.github.pr.user.login
-  if (changelogDiff && changelogDiff.indexOf(contributorName) === -1) {
-    warn("Please add your GitHub name (\"" + contributorName + "\") to the changelog entry.")
-  }
-}
+// if (hasChangelog) {
+//   const changelogDiff = danger.git.diffForFile("changelog.md")
+//   const contributorName = danger.github.pr.user.login
+//   if (changelogDiff && changelogDiff.indexOf(contributorName) === -1) {
+//     warn("Please add your GitHub name (\"" + contributorName + "\") to the changelog entry.")
+//   }
+// }
