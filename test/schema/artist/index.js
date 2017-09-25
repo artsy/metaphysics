58d60cbe50de94d99e194a0b83bcdc1e3d0769b2
Matthew Zikherman <matt@bestvendor.com>
2017-09-25 13:50:36 -0400

    Dont return related shows for an artist from private partners

--- test/schema/artist/index.js
+++ test/schema/artist/index.js
@@ -22,6 +22,7 @@ describe("Artist type", () => {
         .stub()
         .withArgs(artist.id)
         .returns(Promise.resolve(artist)),
+
     }
 
     Artist.__Rewire__(
@@ -534,6 +535,54 @@ describe("Artist type", () => {
               credit: "Submitted by Catty Partner",
               partner_id: "catty-partner",
             },
+          },
+        })
+      })
+    })
+  })
+  describe("concerning related shows", () => {
+    it("excludes shows from private partners", () => {
+      const privateShow = {
+        id: "oops",
+        partner: {
+          type: "Private Dealer",
+          has_full_profile: true,
+          profile_banner_display: false,
+        },
+        is_reference: true,
+        display_on_partner_profile: false,
+      }
+      const publicShow = {
+        id: "ok",
+        partner: {
+          type: "Gallery",
+          has_full_profile: true,
+          profile_banner_display: false,
+        },
+        is_reference: true,
+        display_on_partner_profile: false,
+      }
+      rootValue.relatedShowsLoader = sinon
+        .stub()
+        .withArgs(artist.id)
+        .returns(Promise.resolve([privateShow, publicShow]))
+      const query = `
+        {
+          artist(id: "foo-bar") {
+            shows {
+              id
+            }
+          }
+        }
+      `
+      return runQuery(query, rootValue).then(data => {
+        expect(data).toEqual({
+          artist: {
+            shows: [
+              {
+                id: "ok",
+              },
+            ],
           },
         })
       })
