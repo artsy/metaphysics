eea887e95d9bfa6f55318f099161c9fc703745b5
Sarah Scott <sarahoscott@gmail.com>
2017-11-01 10:43:39 +0100

    added tests for home fairs module

--- schema/home/home_page_fairs_module.js
+++ schema/home/home_page_fairs_module.js
@@ -1,21 +1,18 @@
 import Fair from "schema/fair"
-import { GraphQLList, GraphQLObjectType } from "graphql"
+import { GraphQLList, GraphQLObjectType, GraphQLNonNull } from "graphql"
 
 export const HomePageFairsModuleType = new GraphQLObjectType({
   name: "HomePageFairsModule",
   fields: {
     results: {
-      type: new GraphQLList(Fair.type),
+      type: new GraphQLNonNull(new GraphQLList(Fair.type)),
       resolve: (root, options, request, { rootValue: { fairsLoader } }) => {
         // Check for all fairs that are currently running
-        const isEligible = { has_full_feature: true, sort: "-start_at" }
-        const gravityOptions = { ...options, ...isEligible, active: true }
+        const gravityOptions = { has_full_feature: true, sort: "-start_at", active: true }
         return fairsLoader(gravityOptions).then(runningFairs => {
           // If there are less than 8, get the most recent closed fairs
           if (runningFairs.length < 8) {
-            gravityOptions.status = "closed"
-            gravityOptions.active = false
-            const newOptions = { ...gravityOptions, size: 8 - runningFairs.length }
+            const newOptions = { ...gravityOptions, status: "closed", active: false, size: 8 - runningFairs.length }
             return fairsLoader(newOptions).then(closedFairs => {
               const allFairs = runningFairs.concat(closedFairs)
               return allFairs
