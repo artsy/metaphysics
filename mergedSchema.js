ce0bb0c2a2ebbfba799b5dbf4d41ebf308ee6fa0
Eloy Durán <eloy.de.enige@gmail.com>
2017-11-14 21:08:11 +0100

    [Stitching] Fetch Convection schema on startup using just XApp token.

--- mergedSchema.js
+++ mergedSchema.js
@@ -1,17 +1,53 @@
 import { mergeSchemas, introspectSchema, makeRemoteExecutableSchema } from "graphql-tools"
+import { ApolloLink } from "apollo-link"
 import { createHttpLink } from "apollo-link-http"
+import { setContext } from "apollo-link-context"
 import fetch from "node-fetch"
 
+import config from "./config"
 import localSchema from "./schema"
 
-export default async function mergedSchema() {
-  const convectionLink = createHttpLink({
+function createConvectionLink() {
+  const httpLink = createHttpLink({
     fetch,
     uri: process.env.CONVECTION_GRAPH_URL,
-    headers: {
-      Authorization: `Bearer ${process.env.CONVECTION_TOKEN}`,
-    },
   })
+
+  const middlewareLink = new ApolloLink((operation, forward) => forward(operation))
+
+  const authMiddleware = setContext((_request, context) => {
+    const loaders = context.graphqlContext && context.graphqlContext.res.locals.dataLoaders
+    const tokenLoader = loaders && loaders.convectionTokenLoader
+
+    // If a token loader exists for Convection (i.e. this is an authenticated request), use that token to make
+    // authenticated requests to Convection.
+    if (tokenLoader) {
+      return tokenLoader().then(({ token }) => {
+        return {
+          ...context.headers,
+          headers: {
+            authorization: `Bearer ${token}`,
+          },
+        }
+      })
+    }
+
+    // Otherwise use MP’s XApp token so that on startup MP can fetch Convection’s schema.
+    // TODO: Actually enable once Convection supports this.
+    return {
+      ...context.headers,
+      headers: {
+        // authorization: `XApp ${config.GRAVITY_XAPP_TOKEN}`,
+        authorization: `Bearer ${process.env.CONVECTION_TOKEN}`,
+      },
+    }
+  })
+
+  return middlewareLink.concat(authMiddleware).concat(httpLink)
+}
+
+export default async function mergedSchema() {
+  const convectionLink = createConvectionLink()
 
   const convectionSchema = await makeRemoteExecutableSchema({
     schema: await introspectSchema(convectionLink),
