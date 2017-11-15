6a38a2735cb525ba9c6144e25b1ef948aedb3f2b
Ash Furrow <ash@ashfurrow.com>
2017-11-15 15:38:19 -0500

    Adds time to schema, accessing system/time endpoint.

---
+++ schema/time/index.js
@@ -0,0 +1,33 @@
+import { GraphQLString, GraphQLBoolean, GraphQLInt, GraphQLObjectType } from "graphql"
+
+export const timeFields = () => {
+  return {
+    day: { type: GraphQLInt },
+    wday: { type: GraphQLInt },
+    month: { type: GraphQLInt },
+    year: { type: GraphQLInt },
+    hour: { type: GraphQLInt },
+    min: { type: GraphQLInt },
+    sec: { type: GraphQLInt },
+    dst: { type: GraphQLBoolean },
+    unix: { type: GraphQLInt },
+    utc_offset: { type: GraphQLInt },
+    zone: { type: GraphQLString },
+    iso8601: { type: GraphQLString },
+  }
+}
+
+export const TimeType = new GraphQLObjectType({
+  name: "Time",
+  fields: () => ({
+    ...timeFields(),
+  }),
+})
+
+const Time = {
+  type: TimeType,
+  description: "Artsy system time",
+  resolve: (root, params, request, { rootValue: { timeLoader } }) => timeLoader(),
+}
+
+export default Time
