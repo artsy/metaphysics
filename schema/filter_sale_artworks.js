2254551345e864ade6a3b84b09f32acb2be4881e
Christopher Pappas <damassi.pappas@gmail.com>
2017-11-21 19:40:19 -0800

    Finish refactor

--- schema/filter_sale_artworks.js
+++ schema/filter_sale_artworks.js
@@ -2,14 +2,15 @@ import gravity from "lib/loaders/legacy/gravity"
 import { map, omit } from "lodash"
 import SaleArtwork from "./sale_artwork"
 import numeral from "./fields/numeral"
+import { GraphQLList, GraphQLObjectType, GraphQLString, GraphQLBoolean, GraphQLInt, GraphQLID } from "graphql"
 import {
   SaleArtworksAggregationResultsType,
   SaleArtworksAggregation,
 } from "./aggregations/filter_sale_artworks_aggregation"
-import { GraphQLList, GraphQLObjectType, GraphQLString, GraphQLBoolean, GraphQLInt, GraphQLID } from "graphql"
-import { connectionDefinitions, connectionFromArraySlice } from "graphql-relay"
-import { parseRelayOptions } from "lib/helpers"
-import { pageable } from "relay-cursor-paging"
+
+/**
+ * NOTE: This type has been deprecated in favor of `SaleArtworks`.
+ */
 
 export const filterSaleArtworksArgs = {
   aggregations: {
@@ -41,52 +42,37 @@ export const filterSaleArtworksArgs = {
   },
 }
 
+export const SaleArtworkAggregations = {
+  description: "Returns aggregation counts for the given filter query.",
+  type: new GraphQLList(SaleArtworksAggregationResultsType),
+  resolve: ({ aggregations }) => {
+    const whitelistedAggregations = omit(aggregations, ["total", "followed_artists"])
+    return map(whitelistedAggregations, (counts, slice) => ({
+      slice,
+      counts,
+    }))
+  },
+}
+
+export const SaleArtworkCounts = {
+  type: new GraphQLObjectType({
+    name: "FilterSaleArtworksCounts",
+    fields: {
+      total: numeral(({ aggregations }) => aggregations.total.value),
+      followed_artists: numeral(({ aggregations }) => aggregations.followed_artists.value),
+    },
+  }),
+  resolve: artist => artist,
+}
+
 export const FilterSaleArtworksType = new GraphQLObjectType({
   name: "FilterSaleArtworks",
   fields: () => ({
-    aggregations: {
-      description: "Returns aggregation counts for the given filter query.",
-      type: new GraphQLList(SaleArtworksAggregationResultsType),
-      resolve: ({ aggregations }) => {
-        const whitelistedAggregations = omit(aggregations, ["total", "followed_artists"])
-        return map(whitelistedAggregations, (counts, slice) => ({
-          slice,
-          counts,
-        }))
-      },
-    },
-    counts: {
-      type: new GraphQLObjectType({
-        name: "FilterSaleArtworksCounts",
-        fields: {
-          total: numeral(({ aggregations }) => aggregations.total.value),
-          followed_artists: numeral(({ aggregations }) => aggregations.followed_artists.value),
-        },
-      }),
-      resolve: artist => artist,
-    },
+    aggregations: SaleArtworkAggregations,
+    counts: SaleArtworkCounts,
     hits: {
       description: "Sale Artwork results.",
       type: new GraphQLList(SaleArtwork.type),
-    },
-    sale_artworks_connection: {
-      type: connectionDefinitions({
-        name: "FilterSaleArtworks",
-        nodeType: SaleArtwork.type,
-      }).connectionType,
-      args: pageable(filterSaleArtworksArgs),
-      resolve: async ({ aggregations }, options, _request, { rootValue: { accessToken } }) => {
-        if (!aggregations || !aggregations.total) {
-          throw new Error("This query must contain the total aggregation")
-        }
-
-        const relayOptions = parseRelayOptions(options)
-        const { hits } = await gravity.with(accessToken)("filter/sale_artworks", relayOptions)
-        return connectionFromArraySlice(hits, options, {
-          arrayLength: aggregations.total.value,
-          sliceStart: relayOptions.offset,
-        })
-      },
     },
   }),
 })
@@ -94,6 +80,7 @@ export const FilterSaleArtworksType = new GraphQLObjectType({
 const FilterSaleArtworks = {
   type: FilterSaleArtworksType,
   description: "Sale Artworks Elastic Search results",
+  deprecationReason: "This type has been superceded by `sale_artworks`",
   args: filterSaleArtworksArgs,
   resolve: (root, options, request, { rootValue: { accessToken } }) => {
     return gravity.with(accessToken)("filter/sale_artworks", options)
---
+++ schema/sale_artworks.js
@@ -0,0 +1,42 @@
+import gravity from "lib/loaders/legacy/gravity"
+import SaleArtwork from "./sale_artwork"
+import { parseRelayOptions } from "lib/helpers"
+import { pageable } from "relay-cursor-paging"
+import { connectionDefinitions, connectionFromArraySlice } from "graphql-relay"
+import { filterSaleArtworksArgs } from "schema/filter_sale_artworks"
+import { SaleArtworkAggregations, SaleArtworkCounts } from "schema/filter_sale_artworks"
+
+const DEFAULTS = {
+  aggregations: ["total"],
+  page: 1,
+  size: 10,
+  offset: 0,
+}
+
+const SaleArtworksType = connectionDefinitions({
+  name: "SaleArtworks",
+  nodeType: SaleArtwork.type,
+  connectionFields: {
+    aggregations: SaleArtworkAggregations,
+    counts: SaleArtworkCounts,
+  },
+}).connectionType
+
+export default {
+  args: pageable(filterSaleArtworksArgs),
+  description: "Sale Artworks Elastic Search results",
+  type: SaleArtworksType,
+  resolve: async (_root, options, _request, { rootValue: { accessToken } }) => {
+    const params = parseRelayOptions({ ...DEFAULTS, ...options })
+    const response = await gravity.with(accessToken)("filter/sale_artworks", params)
+    const data = {
+      ...response,
+      ...connectionFromArraySlice(response.hits, options, {
+        arrayLength: response.aggregations.total.value,
+        sliceStart: params.offset,
+      }),
+    }
+
+    return data
+  },
+}
