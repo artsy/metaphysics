55428826951296e725c3fab5d29cf9d9cb1429cb
Christopher Pappas <damassi.pappas@gmail.com>
2017-11-22 14:08:43 -0800

    [Loader] Simplify auth/unauth between popular and sale_artworks

--- schema/artists/__tests__/popular.test.js
+++ schema/artists/__tests__/popular.test.js
@@ -1,35 +1,8 @@
-import { runAuthenticatedQuery, runQuery } from "test/utils"
 import gql from "test/gql"
+import { runQuery } from "test/utils"
 
-describe("when logged in", () => {
-  it("makes a call for popular artists to the auth'd popularArtistsLoader", () => {
-    const query = gql`
-      {
-        popular_artists {
-          artists {
-            id
-          }
-        }
-      }
-    `
-
-    const rootValue = {
-      authenticatedPopularArtistsLoader: () =>
-        Promise.resolve([
-          { birthday: "1900", artworks_count: 100, id: "ortina" },
-          { birthday: "1900", artworks_count: 100, id: "xtina" },
-        ]),
-    }
-
-    expect.assertions(1)
-    return runAuthenticatedQuery(query, rootValue).then(data => {
-      expect(data).toMatchSnapshot()
-    })
-  })
-})
-
-describe("when an anonymous user", () => {
-  it("makes a call for popular artists to the popularArtistsLoader", () => {
+describe("Popular Artists", () => {
+  it("makes a call for popular artists", () => {
     const query = gql`
       {
         popular_artists {
