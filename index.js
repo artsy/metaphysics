b17344c1512b2d8c3efb5596fbacc540a3de5061
Christopher Pappas <damassi.pappas@gmail.com>
2017-12-13 16:37:24 -0800

    Add @artsy/express-reloadable

--- index.js
+++ index.js
@@ -7,24 +7,42 @@ import express from "express"
 import forceSSL from "express-force-ssl"
 import bodyParser from "body-parser"
 import config from "./src/config"
+import { startApp } from "./src"
+import { createReloadable } from "@artsy/express-reloadable"
 import { info, error } from "./src/lib/loggers"
-import { mergeSchemas } from "./src/lib/mergeSchemas"
+
+const { GRAVITY_API_URL, GRAVITY_ID, GRAVITY_SECRET, NODE_ENV, PORT } = process.env
 
 global.Promise = Bluebird
-
-const { PORT, NODE_ENV, GRAVITY_API_URL, GRAVITY_ID, GRAVITY_SECRET, ENABLE_SCHEMA_STITCHING } = process.env
 
 const app = express()
 const port = PORT || 3000
 const isDevelopment = NODE_ENV === "development"
 const isProduction = NODE_ENV === "production"
-const enableSchemaStitching = ENABLE_SCHEMA_STITCHING === "true"
 
-function startApp() {
+xapp.on("error", err => {
+  error(err)
+  process.exit()
+})
+
+const xappConfig = {
+  url: GRAVITY_API_URL,
+  id: GRAVITY_ID,
+  secret: GRAVITY_SECRET,
+}
+
+xapp.init(xappConfig, () => {
+  config.GRAVITY_XAPP_TOKEN = xapp.token
+  bootApp() // eslint-disable-line
+})
+
+function bootApp() {
   if (isProduction) {
     app.set("forceSSLOptions", { trustXFPHeader: true }).use(forceSSL)
     app.set("trust proxy", 1)
   }
+
+  app.use(bodyParser.json())
 
   app.get("/favicon.ico", (_req, res) => {
     res
@@ -34,36 +52,14 @@ function startApp() {
   })
 
   app.all("/graphql", (_req, res) => res.redirect("/"))
-  app.use(bodyParser.json())
 
-  // add mount point here
   if (isDevelopment) {
-    app.use(require("./src"))
+    const mountAndReload = createReloadable(app, require)
+    mountAndReload("./src")
+    startApp()
   } else {
-    app.use(require("./src"))
+    app.use(require("./src").default)
   }
 
   app.listen(port, () => info(`Listening on ${port}`))
 }
-
-xapp.on("error", err => {
-  error(err)
-  process.exit()
-})
-
-xapp.init(
-  {
-    url: GRAVITY_API_URL,
-    id: GRAVITY_ID,
-    secret: GRAVITY_SECRET,
-  },
-  () => {
-    config.GRAVITY_XAPP_TOKEN = xapp.token
-    if (enableSchemaStitching) {
-      console.warn("[FEATURE] Enabling schema stitching") // eslint-disable-line
-      mergeSchemas().then(startApp)
-    } else {
-      startApp()
-    }
-  }
-)
--- src/index.js
+++ src/index.js
@@ -5,74 +5,88 @@ import express from "express"
 import graphqlErrorHandler from "./lib/graphql-error-handler"
 import graphqlHTTP from "express-graphql"
 import legacyLoaders from "./lib/loaders/legacy"
+import localSchema from "./schema"
 import moment from "moment"
 import morgan from "artsy-morgan"
-import schema from "./schema"
 import { fetchLoggerSetup, fetchLoggerRequestDone } from "lib/loaders/api/logger"
 import { info } from "./lib/loggers"
+import { mergeSchemas } from "./lib/mergeSchemas"
 import { middleware as requestIDsAdder } from "./lib/requestIDs"
 import { middleware as requestTracer, makeSchemaTraceable } from "./lib/tracer"
 
-const { ENABLE_QUERY_TRACING, NODE_ENV, QUERY_DEPTH_LIMIT } = process.env
+const { ENABLE_QUERY_TRACING, ENABLE_SCHEMA_STITCHING, NODE_ENV, QUERY_DEPTH_LIMIT } = process.env
 const isProduction = NODE_ENV === "production"
 const queryLimit = (QUERY_DEPTH_LIMIT && parseInt(QUERY_DEPTH_LIMIT, 10)) || 10 // Default to ten.
+const enableSchemaStitching = ENABLE_SCHEMA_STITCHING === "true"
 const enableQueryTracing = ENABLE_QUERY_TRACING === "true"
 
 const app = express()
-module.exports = app
+export default app
 
-if (enableQueryTracing) {
-  console.warn("[FEATURE] Enabling query tracing") // eslint-disable-line
-  makeSchemaTraceable(schema)
-  app.use(requestTracer)
+export async function startApp() {
+  let schema = localSchema
+
+  if (enableSchemaStitching) {
+    try {
+      schema = await mergeSchemas()
+    } catch (error) {
+      console.log("Error merging schemas:", error) // eslint-disable-line
+    }
+  }
+
+  if (enableQueryTracing) {
+    console.warn("[FEATURE] Enabling query tracing") // eslint-disable-line
+    makeSchemaTraceable(schema)
+    app.use(requestTracer)
+  }
+
+  app.use(requestIDsAdder)
+
+  app.use(
+    "/",
+    cors(),
+    morgan,
+    graphqlHTTP((req, res) => {
+      info("----------")
+
+      legacyLoaders.clearAll()
+
+      const accessToken = req.headers["x-access-token"]
+      const userID = req.headers["x-user-id"]
+      const timezone = req.headers["x-timezone"]
+
+      const { requestIDs, span } = res.locals
+      const requestID = requestIDs.requestID
+
+      if (!isProduction) {
+        fetchLoggerSetup(requestID)
+      }
+
+      // Accepts a tz database timezone string. See http://www.iana.org/time-zones,
+      // https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+      let defaultTimezone
+      if (moment.tz.zone(timezone)) {
+        defaultTimezone = timezone
+      }
+
+      const loaders = createLoaders(accessToken, userID, requestID)
+      // Share with e.g. the Convection ApolloLink in mergedSchema.
+      res.locals.dataLoaders = loaders // eslint-disable-line no-param-reassign
+
+      return {
+        schema,
+        graphiql: true,
+        rootValue: {
+          accessToken,
+          userID,
+          defaultTimezone,
+          span,
+          ...createLoaders(accessToken, userID, requestIDs),
+        },
+        formatError: graphqlErrorHandler(req.body),
+        validationRules: [depthLimit(queryLimit)],
+        extensions: isProduction ? undefined : fetchLoggerRequestDone(requestID),
+      }
+    })
+  )
 }
-
-app.use(requestIDsAdder)
-
-app.use(
-  "/",
-  cors(),
-  morgan,
-  graphqlHTTP((req, res) => {
-    info("----------")
-
-    legacyLoaders.clearAll()
-
-    const accessToken = req.headers["x-access-token"]
-    const userID = req.headers["x-user-id"]
-    const timezone = req.headers["x-timezone"]
-
-    const { requestIDs, span } = res.locals
-    const requestID = requestIDs.requestID
-
-    if (!isProduction) {
-      fetchLoggerSetup(requestID)
-    }
-
-    // Accepts a tz database timezone string. See http://www.iana.org/time-zones,
-    // https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
-    let defaultTimezone
-    if (moment.tz.zone(timezone)) {
-      defaultTimezone = timezone
-    }
-
-    const loaders = createLoaders(accessToken, userID, requestID)
-    // Share with e.g. the Convection ApolloLink in mergedSchema.
-    res.locals.dataLoaders = loaders // eslint-disable-line no-param-reassign
-
-    return {
-      schema,
-      graphiql: true,
-      rootValue: {
-        accessToken,
-        userID,
-        defaultTimezone,
-        span,
-        ...createLoaders(accessToken, userID, requestIDs),
-      },
-      formatError: graphqlErrorHandler(req.body),
-      validationRules: [depthLimit(queryLimit)],
-      extensions: isProduction ? undefined : fetchLoggerRequestDone(requestID),
-    }
-  })
-)
