13d457899121ea1478d3452132e5a54b10e758af
Eve Essex <eve.essex@gmail.com>
2018-02-23 14:35:00 -0500

    can return artists by array of ids

--- src/schema/__tests__/artists.test.js
+++ src/schema/__tests__/artists.test.js
@@ -26,4 +26,27 @@ describe("Artists", () => {
     const { artists } = await runQuery(query, { artistsLoader })
     expect(artists).toEqual([{ name: "Han Myung-Ok" }])
   })
+
+  it("returns a list of artists matching array of ids", async () => {
+    const artistsLoader = ({ ids }) => {
+      if (ids) {
+        return Promise.resolve([
+          {
+            _id: "52c721e5b202a3edf1000072",
+            name: "Han Myung-Ok",
+          },
+        ])
+      }
+      throw new Error("Unexpected invocation")
+    }
+    const query = gql`
+      {
+        artists(ids: ["52c721e5b202a3edf1000072"]) {
+          _id
+        }
+      }
+    `
+    const { artists } = await runQuery(query, { artistsLoader })
+    expect(artists[0]._id).toEqual("52c721e5b202a3edf1000072")
+  })
 })
