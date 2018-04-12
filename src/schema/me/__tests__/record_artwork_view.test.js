8ddeced90a34dc2d89cfc0f134057a1f22091c1e
Matthew Zikherman <matt@bestvendor.com>
2018-04-12 13:48:22 -0400

    Add spec for using Gravity schema

---
+++ src/schema/me/__tests__/record_artwork_view.test.js
@@ -0,0 +1,42 @@
+import { graphql } from "graphql"
+import gql from "test/gql"
+
+import { makeExecutableSchema } from "graphql-tools"
+import fs from "fs"
+import path from "path"
+
+describe("Recording artwork views", () => {
+  const mutation = gql`
+    mutation {
+      recordArtworkView(
+        input: { artwork_id: "artwork-id", clientMutationId: "2" }
+      ) {
+        clientMutationId
+        artwork_id
+      }
+    }
+  `
+
+  const typeDefs = fs.readFileSync(
+    path.resolve(__dirname, "../../../data/gravity.graphql"),
+    "utf8"
+  )
+
+  const resolvers = {
+    Mutation: {
+      recordArtworkView: () => {
+        return { artwork_id: "artwork-id", clientMutationId: "2" }
+      },
+    },
+  }
+
+  const gravitySchema = makeExecutableSchema({ typeDefs, resolvers })
+
+  it("records an artwork view", () => {
+    return graphql(gravitySchema, mutation, null, null).then(({ data }) => {
+      expect(data).toEqual({
+        recordArtworkView: { artwork_id: "artwork-id", clientMutationId: "2" },
+      })
+    })
+  })
+})
