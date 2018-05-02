63bb47904de5f49d8d99bbf2ed5325e4a1f2692d
sepans <sepans@sepans.com>
2018-05-02 17:15:52 -0400

    schema change, cleanup

--- src/schema/me/__tests__/bidder_position_mutation.test.js
+++ src/schema/me/__tests__/bidder_position_mutation.test.js
@@ -1,33 +1,122 @@
 import { runAuthenticatedQuery } from "test/utils"
 
-describe("Bidder position mutation", () => {
-  const createBidderPosition = {
-      suggested_next_bid_cents: 110000,
-  }
-
-  const query = `
+const query = `
   mutation {
     createBidderPosition(input: {
       artwork_id: "daryl-daniels-free-thyself-1"
       max_bid_amount_cents: 100000
       sale_id: "one-year-of-resistance-benefit-auction-2018"
     }) {
-      position {
-        suggested_next_bid_cents
+      result {
+        status
+        position {
+          suggested_next_bid_cents
+        }
+        message_header
+        message_description
       }
     }
   }
   `
 
-  const rootValue = {
-    createBidderPositionLoader: sinon.stub().returns(
-      Promise.resolve(createBidderPosition)
-    ),
+const errorMEssageTemplate = `{ Error: https://stagingapi.artsy.net/api/v1/me/bidder_position?
+        artwork_id=mohammed-qasim-ashfaq-surrealism&max_bid_amount_cents=3700000&
+        sale_id=bidding-test - `
+
+describe("Bidder position mutation", () => {
+  const createBidderPosition = {
+    suggested_next_bid_cents: 110000,
   }
 
-  it("creates a bidder position", async () => {
-    return runAuthenticatedQuery(query, rootValue).then(data => {
-      expect(data.createBidderPosition.position.suggested_next_bid_cents).toEqual(110000)
+  describe("success", () => {
+    it("creates a bidder position", async () => {
+      const rootValue = {
+        createBidderPositionLoader: sinon.stub().returns(
+          Promise.resolve(createBidderPosition)
+        ),
+      }
+      return runAuthenticatedQuery(query, rootValue).then(data => {
+        expect(data.createBidderPosition.result.position.suggested_next_bid_cents).toEqual(110000)
+        expect(data.createBidderPosition.result.message_header).toBeNull()
+        expect(data.createBidderPosition.result.message_description).toBeNull()
+      })
+    })
+  })
+
+  describe("error", () => {
+    it("creates correct message when bid is not high enough", async () => {
+      const errorObjectString =
+        `{"type": "param_error", "message":"Please enter a bid higher than $37,000.","detail"\
+:{"base":["Please enter a bid higher than $37,000"]}}`
+      const errorMessage = {
+        message: errorMEssageTemplate + errorObjectString,
+      }
+      const rootValue = {
+        createBidderPositionLoader: sinon.stub().returns(
+          Promise.reject(errorMessage)
+        ),
+      }
+      return runAuthenticatedQuery(query, rootValue).then(data => {
+        expect(data.createBidderPosition.result.position).toBeNull()
+        expect(data.createBidderPosition.result.message_header).toEqual("Your bid wasn't high enough")
+        expect(data.createBidderPosition.result.message_description).toEqual(`Another bidder placed a\
+ higher max bid or the same max bid before you did. Bid again to take the lead.`)
+      })
+    })
+
+    it("creates correct message when sale is closed", async () => {
+      const errorObjectString = `{"type": "param_error", "message":"Sale Closed to Bids"}`
+      const errorMessage = {
+        message: errorMEssageTemplate + errorObjectString,
+      }
+      const rootValue = {
+        createBidderPositionLoader: sinon.stub().returns(
+          Promise.reject(errorMessage)
+        ),
+      }
+      return runAuthenticatedQuery(query, rootValue).then(data => {
+        expect(data.createBidderPosition.result.position).toBeNull()
+        expect(data.createBidderPosition.result.message_header).toEqual("Lot closed")
+        expect(data.createBidderPosition.result.message_description).toEqual(
+          "Sorry, your bid wasn’t received before the lot closed.")
+      })
+    })
+    it("creates correct message when live bidding has started", async () => {
+      const errorObjectString = `{"type": "param_error", "message":"Live Bidding has Started"}`
+      const errorMessage = {
+        message: errorMEssageTemplate + errorObjectString,
+      }
+      const rootValue = {
+        createBidderPositionLoader: sinon.stub().returns(
+          Promise.reject(errorMessage)
+        ),
+      }
+      return runAuthenticatedQuery(query, rootValue).then(data => {
+        expect(data.createBidderPosition.result.position).toBeNull()
+        expect(data.createBidderPosition.result.message_header).toEqual("Live bidding has started")
+        expect(data.createBidderPosition.result.message_description).toEqual(
+          `Sorry, your bid wasn’t received before live bidding started.\
+ To continue bidding, please join the live auction.`)
+      })
+    })
+
+    it("creates correct message when bidder is not qualifdied", async () => {
+      const errorObjectString = `{"type": "param_error", "message":"Bidder not qualified to bid on this auction."}`
+      const errorMessage = {
+        message: errorMEssageTemplate + errorObjectString,
+      }
+      const rootValue = {
+        createBidderPositionLoader: sinon.stub().returns(
+          Promise.reject(errorMessage)
+        ),
+      }
+      return runAuthenticatedQuery(query, rootValue).then(data => {
+        expect(data.createBidderPosition.result.position).toBeNull()
+        expect(data.createBidderPosition.result.message_header).toEqual("Bid not placed")
+        expect(data.createBidderPosition.result.message_description).toEqual(
+          `Your bid can’t be placed at this time.\
+ Please contact support@artsy.net for more information.`)
+      })
     })
   })
 })
--- src/schema/me/bidder_position_mutation.js
+++ src/schema/me/bidder_position_mutation.js
@@ -1,6 +1,6 @@
 // @ts-check
 
-import { GraphQLString, GraphQLFloat, GraphQLNonNull } from "graphql"
+import { GraphQLString, GraphQLFloat, GraphQLObjectType, GraphQLNonNull } from "graphql"
 import { mutationWithClientMutationId } from "graphql-relay"
 
 import BidderPosition from "schema/bidder_position"
@@ -10,8 +10,8 @@ const biddingErrors = [
     id: "ERROR_BID_LOW",
     gravity_key: "Please enter a bid higher than",
     header: "Your bid wasn't high enough",
-    description: `Another bidder placed a higher max bid or the same max bid before you did.
-     Bid again to take the lead.`,
+    description: `Another bidder placed a higher max bid or the same max bid before you did.\
+ Bid again to take the lead.`,
   },
   {
     id: "ERROR_SALE_CLOSED",
@@ -20,38 +20,51 @@ const biddingErrors = [
     description: "Sorry, your bid wasn’t received before the lot closed.",
   },
   {
-    id: "ERROR_CONNECTION",
-    greavity_key: "Please check your network connectivity and try again",
-    header: "An error occurred",
-    description: "Your bid couldn’t be placed. Please check your internet connection and try again.",
-  },
-  {
     id: "ERROR_LIVE_BIDDING_STARTED",
     gravity_key: "Live Bidding has Started",
     header: "Live bidding has started",
     // TODO link
-    description: `Sorry, your bid wasn’t received before live bidding started.
-     To continue bidding, please join the live auction.`,
+    description: `Sorry, your bid wasn’t received before live bidding started.\
+ To continue bidding, please join the live auction.`,
   },
   {
     id: "ERROR_BIDDER_NOT_QUALIFIED",
-    greavity_key: "Bidder not qualified to bid on this auction.",
+    gravity_key: "Bidder not qualified to bid on this auction.",
     header: "Bid not placed",
     // TODO link
-    description: `Your bid can’t be placed at this time.
-      Please contact support@artsy.net for more information.`,
+    description: `Your bid can’t be placed at this time.\
+ Please contact support@artsy.net for more information.`,
   },
   {
     id: "ERROR_UNKNOWN",
-    greavity_key: "unknown error",
+    gravity_key: "unknown error",
     header: "Bid not placed",
     // TODO link
-    description: `Your bid can’t be placed at this time.
-      Please contact support@artsy.net for more information.`,
+    description: `Your bid can’t be placed at this time.\
+ Please contact support@artsy.net for more information.`,
 
   },
 ]
 
+const BidderPositionMutationResultType = new GraphQLObjectType({
+  name: "BidderPositionMutationResult",
+  fields: () => ({
+    status: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+    message_header: {
+      type: GraphQLString,
+    },
+    message_description: {
+      type: GraphQLString,
+    },
+    position: {
+      type: BidderPosition.type,
+    },
+  }),
+})
+
+// @ts-ignore
 export const BidderPositionMutation = mutationWithClientMutationId({
   name: "BidderPosition",
   description:
@@ -68,9 +81,9 @@ export const BidderPositionMutation = mutationWithClientMutationId({
     },
   },
   outputFields: {
-    position: {
-      type: BidderPosition.type,
-      resolve: (position) => position,
+    result: {
+      type: BidderPositionMutationResultType,
+      resolve: (result) => result,
     },
   },
   mutateAndGetPayload: (
@@ -87,14 +100,15 @@ export const BidderPositionMutation = mutationWithClientMutationId({
       return new Error("You need to be signed in to perform this action")
     }
     return createBidderPositionLoader({ sale_id, artwork_id, max_bid_amount_cents })
-      .catch((e) => {
+      .then(p => { return { status: "SUCCESS", position: p } })
+      .catch(e => {
         const errorSplit = e.message.split(" - ")
         const errorObject = errorSplit.length > 1 ? JSON.parse(e.message.split(" - ")[1]) : null
         if (errorObject && errorObject.type === "param_error") {
-          const error = biddingErrors.find(d => errorObject.message.startsWith(d.gravity_key)) ||
+          const error = biddingErrors.find(d => errorObject.message.trim().startsWith(d.gravity_key)) ||
             errorObject.ERROR_UNKNOWN
           return {
-            id: error.id,
+            status: error.id,
             message_header: error.header,
             message_description: error.description,
 
