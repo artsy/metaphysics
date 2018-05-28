fdfe13cd07a2613ec06fe4f9ee7067406829431b
Zephraph <zephraph@gmail.com>
2018-05-28 15:06:07 -0400

    Add header to response if payload crunched

--- src/lib/crunchInterceptor.js
+++ src/lib/crunchInterceptor.js
@@ -1,13 +1,14 @@
 import { crunch } from "graphql-crunch"
 import interceptor from "express-interceptor"
 
-export const interceptorCallback = req => ({
+export const interceptorCallback = (req, res) => ({
   isInterceptable: () =>
     req.query.hasOwnProperty("crunch") || req.headers["x-crunch"],
   intercept: (body, send) => {
     body = JSON.parse(body) // eslint-disable-line no-param-reassign
     if (body) {
       body = crunch(body) // eslint-disable-line no-param-reassign
+      res.set("X-Crunch", "true")
     }
     send(JSON.stringify(body))
   },
