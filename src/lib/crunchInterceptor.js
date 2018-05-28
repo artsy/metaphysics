d98c095500d943fd095cd5206ee4b130e0f3c7e6
Zephraph <zephraph@gmail.com>
2018-05-28 13:14:17 -0400

    Crunch entire payload instead of only data

--- src/lib/crunchInterceptor.js
+++ src/lib/crunchInterceptor.js
@@ -6,8 +6,8 @@ export const interceptorCallback = req => ({
     req.query.hasOwnProperty("crunch") || req.headers["x-crunch"],
   intercept: (body, send) => {
     body = JSON.parse(body) // eslint-disable-line no-param-reassign
-    if (body && body.data) {
-      body.data = crunch(body.data) // eslint-disable-line no-param-reassign
+    if (body) {
+      body = crunch(body) // eslint-disable-line no-param-reassign
     }
     send(JSON.stringify(body))
   },
