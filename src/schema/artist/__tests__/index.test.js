124663fb2f7c43a11a1d72e52b46d5602893a1b6
Matthew Zikherman <matt@bestvendor.com>
2018-06-07 12:00:44 -0400

    Allow artwork connections to return a `totalPages`, and also take in conventional pagination args

--- src/schema/artist/__tests__/index.test.js
+++ src/schema/artist/__tests__/index.test.js
@@ -749,7 +749,7 @@ describe("Artist type", () => {
               artists: ["percy"],
             },
           ],
-          aggregations: [],
+          aggregations: { total: { value: 74 } },
         })
       )
       rootValue.filterArtworksLoader = filterArtworksLoader
@@ -758,8 +758,13 @@ describe("Artist type", () => {
         {
           artist(id: "percy") {
             filtered_artworks(aggregations:[TOTAL], partner_id: null){
-              hits {
-                id
+              artworks: artworks_connection(page: 1, size: 10) {
+                totalPages
+                edges {
+                  node {
+                    id
+                  }
+                }
               }
             }
           }
@@ -767,11 +772,14 @@ describe("Artist type", () => {
       `
 
       return runQuery(query, rootValue).then(
-        ({ artist: { filtered_artworks: { hits } } }) => {
+        ({
+          artist: { filtered_artworks: { artworks: { totalPages, edges } } },
+        }) => {
           expect(filterArtworksLoader.mock.calls[0][0]).not.toHaveProperty(
             "partner_id"
           )
-          expect(hits).toEqual([{ id: "im-a-cat" }])
+          expect(edges).toEqual([{ node: { id: "im-a-cat" } }])
+          expect(totalPages).toEqual(8)
         }
       )
     })
