877888da8f318f2c7d73e5e8209b33e56bf6ff8f
Isac Petruzzi <isac.petruzzi@gmail.com>
2018-06-18 13:23:58 +0200

    push event loop latency metrics to datadog / statsd

--- src/lib/stats.js
+++ src/lib/stats.js
@@ -1,15 +1,18 @@
 import StatsD from "hot-shots"
+import appmetrics from "appmetrics"
 
 import config from "config"
 import { error } from "./loggers"
 
 const {
   NODE_ENV,
+  ENABLE_METRICS,
   STATSD_HOST,
   STATSD_PORT,
 } = config
 
 const isTest = NODE_ENV === "test"
+const enableMetrics = ENABLE_METRICS === "true"
 
 export const statsClient = new StatsD({
   host: STATSD_HOST,
@@ -20,3 +23,17 @@ export const statsClient = new StatsD({
     error(`Statsd client error ${err}`);
   }
 })
+
+if (enableMetrics && !isTest) {
+  appmetrics.configure({
+    mqtt: 'off'
+  })
+  const monitoring = appmetrics.monitor()
+
+  monitoring.on('eventloop', (eventloopMetrics) => {
+    console.log(eventloopMetrics)
+    statsClient.timing('eventloop.latency.min', eventloopMetrics.latency.min)
+    statsClient.timing('eventloop.latency.max', eventloopMetrics.latency.max)
+    statsClient.timing('eventloop.latency.avg', eventloopMetrics.latency.avg)
+  })
+}
