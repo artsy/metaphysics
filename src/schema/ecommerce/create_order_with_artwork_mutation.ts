1eb8c9cad63c6471919e9afd53aed8f532431169
Orta Therox <orta@artsymail.com>
2018-08-20 17:30:57 -0400

    Changes the mutation method for creating an order to be based on the enw mutation response types
    Signed-off-by: Ashkan Nasseri <ashkan@artsymail.com>

---
+++ src/schema/ecommerce/create_order_with_artwork_mutation.ts
@@ -0,0 +1,125 @@
+import {
+  GraphQLInputObjectType,
+  GraphQLNonNull,
+  GraphQLInt,
+  GraphQLString,
+  graphql,
+} from "graphql"
+
+import { mutationWithClientMutationId } from "graphql-relay"
+import { OrderOrFailureUnionType } from "schema/ecommerce/types/order_or_error_union"
+import gql from "test/gql"
+
+const CreateOrderInputType = new GraphQLInputObjectType({
+  name: "CreateOrderInput",
+  fields: {
+    artworkId: {
+      type: new GraphQLNonNull(GraphQLString),
+      description: "BSON ID of artwork",
+    },
+    editionSetId: {
+      type: GraphQLString,
+      description: "ID of artwork's edition set",
+    },
+    quantity: {
+      type: GraphQLInt,
+      description: "quantity of artwork",
+    },
+  },
+})
+
+export const CreateOrderWithArtworkMutation = mutationWithClientMutationId({
+  name: "CreateOrderWithArtwork",
+  description: "Creates an order with an artwork",
+  inputFields: CreateOrderInputType.getFields(),
+  outputFields: {
+    orderOrError: {
+      type: OrderOrFailureUnionType,
+    },
+  },
+  mutateAndGetPayload: (
+    { artworkId, editionSetId, quantity },
+    context,
+    { rootValue: { accessToken, exchangeSchema } }
+  ) => {
+    if (!accessToken) {
+      return new Error("You need to be signed in to perform this action")
+    }
+
+    const mutation = gql`
+      mutation createOrderWithArtwork(
+        $artworkId: String!
+        $editionSetId: String
+        $quantity: Int
+      ) {
+        ecommerce_createOrderWithArtwork(
+          input: {
+            artworkId: $artworkId
+            editionSetId: $editionSetId
+            quantity: $quantity
+          }
+        ) {
+          orderOrError {
+            ... on EcommerceOrderWithMutationSuccess {
+              order {
+                id
+                buyerTotalCents
+                code
+                commissionFeeCents
+                createdAt
+                currencyCode
+                fulfillmentType
+                itemsTotalCents
+                partnerId
+                sellerTotalCents
+                shippingAddressLine1
+                shippingAddressLine2
+                shippingCity
+                shippingCountry
+                shippingName
+                shippingPostalCode
+                shippingRegion
+                shippingTotalCents
+                state
+                stateExpiresAt
+                stateUpdatedAt
+                taxTotalCents
+                transactionFeeCents
+                updatedAt
+                userId
+                lineItems {
+                  edges {
+                    node {
+                      id
+                      priceCents
+                      artworkId
+                      editionSetId
+                      quantity
+                    }
+                  }
+                }
+              }
+            }
+            ... on EcommerceOrderWithMutationFailure {
+              error {
+                description
+              }
+            }
+          }
+        }
+      }
+    `
+
+    return (
+      graphql(exchangeSchema, mutation, null, context, {
+        artworkId,
+        editionSetId,
+        quantity,
+      })
+        // Because the error types are represented in the type system we
+        // can always assume that data is being used. If the call to Exchange
+        // fails then the error would have stopped execution before here
+        .then(result => result.data!.ecommerce_createOrderWithArtwork)
+    )
+  },
+})
