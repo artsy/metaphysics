de55fbf0f78e426805e955dbc3b33961f8124adb
Zephraph <zephraph@gmail.com>
2018-09-28 17:49:37 -0400

    Add support for dumping schema as json

--- scripts/dump-schema.ts
+++ scripts/dump-schema.ts
@@ -3,9 +3,10 @@ import { printSchema } from "graphql/utilities"
 import path from "path"
 import schema from "schema"
 import prettier from "prettier"
+import { graphql, introspectionQuery } from "graphql"
 
 const message =
-  "Usage: dump-schema.js /path/to/output/directory or /path/to/filename.graphql"
+  "Usage: dump-schema.js /path/to/output/directory or /path/to/filename.graphql or /path/to/schema.json"
 
 const destination = process.argv[2]
 if (destination === undefined) {
@@ -19,9 +20,25 @@ const schemaPath =
     ? path.join(destination, "schema.graphql")
     : destination
 
-// commentDescriptions means it uses # instead of the ugly """
-const schemaText = printSchema(schema, { commentDescriptions: true })
-const prettySchema = prettier.format(schemaText, { parser: "graphql" })
+if (schemaPath.endsWith("json")) {
+  console.log(`Dumping JSON to ${schemaPath}`)
+  graphql(schema, introspectionQuery).then(
+    result => {
+      fs.writeFileSync(path.join(schemaPath), JSON.stringify(result, null, 2))
+    },
+    error => {
+      console.error(
+        "ERROR introspecting schema: ",
+        JSON.stringify(error, null, 2)
+      )
+    }
+  )
+} else {
+  console.log(`Dumping SDL to ${schemaPath}`)
+  // commentDescriptions means it uses # instead of the ugly """
+  const schemaText = printSchema(schema, { commentDescriptions: true })
+  const prettySchema = prettier.format(schemaText, { parser: "graphql" })
 
-// Save user readable type system shorthand of schema
-fs.writeFileSync(schemaPath, prettySchema, "utf8")
+  // Save user readable type system shorthand of schema
+  fs.writeFileSync(schemaPath, prettySchema, "utf8")
+}
