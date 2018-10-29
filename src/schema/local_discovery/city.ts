fbd3930c0dc14fc5047459b80d9473d6f0a2cfd9
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2018-10-29 12:07:20 -0400

    Add a resolver for local shows

--- src/schema/local_discovery/city.ts
+++ src/schema/local_discovery/city.ts
@@ -1,11 +1,19 @@
 import {
-  GraphQLString,
   GraphQLFloat,
-  GraphQLObjectType,
+  GraphQLInt,
+  GraphQLList,
   GraphQLNonNull,
+  GraphQLObjectType,
+  GraphQLString,
 } from "graphql"
 
+import Show from "schema/show"
+import EventStatus from "schema/input_fields/event_status"
+import PartnerShowSorts from "schema/sorts/partner_show_sorts"
+
 import cityData from "./city_data.json"
+
+const LOCAL_DISCOVERY_RADIUS_KM = 75
 
 const LatLngType = new GraphQLObjectType({
   name: "LatLng",
@@ -44,6 +52,33 @@ const LocalDiscoveryCityType = new GraphQLObjectType({
       type: LatLngType,
       resolve: obj => {
         return obj.coordinates
+      },
+    },
+    shows: {
+      type: new GraphQLList(Show.type),
+      args: {
+        size: { type: GraphQLInt },
+        sort: PartnerShowSorts,
+        status: EventStatus,
+      },
+      resolve: (obj, args, _context, { rootValue: { showsLoader } }) => {
+        let gravityOptions = {
+          ...args,
+          displayable: true,
+          near: `${obj.coordinates.lat},${obj.coordinates.lng}`,
+          max_distance: LOCAL_DISCOVERY_RADIUS_KM,
+        }
+
+        // here is where we could fetch non-partner shows and
+        // merge them in with partner shows.
+        //
+        // but how to deal with sorting across two disparate
+        // collections?
+        //
+        // then again, if we model non-partner shows akin to
+        // current reference shows, this problem may be solvable
+
+        return showsLoader(gravityOptions)
       },
     },
   },
