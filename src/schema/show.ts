261c88b5b19c87a9b68864cfa1cd68626dd2853c
Orta Therox <orta.therox@gmail.com>
2018-11-05 19:09:56 -0500

    Add descriptions to all the show fields

--- src/schema/show.ts
+++ src/schema/show.ts
@@ -88,10 +88,13 @@ export const ShowType = new GraphQLObjectType({
     ...GravityIDFields,
     cached,
     artists: {
+      description: "The Artists presenting in this show",
       type: new GraphQLList(Artist.type),
       resolve: ({ artists }) => artists,
     },
     artworks: {
+      description: "The artworks featured in this show",
+      deprecationReason: "Use artworks_connection instead",
       type: new GraphQLList(Artwork.type),
       args: artworksArgs,
       resolve: (
@@ -125,6 +128,7 @@ export const ShowType = new GraphQLObjectType({
       },
     },
     artworks_connection: {
+      description: "The artworks featured in the show",
       type: artworkConnection,
       args: pageable(artworksArgs),
       resolve: (
@@ -158,10 +162,13 @@ export const ShowType = new GraphQLObjectType({
       },
     },
     artists_without_artworks: {
+      description: "Artists inside the show who do not have artworks present",
       type: new GraphQLList(Artist.type),
       resolve: ({ artists_without_artworks }) => artists_without_artworks,
     },
     city: {
+      description:
+        "The general city, derived from a fair location, a show location or a potential city",
       type: GraphQLString,
       resolve: ({ fair, location, partner_city }) => {
         if (fair && fair.location && fair.location.city) {
@@ -174,6 +181,7 @@ export const ShowType = new GraphQLObjectType({
       },
     },
     cover_image: {
+      description: "The image you should use to represent this show",
       type: Image.type,
       resolve: (
         { id, partner, image_versions, image_url },
@@ -208,6 +216,8 @@ export const ShowType = new GraphQLObjectType({
       },
     },
     counts: {
+      description:
+        "An object that represents some of the numbers you might want to highlight",
       type: new GraphQLObjectType({
         name: "ShowCounts",
         fields: {
@@ -243,6 +253,7 @@ export const ShowType = new GraphQLObjectType({
       resolve: partner_show => partner_show,
     },
     description: {
+      description: "A description of the show",
       type: GraphQLString,
     },
     displayable: {
@@ -252,6 +263,7 @@ export const ShowType = new GraphQLObjectType({
 
     end_at: date,
     events: {
+      description: "Events from the partner that runs this show",
       type: new GraphQLList(PartnerShowEventType),
       resolve: (
         { partner, id },
@@ -270,10 +282,12 @@ export const ShowType = new GraphQLObjectType({
       resolve: ({ start_at, end_at }) => exhibitionPeriod(start_at, end_at),
     },
     fair: {
+      description: "If the show is in a Fair, then that fair",
       type: Fair.type,
       resolve: ({ fair }) => fair,
     },
     href: {
+      description: "A path to the show on Artsy",
       type: GraphQLString,
       resolve: ({ id, is_reference, displayable }) => {
         if (is_reference || !displayable) return null
@@ -281,6 +295,8 @@ export const ShowType = new GraphQLObjectType({
       },
     },
     images: {
+      description:
+        "Images that represent the show, you may be interested in meta_image or cover_image for a definitive thumbnail",
       type: new GraphQLList(Image.type),
       args: {
         size: {
@@ -322,18 +338,22 @@ export const ShowType = new GraphQLObjectType({
       },
     },
     is_displayable: {
+      description: "Is this something we can display to the front-end?",
       type: GraphQLBoolean,
       resolve: ({ displayable }) => displayable,
     },
     is_fair_booth: {
+      description: "Does the show exist as a fair booth?",
       type: GraphQLBoolean,
       resolve: ({ fair }) => isExisty(fair),
     },
     is_reference: {
+      description: "Is it a stubbed show?",
       type: GraphQLBoolean,
       resolve: ({ is_reference }) => is_reference,
     },
     kind: {
+      description: "Whether the show is in a fair, group or solo",
       type: GraphQLString,
       resolve: (
         show,
@@ -349,10 +369,13 @@ export const ShowType = new GraphQLObjectType({
       },
     },
     location: {
+      description: "Where the show is located (Could also be a fair location)",
       type: Location.type,
       resolve: ({ location, fair_location }) => location || fair_location,
     },
     meta_image: {
+      description:
+        "An image representing the show, or a sharable image from an artwork in the show",
       type: Image.type,
       resolve: (
         { id, partner, image_versions, image_url },
@@ -391,6 +414,7 @@ export const ShowType = new GraphQLObjectType({
       resolve: ({ name }) => (isExisty(name) ? name.trim() : name),
     },
     nearbyShows: {
+      description: "Shows that are near (~75km) from this show",
       type: showConnection,
       args: pageable({
         size: {
@@ -419,6 +443,8 @@ export const ShowType = new GraphQLObjectType({
       },
     },
     partner: {
+      description:
+        "The partner that represents this show, could be a non-Artsy partner",
       type: new GraphQLUnionType({
         name: "PartnerTypes",
         types: [Partner.type, ExternalPartner.type],
@@ -443,9 +469,16 @@ export const ShowType = new GraphQLObjectType({
         }
       },
     },
-    press_release: markdown(),
-    start_at: date,
+    press_release: {
+      description: "The press release for this show",
+      ...markdown(),
+    },
+    start_at: {
+      description: "When this show starts",
+      ...date,
+    },
     status: {
+      description: "Is this show running, upcoming or closed?",
       type: GraphQLString,
     },
     status_update: {
@@ -461,6 +494,7 @@ export const ShowType = new GraphQLObjectType({
         exhibitionStatus(start_at, end_at, options.max_days),
     },
     type: {
+      description: "Is it a fair booth or a show?",
       type: GraphQLString,
       resolve: ({ fair }) => (isExisty(fair) ? "Fair Booth" : "Show"),
     },
