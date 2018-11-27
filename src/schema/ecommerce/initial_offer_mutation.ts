2d6b3472ec0dfffc0db86ed9b10fe6d816273fdd
Sarah Weir <sweir27@gmail.com>
2018-11-27 09:41:38 -0500

    remove custom type-- add @types/graphql-relay to package.json

--- src/schema/ecommerce/initial_offer_mutation.ts
+++ src/schema/ecommerce/initial_offer_mutation.ts
@@ -1,15 +1,11 @@
 import { graphql } from "graphql"
-import { mutationWithClientMutationId, MutationConfig } from "graphql-relay"
+import { mutationWithClientMutationId } from "graphql-relay"
 import { OrderOrFailureUnionType } from "schema/ecommerce/types/order_or_error_union"
 import gql from "lib/gql"
 import { BuyerOrderFields } from "./query_helpers"
 import { extractEcommerceResponse } from "./extractEcommerceResponse"
 import { InitialOfferInputType } from "./types/initial_offer_input_type"
 import { moneyFieldToUnit } from "lib/moneyHelper"
-
-type MutationConfigWithDeprecation = MutationConfig & {
-  deprecationReason: string
-}
 
 export const InitialOfferMutation = mutationWithClientMutationId({
   name: "InitialOffer",
@@ -63,4 +59,4 @@ export const InitialOfferMutation = mutationWithClientMutationId({
       orderId,
     }).then(extractEcommerceResponse("ecommerceInitialOffer"))
   },
-} as MutationConfigWithDeprecation)
+})
