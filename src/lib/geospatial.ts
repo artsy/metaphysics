2af048b05e93445097e6535ddf17ab1c31278bda
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2018-12-27 13:25:39 -0500

    Add docs to geo lib functions

--- src/lib/geospatial.ts
+++ src/lib/geospatial.ts
@@ -3,10 +3,29 @@ interface LatLng {
   lng: number
 }
 
-const EARTH_RADIUS_IN_METERS = 6371e3
+const EARTH_RADIUS_IN_METERS = 6371e3 // https://en.wikipedia.org/wiki/Earth_radius#Mean_radius
 
+/**
+ * Convert angular distance from degrees to radians
+ *
+ * @param {number} degrees - decimal degrees
+ * @returns {number} radians
+ */
 const toRadians = (degrees: number): number => degrees * (Math.PI / 180)
 
+/**
+ * Calculates the haversine (spherical) distance between two geographic points.
+ * Arguments are supplied as `LatLng` objects, with coordinates specified
+ * as `lat` and `lng` properties in decimal degrees.
+ *
+ * See:
+ * https://en.wikipedia.org/wiki/Haversine_formula
+ * https://www.movable-type.co.uk/scripts/latlong.html
+ *
+ * @param {LatLng} point1 - an object with `lat` and `lng` properties
+ * @param {LatLng} point2 - an object with `lat` and `lng` properties
+ * @returns {number} Distance between point1 and point2, in meters
+ */
 const haversineDistance = (point1: LatLng, point2: LatLng): number => {
   const φ1 = toRadians(point1.lat)
   const φ2 = toRadians(point2.lat)
