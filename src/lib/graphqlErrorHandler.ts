b4b0d584d7977ae4253a9e8431ed24a4cfe2b41a
Eloy Durán <eloy.de.enige@gmail.com>
2019-01-11 14:03:54 +0100

    [graphqlErrorHandler] Include upstream HTTP status code.

--- src/lib/graphqlErrorHandler.ts
+++ src/lib/graphqlErrorHandler.ts
@@ -3,7 +3,7 @@ import { error as log } from "lib/loggers"
 import { GraphQLTimeoutError } from "lib/graphqlTimeoutMiddleware"
 import { Request } from "../../node_modules/@types/express"
 import config from "config"
-import { GraphQLError } from "graphql/error/GraphQLError"
+import { GraphQLError, GraphQLFormattedError } from "graphql/error"
 import { HTTPError } from "./HTTPError"
 
 const blacklistHttpStatuses = [401, 403, 404]
@@ -14,8 +14,19 @@ declare class CombinedError extends Error {
 }
 
 const isCombinedError = (
-  error: Error | CombinedError
-): error is CombinedError => error.hasOwnProperty("errors")
+  error?: Error | CombinedError | null
+): error is CombinedError => !!error && error.hasOwnProperty("errors")
+
+const flattenErrors = (error: GraphQLError) => {
+  const originalTopLevelError = error.originalError as
+    | null
+    | undefined
+    | Error
+    | CombinedError
+  return isCombinedError(originalTopLevelError)
+    ? originalTopLevelError.errors
+    : [error]
+}
 
 interface QueryContext {
   req: Request
@@ -63,46 +74,56 @@ const reportErrorToSentry = (
   })
 }
 
+type WriteablePartial<T> = { -readonly [P in keyof T]+?: T[P] }
+
+export const formattedGraphQLError = (
+  topLevelError: GraphQLError,
+  flattenedErrors?: ReadonlyArray<GraphQLError>
+) => {
+  const result: WriteablePartial<GraphQLFormattedError> = {
+    message: topLevelError.message,
+  }
+  if (topLevelError.locations) {
+    result.locations = topLevelError.locations
+  }
+  // Question: Should this be a flag on Sentry enabled, or on being in the production env?
+  if (config.PRODUCTION_ENV) {
+    result.path = topLevelError.path
+    result.stack = topLevelError.stack
+  }
+
+  const httpStatusCodes: number[] = []
+  ;(flattenedErrors || flattenErrors(topLevelError)).forEach(
+    e =>
+      e.originalError instanceof HTTPError &&
+      httpStatusCodes.push(e.originalError.statusCode)
+  )
+  if (httpStatusCodes.length > 0) {
+    result.extensions = { httpStatusCodes }
+  }
+
+  return result
+}
+
 export const graphqlErrorHandler = (
   enableSentry: boolean,
   queryContext: QueryContext
 ) => {
-  return (error: GraphQLError) => {
+  return (topLevelError: GraphQLError) => {
+    const flattenedErrors = flattenErrors(topLevelError)
     if (enableSentry) {
-      const originalTopLevelError = error.originalError as
-        | null
-        | undefined
-        | Error
-        | CombinedError
-
-      if (originalTopLevelError) {
-        if (isCombinedError(originalTopLevelError)) {
-          originalTopLevelError.errors.forEach(error => {
-            if (shouldReportParentError(error.originalError)) {
-              reportErrorToSentry(error, queryContext)
-            }
-          })
-        } else {
-          if (shouldReportParentError(originalTopLevelError)) {
-            reportErrorToSentry(error, queryContext)
-          }
+      flattenedErrors.forEach(e => {
+        if (shouldReportParentError(e.originalError)) {
+          reportErrorToSentry(e, queryContext)
         }
-      } else {
-        reportErrorToSentry(error, queryContext)
-      }
+      })
     } else {
       const path =
-        error.path && error.path.length > 0
-          ? ` (${JSON.stringify(error.path)})`
+        topLevelError.path && topLevelError.path.length > 0
+          ? ` (${JSON.stringify(topLevelError.path)})`
           : ""
-      log(`${error.message}${path}`)
+      log(`${topLevelError.message}${path}`)
     }
-    return {
-      message: error.message,
-      locations: error.locations,
-      // Question: Should this be a flag on Sentry enabled, or on being in the production env?
-      path: config.PRODUCTION_ENV ? null : error.path,
-      stack: config.PRODUCTION_ENV ? null : error.stack,
-    }
+    return formattedGraphQLError(topLevelError, flattenedErrors)
   }
 }
