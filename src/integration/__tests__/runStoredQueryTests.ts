d9e87a99c030d055747c6f19dc0b32960c024575
Eloy Durán <eloy.de.enige@gmail.com>
2019-02-26 14:56:17 +0100

    [runStoredQueryTests] Update doc and skip legit failing queries.

--- src/integration/__tests__/runStoredQueryTests.ts
+++ src/integration/__tests__/runStoredQueryTests.ts
@@ -8,13 +8,16 @@ if (!accessToken && !userID) {
   Cannot run queries without a user access token and user ID. You can get one by 
   - 1. Signing in on staging.artsy.net
   - 2. Open a dev console
-  - 3. and get the values \`sd.CURRENT_USER.id\` and \`sd.CURRENT_USER.accessToken\`
+  - 3. and get the values \`copy(sd.CURRENT_USER.id)\` and \`copy(sd.CURRENT_USER.accessToken)\`
   - 4. you can add these to your .env as:
 
        USER_ID=[id]
        USER_ACCESS_TOKEN=[accessToken]
 
     You can also grab these from Reaction's .env if you have that set up.
+  - 5. Run this script like so:
+
+       $ env USER_ID=[id] USER_ACCESS_TOKEN=[accessToken] yarn test:validQueries
   `)
   process.exit(1)
 }
@@ -51,6 +54,25 @@ const variablesLookup = {
   },
 }
 
+// These are queries that should be skipped, becuase they are known to fail with
+// the current schema (and presumably for good reasons).
+const KnownToFail = [
+  "e3c3792bba0779073c8650e4dc8e9112",
+  "8abdeb580e8273e92d409e8d5084d1da",
+  "ae4881bc22e6c72481ffe52aefd8292a",
+  "671c8e35d0cc4647276acf57d707d7c8",
+  "55be0af4e47ea70f24dd6cdc9d9e3aa0",
+  "7480b5aa24d2a5de3d84024d4fb3cd37",
+  "f42a41c4bc40fb2dd574a5c31bcad0b3",
+  "82bf82155b8d7cd5b12d0659b36ef02c",
+  "6df12a44c12d6839296fff1e1c88551d",
+  "fe2da1d1b1abcd66129c15a6bce5abc7",
+  "35ac3b92824f845526fead61ce39b078",
+  "114550fc02504f0cfe5cb99ef099df28",
+  "c733b779a5e851aa0ab7d5e0154264a9",
+  "aa7c13bc9b101f2e7d8462bf6b3692ca",
+]
+
 const go = async () => {
   const map = readFileSync("src/data/complete.queryMap.json", "utf8")
   const queries = JSON.parse(map)
@@ -66,20 +88,30 @@ const go = async () => {
 
       // Mutations aren't complicated queries generally
       if (queryString.includes("mutation")) {
-        console.log(chalk.grey(`  ${name}: Skipping due to being a mutation`))
+        console.log(
+          chalk.grey(`  ${name} (${key}): Skipping due to being a mutation`)
+        )
+      } else if (KnownToFail.includes(key)) {
+        console.log(
+          chalk.grey(
+            `  ${name} (${key}): Skipping due to being a known failure with current schema`
+          )
+        )
       } else {
         const queryHasArgs = queryString.includes("Query(")
         const queryVariables = variablesLookup[name]
         if (queryHasArgs && !queryVariables) {
           const msg = chalk.grey(
-            `  ${name}: Skipping due to query args which aren't in the lookup`
+            `  ${name} (${key}): Skipping due to query args which aren't in the lookup`
           )
           console.log(msg)
           continue
         }
 
         // Start making the query
-        process.stdout.write(`   ${chalk.bold.whiteBright(name)}: calling `)
+        process.stdout.write(
+          `   ${chalk.bold.whiteBright(name)} (${key}): calling `
+        )
         const response = await request(queryString, queryVariables)
         if (!response.errors) {
           console.log(chalk.green.bold("✓"))
@@ -105,15 +137,26 @@ const request = (query: string, variables?: any) =>
       "X-ACCESS-TOKEN": accessToken,
     },
     body: JSON.stringify({ query, variables }),
-  }).then(response => {
-    if (!(response.status >= 200 && response.status < 300)) {
-      return response
-    } else {
-      const error: any = new Error(response.statusText)
-      error.response = response
-      throw error
-    }
   })
+    .catch(err => {
+      if (err.message.includes("ESOCKETTIMEDOUT")) {
+        process.stdout.write(
+          `\n${chalk.red.bold("[!]")} Timed out, retrying... `
+        )
+        return request(query, variables)
+      } else {
+        throw err
+      }
+    })
+    .then(response => {
+      if (!(response.status >= 200 && response.status < 300)) {
+        return response
+      } else {
+        const error: any = new Error(response.statusText)
+        error.response = response
+        throw error
+      }
+    })
 
 function getURL(environment) {
   switch (environment) {
