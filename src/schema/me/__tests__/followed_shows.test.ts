8a3071d6f782d044c498e74ad7989863188613d2
myk bilokonsky <mbilokonsky@gmail.com>
2019-03-21 15:00:00 -0400

    switch to await/async style asserts

--- src/schema/me/__tests__/followed_shows.test.ts
+++ src/schema/me/__tests__/followed_shows.test.ts
@@ -4,17 +4,8 @@ import gql from "lib/gql"
 import cityData from "../../../schema/city/cityDataSortedByDisplayPreference.json"
 import { LOCAL_DISCOVERY_RADIUS_KM } from "../../../schema/city/constants"
 
-const BASE_GRAVITY_ARGS = {
-  size: 10,
-  offset: 0,
-  total_count: true,
-}
-
-const stubResolver = () =>
-  Promise.resolve({
-    body: [],
-    headers: {},
-  })
+const BASE_GRAVITY_ARGS = { size: 10, offset: 0, total_count: true }
+const stubResolver = () => Promise.resolve({ body: [], headers: {} })
 
 const generate_query = (params = `(first: 10)`) =>
   gql`
@@ -40,17 +31,10 @@ describe("returns followed shows for a user", () => {
     followedShowsLoader = jest.fn(stubResolver)
   })
 
-  const expectArgs = args =>
-    followedShowsLoader.mock.calls.some(val => args === val)
-
-  it("generates a predictable URL with no parameters", done => {
+  it("generates a predictable URL with no parameters", async () => {
     const query = generate_query()
-    runAuthenticatedQuery(query, {
-      followedShowsLoader,
-    }).then(_data => {
-      expectArgs(BASE_GRAVITY_ARGS)
-      done()
-    })
+    await runAuthenticatedQuery(query, { followedShowsLoader })
+    expect(followedShowsLoader).toHaveBeenCalledWith(BASE_GRAVITY_ARGS)
   })
 
   describe("respects optional city parameter", () => {
@@ -64,40 +48,40 @@ describe("returns followed shows for a user", () => {
           }
 
     const buildTestData = city => ({
+      name: city.name,
       query: generate_query(`(first: 10, city: "${city.slug}")`),
       expectedArgs: generate_city_args(city),
     })
 
-    cityData.forEach(city => {
-      it.call(null, `Handles ${city.name}`, done => {
-        const { query, expectedArgs } = buildTestData(city)
-        runAuthenticatedQuery(query, {
-          followedShowsLoader,
-        }).then(_data => {
-          expectArgs(expectedArgs)
-          done()
-        })
+    cityData.map(buildTestData).forEach(({ name, query, expectedArgs }) => {
+      // I totally grant that this is a bit weird
+      // but the behavior under test is tightly coupled
+      // to the contents of the cityData file. If a change
+      // to that file breaks this test I want to know about it,
+      // and this will tell us.
+
+      // When Jest hits this point in the code it will treat this almost
+      // like a macro - we'll have a new test generated for each city,
+      // you'll see the test results enumerated as if we'd done this manually.
+      it.call(null, `Handles City: ${name}`, async () => {
+        await runAuthenticatedQuery(query, { followedShowsLoader })
+        expect(followedShowsLoader).toHaveBeenCalledWith(expectedArgs)
       })
     })
 
-    it("throws an error if presented with an invalid city slug", done => {
+    it("throws an error if presented with an invalid city slug", async () => {
       const { query } = buildTestData({
         slug: "invalid-city-location",
         coordinates: { lat: 0, lng: 0 },
       })
 
-      runAuthenticatedQuery(query, {
-        followedShowsLoader,
-      })
-        .then(_data => {
-          // should never get here
-          expect(true).toEqual(false)
-          done()
-        })
-        .catch(e => {
-          expect(e).toBeDefined()
-          done()
-        })
+      try {
+        await runAuthenticatedQuery(query, { followedShowsLoader })
+        // shouldn't get here - if we did then the error wasn't thrown.
+        expect(true).toBeFalsy()
+      } catch (e) {
+        expect(e).toBeDefined()
+      }
     })
   })
 })
