c3066a3bbb7f121c895644b0308c6a209ec2f01f
Lily Pacegit <lilyfpace@gmail.com>
2019-06-11 15:17:28 -0400

    added test for artwork attribution classes

--- src/schema/__tests__/artworkAttributionClasses.test.ts
+++ src/schema/__tests__/artworkAttributionClasses.test.ts
@@ -16,12 +16,12 @@ describe("ArtworkAttributionClasses type", () => {
     `
 
     return runQuery(query).then(data => {
-      expect(data.artworkAttributionClasses[0].id).toBe("unique")
-      expect(data.artworkAttributionClasses[0].name).toBe("Unique")
-      expect(data.artworkAttributionClasses[0].short_description).toBe(
+      expect(data!.artworkAttributionClasses[0].id).toBe("unique")
+      expect(data!.artworkAttributionClasses[0].name).toBe("Unique")
+      expect(data!.artworkAttributionClasses[0].short_description).toBe(
         "This is a unique work"
       )
-      expect(data.artworkAttributionClasses[0].long_description).toBe(
+      expect(data!.artworkAttributionClasses[0].long_description).toBe(
         "One of a kind piece, created by the artist."
       )
     })
