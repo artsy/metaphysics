99ca8ae2694c65a3f27d4e4f9c34f343bfd9c6a3
Ashkan Nasseri <ashkan18@users.noreply.github.com>
2019-07-17 16:00:00 -0400

    Fix Exchange CommerceDateTime issue (#1803)
    * Fix exchange stitched schema issue by replacing DateTime fields with MP's dateField
    
    * Fix name issue and use ...dateField on newFields
    
    * Add tests for ReplaceCommerceDataTime transformer
    
    Co-authored-by: David Sheldrick <djsheldrick@gmail.com>
    
    * Update test description
    
    Co-Authored-By: David Sheldrick <d.j.sheldrick@gmail.com>

---
+++ src/lib/stitching/exchange/__tests__/replaceComerceDateTime.test.ts
@@ -0,0 +1,56 @@
+import { GraphQLSchema, GraphQLObjectType, GraphQLScalarType } from "graphql"
+import { runQueryOrThrow } from "test/utils"
+import gql from "lib/gql"
+import { transformSchema } from "graphql-tools"
+import { ReplaceCommerceDateTimeType } from "../transformers/replaceCommerceDateTimeType"
+
+const originalSchema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: "Query",
+    fields: {
+      someRootField: {
+        type: new GraphQLObjectType({
+          name: "SomeType",
+          fields: {
+            someDateField: {
+              type: new GraphQLScalarType({
+                name: "CommerceDateTime",
+                serialize: x => x,
+              }),
+            },
+          },
+        }),
+      },
+    },
+  }),
+})
+
+const schema = transformSchema(originalSchema, [
+  new ReplaceCommerceDateTimeType(),
+])
+
+describe(ReplaceCommerceDateTimeType, () => {
+  it("replaces CommerceDateTime fields", async () => {
+    const data = await runQueryOrThrow({
+      schema,
+      source: gql`
+        query {
+          someRootField {
+            someDateField(format: "[The year is] YYYY")
+          }
+        }
+      `,
+      rootValue: {
+        someRootField: {
+          someDateField: "2019-07-16T19:39:10.001Z",
+        },
+      },
+      contextValue: {},
+    })
+    expect(data).toEqual({
+      someRootField: {
+        someDateField: "The year is 2019",
+      },
+    })
+  })
+})
