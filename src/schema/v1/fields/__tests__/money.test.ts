6cf5863092aa316474899909ee0b7afa8a93465c
Sarah Weir <sweir27@gmail.com>
2019-07-29 16:42:57 -0400

    Use currencyCode to determine symbol while displaying amount() wrapped fields

--- src/schema/v1/fields/__tests__/money.test.ts
+++ src/schema/v1/fields/__tests__/money.test.ts
@@ -3,12 +3,15 @@ import { amount } from "../money"
 describe(amount, () => {
   let amountCents: number
   let args: any
+  let obj: any
+
   beforeEach(() => {
+    obj = {}
     args = { symbol: "$" }
     amountCents = 1234
   })
 
-  const getResult = () => amount(() => amountCents).resolve({}, args)
+  const getResult = () => amount(() => amountCents).resolve(obj, args)
 
   it("formats dollars", () => {
     expect(getResult()).toMatchInlineSnapshot(`"$12.34"`)
@@ -34,5 +37,22 @@ describe(amount, () => {
     expect(getResult()).toMatchInlineSnapshot(`null`)
     amountCents = undefined as any
     expect(getResult()).toMatchInlineSnapshot(`null`)
+  })
+
+  it("formats from a currencyCode if provided", () => {
+    obj = { currencyCode: "GBP" }
+    args = {}
+    expect(getResult()).toMatchInlineSnapshot(`"£12.34"`)
+  })
+
+  it("prefers object symbol over currencyCode", () => {
+    obj = { symbol: "$", currencyCode: "GBP" }
+    args = {}
+    expect(getResult()).toMatchInlineSnapshot(`"$12.34"`)
+  })
+
+  it("prefers argument symbol over currencyCode", () => {
+    obj = { symbol: "£", currencyCode: "GBP" }
+    expect(getResult()).toMatchInlineSnapshot(`"$12.34"`)
   })
 })
