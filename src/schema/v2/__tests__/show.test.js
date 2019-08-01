3b88d99a29349ef6d065997c536b35f5738f3580
Anson Wang <anson.wang@artsymail.com>
2019-08-01 15:34:52 -0400

    add stub dataloaders to tests

--- src/schema/v2/__tests__/show.test.js
+++ src/schema/v2/__tests__/show.test.js
@@ -58,6 +58,12 @@ describe("Show type", () => {
       ),
       galaxyGalleriesLoader: sinon.stub().returns(Promise.resolve(galaxyData)),
       partnerShowLoader: sinon.stub().returns(Promise.resolve(showData)),
+      unauthenticatedLoaders: {
+        showLoader: sinon.stub().returns(Promise.resolve(showData)),
+      },
+      authenticatedLoaders: {
+        showLoader: sinon.stub().returns(Promise.resolve(showData)),
+      },
     }
   })
 
@@ -982,6 +988,12 @@ describe("Show type", () => {
             },
           }),
         showLoader: () => Promise.resolve(showData),
+        unauthenticatedLoaders: {
+          showLoader: sinon.stub().returns(Promise.resolve(showData)),
+        },
+        authenticatedLoaders: {
+          showLoader: sinon.stub().returns(Promise.resolve(showData)),
+        },
       }
     })
 
@@ -1083,7 +1095,9 @@ describe("Show type", () => {
     it("fetches FilterArtworks using the show id and partner id", async () => {
       context = {
         ...context,
-        authenticatedLoaders: {},
+        authenticatedLoaders: {
+          showLoader: sinon.stub().returns(Promise.resolve(showData)),
+        },
         unauthenticatedLoaders: {
           filterArtworksLoader: jest.fn().mockReturnValue(
             Promise.resolve({
@@ -1104,6 +1118,7 @@ describe("Show type", () => {
               },
             })
           ),
+          showLoader: sinon.stub().returns(Promise.resolve(showData)),
         },
       }
 
