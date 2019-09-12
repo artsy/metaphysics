b81c804e45d6e9a150b97f1f5849ade13e0eac87
Matthew Zikherman <mzikherman@gmail.com>
2019-09-12 10:18:18 -0400

    [Package] Re-enable upgrades
    This reverts commit 3db65cff0b846ea073d8ffbec306033805d9675e.

--- src/schema/v1/partner.ts
+++ src/schema/v1/partner.ts
@@ -52,7 +52,7 @@ const artworksArgs: GraphQLFieldConfigArgumentMap = {
   },
 }
 
-const PartnerType = new GraphQLObjectType<any, ResolverContext>({
+export const PartnerType = new GraphQLObjectType<any, ResolverContext>({
   name: "Partner",
   interfaces: [NodeInterface],
   fields: () => {
