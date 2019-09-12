4ae59bf003967598d2dd4a05a95d01799fd1e6a9
Yuki Nishijima <yk.nishijima@gmail.com>
2019-09-11 22:29:13 -0400

    Add Bidder query to v2 as well

---
+++ src/schema/v2/__tests__/bidder.test.js
@@ -0,0 +1,62 @@
+/* eslint-disable promise/always-return */
+import { runQuery } from "schema/v2/test/utils"
+
+describe("Bidder type", () => {
+  let context = null
+  const bidder = {
+    id: "bidder123",
+    created_by_admin: false,
+    created_at: "2018-05-29T15:43:22+00:00",
+    pin: "1234",
+    qualified_for_bidding: true,
+    user: {
+      id: "user123",
+    },
+    sale: {
+      id: "shared-live-mocktion",
+    },
+  }
+
+  const user = {
+    id: "user123",
+    name: "Lucille Bluth",
+    email: "lucille@gmail.com",
+  }
+
+  const sale = {
+    id: "shared-live-mocktion",
+    name: "Shared Live Mocktion",
+  }
+
+  beforeEach(() => {
+    context = {
+      bidderLoader: sinon.stub().returns(Promise.resolve(bidder)),
+      userByIDLoader: sinon.stub().returns(Promise.resolve(user)),
+      saleLoader: sinon.stub().returns(Promise.resolve(sale)),
+    }
+  })
+
+  it("fetches a bidder by ID", () => {
+    const query = `
+      {
+        bidder(id: "5cdae6b0478dbf000ece64b9") {
+          qualifiedForBidding
+          user {
+            name
+            email
+          }
+          sale {
+            id
+            name
+          }
+        }
+      }
+    `
+
+    return runQuery(query, context).then(data => {
+      expect(data.bidder.qualifiedForBidding).toBe(true)
+      expect(data.bidder.user.name).toEqual("Lucille Bluth")
+      expect(data.bidder.sale.name).toEqual("Shared Live Mocktion")
+    })
+  })
+})
--- src/schema/v2/bidder.ts
+++ src/schema/v2/bidder.ts
@@ -3,11 +3,13 @@ import {
   GraphQLObjectType,
   GraphQLString,
   GraphQLFieldConfig,
+  GraphQLNonNull,
 } from "graphql"
 import { ResolverContext } from "types/graphql"
 import date from "./fields/date"
 import { InternalIDFields } from "./object_identification"
 import Sale from "./sale/index"
+import { User } from "./user"
 
 const BidderType = new GraphQLObjectType<any, ResolverContext>({
   name: "Bidder",
@@ -21,14 +23,27 @@ const BidderType = new GraphQLObjectType<any, ResolverContext>({
       type: GraphQLBoolean,
       resolve: ({ qualified_for_bidding }) => qualified_for_bidding,
     },
+    user: {
+      type: User.type,
+      resolve: ({ user }, _, { userByIDLoader }) => userByIDLoader(user.id),
+    },
     sale: {
       type: Sale.type,
+      resolve: ({ sale }, _, { saleLoader }) => saleLoader(sale.id),
     },
   }),
 })
 
 const Bidder: GraphQLFieldConfig<void, ResolverContext> = {
   type: BidderType,
+  description: "A Bidder",
+  args: {
+    id: {
+      type: new GraphQLNonNull(GraphQLString),
+      description: "The ID of the bidder",
+    },
+  },
+  resolve: (_root, { id }, { bidderLoader }) => bidderLoader(id),
 }
 
 export default Bidder
