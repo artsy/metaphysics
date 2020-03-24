1d81ea117a13988189373375e30e1dfc3eca8639
Barry Hoggard <barry@hoggard.org>
2020-03-24 16:14:32 -0400

    Add identityVerified to me object

--- src/schema/v2/me/index.ts
+++ src/schema/v2/me/index.ts
@@ -92,6 +92,10 @@ const Me = new GraphQLObjectType<any, ResolverContext>({
     },
     invoice: Invoice,
     identityVerification: IdentityVerification,
+    identityVerified: {
+      type: GraphQLBoolean,
+      resolve: ({ identity_verified }) => identity_verified,
+    },
     lotsByFollowedArtistsConnection: SaleArtworksConnectionField,
     lotStanding: LotStanding,
     lotStandings: LotStandings,
@@ -151,6 +155,7 @@ const MeField: GraphQLFieldConfig<void, ResolverContext> = {
       "followsAndSaves",
       "lotsByFollowedArtistsConnection",
       "identityVerification",
+      "identityVerified",
       "unreadNotificationsCount",
     ]
     if (includesFieldsOtherThanSelectionSet(info, fieldsNotRequireLoader)) {
