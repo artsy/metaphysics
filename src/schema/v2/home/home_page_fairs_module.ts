79be3dcdd4506b2efc5483f6d8350588422f7b7b
Ash Furrow <ash@ashfurrow.com>
2020-05-01 16:22:20 -0400

    Re-orders fair results so that fairs that haven't started yet are at the end.
    Co-authored-by: Brian Beckerle <brian.beckerle@artsymail.com>

--- src/schema/v2/home/home_page_fairs_module.ts
+++ src/schema/v2/home/home_page_fairs_module.ts
@@ -1,6 +1,8 @@
 import Fair from "schema/v2/fair"
 import { GraphQLList, GraphQLObjectType, GraphQLNonNull } from "graphql"
 import { ResolverContext } from "types/graphql"
+import { groupBy } from "lodash"
+import moment from "moment"
 
 export const HomePageFairsModuleType = new GraphQLObjectType<
   any,
@@ -17,22 +19,36 @@ export const HomePageFairsModuleType = new GraphQLObjectType<
           sort: "-start_at",
           active: true,
         }
-        return fairsLoader(gravityOptions).then(({ body: runningFairs }) => {
-          // If there are less than 8, get the most recent closed fairs
-          if (runningFairs.length < 8) {
-            const newOptions = {
-              ...gravityOptions,
-              status: "closed",
-              active: false,
-              size: 8 - runningFairs.length,
-            }
-            return fairsLoader(newOptions).then(({ body: closedFairs }) => {
-              const allFairs = runningFairs.concat(closedFairs)
-              return allFairs.filter(fair => fair.mobile_image)
+        return fairsLoader(gravityOptions).then(
+          ({ body: ungroupedRunningFairs }) => {
+            // Gravity returns fairs that are both current and upcoming.
+            // Make sure the current ones appear first in the results list.
+            const now = moment.utc()
+            const returnValue = groupBy(ungroupedRunningFairs, fair => {
+              const startAt = moment.utc(fair.start_at)
+              return now.isAfter(startAt) ? "current" : "upcoming"
             })
+
+            const runningFairs = (returnValue["current"] || []).concat(
+              returnValue["upcoming"] || []
+            )
+
+            // If there are less than 8, get the most recent closed fairs
+            if (runningFairs.length < 8) {
+              const newOptions = {
+                ...gravityOptions,
+                status: "closed",
+                active: false,
+                size: 8 - runningFairs.length,
+              }
+              return fairsLoader(newOptions).then(({ body: closedFairs }) => {
+                const allFairs = runningFairs.concat(closedFairs)
+                return allFairs.filter(fair => fair.mobile_image)
+              })
+            }
+            return runningFairs.filter(fair => fair.mobile_image)
           }
-          return runningFairs.filter(fair => fair.mobile_image)
-        })
+        )
       },
     },
   },
