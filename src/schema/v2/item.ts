89c3e37c51d462b08cba9cd9072c892a57b19b03
dzucconi <mail@damonzucconi.com>
2020-05-11 10:57:50 -0400

    Exposes union typed ordered item connection

--- src/schema/v2/item.ts
+++ src/schema/v2/item.ts
@@ -1,13 +1,17 @@
+import { GraphQLUnionType } from "graphql"
 import { ArtistType } from "./artist"
 import { ArtworkType } from "./artwork"
 import { FeaturedLinkType } from "./featured_link"
 import { GeneType } from "./gene"
-import { GraphQLUnionType } from "graphql"
+import { connectionWithCursorInfo } from "schema/v2/fields/pagination"
+import { Gravity } from "types/runtime"
 
 export const OrderedSetItemType = new GraphQLUnionType({
   name: "OrderedSetItem",
   types: [ArtistType, ArtworkType, FeaturedLinkType, GeneType],
-  resolveType: value => {
+  resolveType: (
+    value: Gravity.OrderedItem & { item_type: Gravity.OrderedSet["item_type"] }
+  ) => {
     switch (value.item_type) {
       case "FeaturedLink":
         return FeaturedLinkType
@@ -21,4 +25,8 @@ export const OrderedSetItemType = new GraphQLUnionType({
         throw new Error(`Unknown context type: ${value.item_type}`)
     }
   },
+})
+
+export const OrderedSetItemConnection = connectionWithCursorInfo({
+  nodeType: OrderedSetItemType,
 })
