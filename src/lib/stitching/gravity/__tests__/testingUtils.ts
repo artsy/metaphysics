90f20e850a076c975591282df75c3c059295d51b
Will Doenlen <wdoenlen@gmail.com>
2020-07-02 17:45:16 -0400

    Add tests for filterArtworksConnection stitching on ArtistSeries

--- src/lib/stitching/gravity/__tests__/testingUtils.ts
+++ src/lib/stitching/gravity/__tests__/testingUtils.ts
@@ -5,8 +5,6 @@ import { executableGravitySchema } from "../schema"
 import localSchema from "schema/v2/schema"
 
 let cachedSchema: GraphQLSchema & { transforms: any }
-let stitchedSchema: ReturnType<typeof gravityStitchingEnvironment>
-let mergedSchema: GraphQLSchema & { transforms: any }
 
 /** Gets a cached copy of the transformed gravity schema  */
 export const getGravityTransformedSchema = async () => {
@@ -17,29 +15,27 @@ export const getGravityTransformedSchema = async () => {
 }
 
 /** Gets a cached copy of the stitched schema, independent of being merged into the local schema */
-export const getGravityStitchedSchema = async () => {
-  if (!stitchedSchema) {
-    const cachedSchema = await getGravityTransformedSchema()
-    stitchedSchema = gravityStitchingEnvironment(localSchema, cachedSchema)
-  }
-  return stitchedSchema
+export const getGravityStitchedSchema = async (schemaVersion = 2) => {
+  const cachedSchema = await getGravityTransformedSchema()
+  return gravityStitchingEnvironment(localSchema, cachedSchema, schemaVersion)
 }
 
 /** Gets a cached fully setup schema with gravity and the localSchema set up */
-export const getGravityMergedSchema = async () => {
-  if (!mergedSchema) {
-    const cachedSchema = await getGravityTransformedSchema()
-    const { extensionSchema, resolvers } = await getGravityStitchedSchema()
+export const getGravityMergedSchema = async (schemaVersion = 2) => {
+  const cachedSchema = await getGravityTransformedSchema()
+  const { extensionSchema, resolvers } = await getGravityStitchedSchema(
+    schemaVersion
+  )
 
-    // The order should only matter in that extension schemas come after the
-    // objects that they are expected to build upon
-    mergedSchema = mergeSchemas({
-      schemas: [localSchema, cachedSchema, extensionSchema],
-      resolvers: resolvers,
-    }) as GraphQLSchema & { transforms: any }
+  // The order should only matter in that extension schemas come after the
+  // objects that they are expected to build upon
+  const mergedSchema = mergeSchemas({
+    schemas: [localSchema, cachedSchema, extensionSchema],
+    resolvers: resolvers,
+  }) as GraphQLSchema & { transforms: any }
 
-    const anyMergedSchema = mergedSchema as any
-    anyMergedSchema.__allowedLegacyNames = ["__id"]
-  }
+  const anyMergedSchema = mergedSchema as any
+  anyMergedSchema.__allowedLegacyNames = ["__id"]
+
   return mergedSchema
 }
