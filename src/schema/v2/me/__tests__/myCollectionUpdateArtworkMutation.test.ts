5cd3af1b9438e67d8548f740081866cc6504f45f
Christopher Pappas <damassi.pappas@gmail.com>
2020-08-17 16:09:24 -0700

    Add myCollectionCreate|UpdateArtwork mutations

---
+++ src/schema/v2/me/__tests__/myCollectionUpdateArtworkMutation.test.ts
@@ -0,0 +1,39 @@
+import { runAuthenticatedQuery } from "schema/v2/test/utils"
+import gql from "lib/gql"
+
+describe("myCollectionUpdateArtworkMutation", () => {
+  it("updates an artwork", async () => {
+    const mutation = gql`
+      mutation {
+        myCollectionUpdateArtwork(
+          input: {
+            artworkId: "foo"
+            artistIds: ["4d8b92b34eb68a1b2c0003f4"]
+            medium: "Painting"
+            dimensions: "20x20"
+          }
+        ) {
+          artwork {
+            medium
+          }
+        }
+      }
+    `
+    const context = {
+      myCollectionUpdateArtworkLoader: () => Promise.resolve({ id: "foo" }),
+      myCollectionArtworkLoader: () =>
+        Promise.resolve({
+          medium: "Updated",
+        }),
+    }
+
+    const data = await runAuthenticatedQuery(mutation, context)
+    expect(data).toEqual({
+      myCollectionUpdateArtwork: {
+        artwork: {
+          medium: "Updated",
+        },
+      },
+    })
+  })
+})
---
+++ src/schema/v2/me/myCollectionCreateArtworkMutation.ts
@@ -0,0 +1,53 @@
+import { GraphQLString, GraphQLList } from "graphql"
+import { mutationWithClientMutationId } from "graphql-relay"
+import { ArtworkType } from "schema/v2/artwork/index"
+import { ResolverContext } from "types/graphql"
+import { GraphQLNonNull } from "graphql"
+
+export const myCollectionCreateArtworkMutation = mutationWithClientMutationId<
+  any,
+  any,
+  ResolverContext
+>({
+  name: "MyCollectionCreateArtwork",
+  description: "Create an artwork in my collection",
+  inputFields: {
+    artistIds: {
+      type: new GraphQLNonNull(new GraphQLList(GraphQLString)),
+    },
+    dimensions: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+    medium: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+  },
+  outputFields: {
+    artwork: {
+      type: ArtworkType,
+      resolve: ({ id }, _, { myCollectionArtworkLoader }) => {
+        if (myCollectionArtworkLoader) {
+          return myCollectionArtworkLoader(id)
+        }
+      },
+    },
+  },
+  mutateAndGetPayload: (
+    { artistIds, dimensions, medium },
+    { myCollectionCreateArtworkLoader }
+  ) => {
+    if (!myCollectionCreateArtworkLoader) {
+      return new Error("You need to be signed in to perform this action")
+    }
+
+    return myCollectionCreateArtworkLoader({
+      artist_ids: artistIds,
+      dimensions,
+      medium,
+    }).then(({ id }) => {
+      return {
+        id,
+      }
+    })
+  },
+})
