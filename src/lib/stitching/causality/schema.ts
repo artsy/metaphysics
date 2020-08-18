37330936a26f6587c7b5686b6c5cb49b66005c5a
Erik Stockmeier <erik.d.stock@gmail.com>
2020-08-18 18:38:02 -0400

    use auctions- prefix, exclude all root fields

--- src/lib/stitching/causality/schema.ts
+++ src/lib/stitching/causality/schema.ts
@@ -4,8 +4,17 @@ import {
   transformSchema,
   RenameTypes,
   RenameRootFields,
+  FilterRootFields,
+  FilterTypes,
 } from "graphql-tools"
 import { readFileSync } from "fs"
+
+const blacklistedTypes: string[] = []
+const whitelistedRootFields: string[] = [
+  // "auctionIncrementPolicy",
+  // "auctionIncrementPolicies",
+  // "auctionIncrementPolicyGroups",
+]
 
 export const executableCausalitySchema = () => {
   const threeBodyLink = createCausalityLink()
@@ -19,12 +28,23 @@ export const executableCausalitySchema = () => {
 
   // Return the new modified schema
   return transformSchema(schema, [
-    new RenameTypes(name => {
-      return `Causality${name}`
+    new FilterTypes((type) => !blacklistedTypes.includes(type.name)),
+    new FilterRootFields((operation, name, _field) => {
+      // We are currently obscuring all root fields
+      if (operation === "Query") return whitelistedRootFields.includes(name)
+      if (operation === "Mutation") return false
+      return false
+    }),
+    new RenameTypes((name) => {
+      // this could be left as AuctionsLong, it is just a scalar (same as int?)
+      if (name === "Long") return "Long"
+      // if (name === "Lot") name = "LotState"
+      // if (name === "Sale") name = "SaleState"
+      return `Auctions${name}`
     }),
     new RenameRootFields(
       (_operation, name) =>
-        `causality${name.charAt(0).toUpperCase() + name.slice(1)}`
+        `auctions${name.charAt(0).toUpperCase() + name.slice(1)}`
     ),
   ])
 }
