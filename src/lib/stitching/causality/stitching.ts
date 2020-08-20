8fca9d70cd888975c6b02f49596ad3d011a29687
Erik Stockmeier <erik.d.stock@gmail.com>
2020-08-20 14:00:55 -0400

    stitch saleArtwork field into `AuctionsLotStanding` type

--- src/lib/stitching/causality/stitching.ts
+++ src/lib/stitching/causality/stitching.ts
@@ -3,8 +3,9 @@ import { GraphQLSchema } from "graphql"
 
 export const causalityStitchingEnvironment = ({
   causalitySchema,
+  localSchema,
 }: {
-  causalitySchema: GraphQLSchema
+  causalitySchema: GraphQLSchema & { transforms: any }
   localSchema: GraphQLSchema
 }) => {
   return {
@@ -12,28 +13,55 @@ export const causalityStitchingEnvironment = ({
       extend type Me {
         auctionsLotStandings: AuctionsLotStandingConnection
       }
+
+      extend type AuctionsLotStanding {
+        saleArtwork: SaleArtwork
+      }
     `,
 
     resolvers: {
+      AuctionsLotStanding: {
+        saleArtwork: {
+          fragment: gql`
+            fragment AuctionsLotStandingSaleArtwork on AuctionsLotStanding {
+              lot {
+                internalID
+              }
+            }
+          `,
+          resolve: (parent, _args, context, info) => {
+            return info.mergeInfo.delegateToSchema({
+              schema: localSchema,
+              operation: "query",
+              fieldName: "saleArtwork",
+              args: { id: parent.lot.internalID },
+              transforms: causalitySchema.transforms,
+              context,
+              info,
+            })
+          },
+        },
+      },
       Me: {
         auctionsLotStandings: {
           // The required query to get access to the object, e.g. we have to
           // request `id` on a Me in order to access the user's lot standings
           fragment: gql`
             fragment MeLotStandings on Me {
-              id
+              internalID
             }
           `,
           // The function to handle getting the lot standings correctly, we
           // use the root query `auctionsLotStandings` to grab the data from the local
           // metaphysics schema
           resolve: (parent, _args, context, info) => {
+            console.log("Fetching lot standings for " + parent.internalID)
             return info.mergeInfo.delegateToSchema({
               schema: causalitySchema,
               operation: "query",
               fieldName: "_unused_auctionsLotStandings",
               args: {
-                userId: parent.id,
+                userId: parent.internalID,
               },
               context,
               info,
