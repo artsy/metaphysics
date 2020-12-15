2c1da886a14b868df1c7aed82f7237f240197e07
Joel Rosenblatt <7117670+jo-rs@users.noreply.github.com>
2020-12-15 18:43:26 +0100

    Commented me.saleRegistrations field on v2 back in to use with force

--- src/schema/v2/me/index.ts
+++ src/schema/v2/me/index.ts
@@ -32,7 +32,7 @@ import Invoice from "./conversation/invoice"
 import LotStanding from "./lot_standing"
 import LotStandings from "./lot_standings"
 import { RecentlyViewedArtworks } from "./recently_viewed_artworks"
-// import SaleRegistrations from "./sale_registrations"
+import SaleRegistrations from "./sale_registrations"
 import { SavedArtworks } from "./savedArtworks"
 import { ResolverContext } from "types/graphql"
 import { SaleArtworksConnectionField } from "../sale_artworks"
@@ -184,7 +184,7 @@ const Me = new GraphQLObjectType<any, ResolverContext>({
       resolve: ({ recently_viewed_artwork_ids }) => recently_viewed_artwork_ids,
     },
     recentlyViewedArtworksConnection: RecentlyViewedArtworks,
-    // saleRegistrations: SaleRegistrations,
+    saleRegistrations: SaleRegistrations,
     type: {
       type: GraphQLString,
     },
