2501ebd69ec4ead377e362191670e3b6c6efe339
Devon Blandin <dblandin@gmail.com>
2020-12-18 11:10:38 -0500

    chore(mediums): update collector-facing copy for artwork mediums

--- src/lib/mediumTypes.ts
+++ src/lib/mediumTypes.ts
@@ -2,20 +2,20 @@ export default {
   Architecture: {
     id: "Architecture",
     long_description:
-      "Includes architectural models; buildings (e.g. house, temple).",
+      "Includes architectural models; buildings (e.g., house, temple).",
     name: "Architecture",
   },
   "Books and Portfolios": {
     id: "Books and Portfolios",
     long_description:
-      "Includes albums; artist books; illustrated manuscripts; portfolios (i.e. group of works such as photographs or prints).",
+      "Includes albums; artist books; illustrated manuscripts; portfolios (i.e., a group of works such as photographs or prints).",
     name: "Books and Portfolios",
   },
   "Design/Decorative Art": {
     id: "Design/Decorative Art",
     long_description: [
       "Objects that are primarily functional.",
-      "Includes such items as containers (e.g. basket, bowl, box, vase); furniture (e.g. cabinet, chair, table); lighting; mirrors; tableware (e.g. flatware, plates).",
+      "Includes containers (e.g., basket, bowl, box, vase); furniture (e.g., cabinet, chair, table); lighting; mirrors; tableware (e.g., flatware, plates).",
     ].join(" "),
     name: "Design/Decorative Art",
   },
@@ -23,21 +23,21 @@ export default {
     id: "Drawing, Collage or Other Work on Paper",
     long_description: [
       "Includes collages; drawings; figure drawing; pen and ink; sketch.",
-      "This medium type also includes paintings where paper is the support.",
+      "This also includes paintings where paper is the support.",
     ].join(" "),
     name: "Drawing, Collage or Other Work on Paper",
   },
   "Ephemera or Merchandise": {
     id: "Ephemera or Merchandise",
     long_description: [
-      "Items related to the artist, created or manufactured for a specific or limited use.",
-      "This also includes exhibition materials, memorabilia, autographs, skateboards, toys, and other items made with branded collaborators, etc.",
+      "Items related to the artist, created for a specific use.",
+      "This includes exhibition materials, memorabilia, autographs, skateboards, toys, and other items made with branded collaborators.",
     ].join(" "),
     name: "Ephemera or Merchandise",
   },
   "Fashion Design and Wearable Art": {
     id: "Fashion Design and Wearable Art",
-    long_description: "Includes costumes; garments (e.g. dress, jacket).",
+    long_description: "Includes costumes; garments (e.g., dress, jacket).",
     name: "Fashion Design and Wearable Art",
   },
   Installation: {
@@ -48,19 +48,19 @@ export default {
   Jewelry: {
     id: "Jewelry",
     long_description:
-      "Includes costume accessories; bracelets; brooches; earrings; necklaces; rings.",
+      "Includes bracelets; brooches; earrings; necklaces; rings; costume accessories.",
     name: "Jewelry",
   },
   "Mixed Media": {
     id: "Mixed Media",
     long_description:
-      "Intermedia; Multimedia work that does not fall into another medium type category (ex: Drawing, Collage, or Other work on paper).",
+      "Intermedia; Multimedia work that does not fall into another category.",
     name: "Mixed Media",
   },
   Other: {
     id: "Other",
     long_description:
-      "Includes armor; musical instruments; tools; weapons; items that do not fall into any of the other categories.",
+      "Includes items that do not fall into any other categories (e.g., armor; musical instruments; tools; weapons).",
     name: "Other",
   },
   Painting: {
@@ -77,14 +77,14 @@ export default {
   Photography: {
     id: "Photography",
     long_description:
-      "Includes chromogenic color prints (i.e. c-print); polaroids; silver gelatin prints; photograms.",
+      "Includes chromogenic color prints (c-prints); polaroids; silver gelatin prints; photograms.",
     name: "Photography",
   },
   Posters: {
     id: "Posters",
     long_description: [
       "Includes advertisements; offset lithograph posters.",
-      "Please note that this does not include exhibition posters (see Merchandise and Ephemera).",
+      "Please note that this does not include exhibition posters (see “Ephemera or Merchandise”).",
     ].join(" "),
     name: "Posters",
   },
@@ -97,9 +97,9 @@ export default {
   Reproduction: {
     id: "Reproduction",
     long_description: [
-      "Reproduction of an original work authorized by the artist, the artist's studio or estate.",
-      "Artist involvement is not implicit.",
+      "Reproduction of an original work authorized by the artist or the artist's studio/estate.",
       "This includes plates from illustrated books, museum replicas, etc.",
+      "Please note that the artist may not have been directly involved in the work’s production.",
     ].join(" "),
     name: "Reproduction",
   },
@@ -107,7 +107,7 @@ export default {
     id: "Sculpture",
     long_description: [
       "Includes assemblage; bas-relief; carvings; figurines; masks; mosaics; statues; wall pieces.",
-      "For figurines made with branded collaborators (ex: BE@RBRICK, Medicom), please see “Ephemera or Merchandise.”",
+      "For figurines made with branded collaborators (e.g., BE@RBRICK, Medicom), please see “Ephemera or Merchandise.”",
     ].join(" "),
     name: "Sculpture",
   },
