a73ac757947e3e8aba51e8889e6dd99e4aa75579
Mounir Dhahri <soussouheros@gmail.com>
2021-02-05 19:02:23 +0100

    fix: fix typo in show sort and remove unavailable sort

--- src/schema/v2/sorts/show_sorts.ts
+++ src/schema/v2/sorts/show_sorts.ts
@@ -3,11 +3,10 @@ import { GraphQLEnumType } from "graphql"
 const SHOW_SORTS = {
   END_AT_ASC: { value: "end_at" },
   END_AT_DESC: { value: "-end_at" },
-  FEATURED_ASC: { value: "-featured,-start_at" },
-  FEATURED_DESC: { value: "-featured,-end_at" },
+  FEATURED_ASC: { value: "featured" },
+  FEATURED_DESC: { value: "-featured" },
   NAME_ASC: { value: "name" },
   NAME_DESC: { value: "-name" },
-  PARTNER_ASC: { value: "fully_qualified_name" },
   SORTABLE_NAME_ASC: { value: "sortable_name" },
   SORTABLE_NAME_DESC: { value: "-sortable_name" },
   START_AT_ASC: { value: "start_at" },
