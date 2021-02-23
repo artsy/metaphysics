0521fa38a3413fb2d8a9d7b42ed22945f2c137d0
brainbicycle <brian.beckerle@artsymail.com>
2021-02-23 15:22:18 -0500

    return null for pricePaid when cents not specified
    Co-authored-by: Barry Hoggard <barry@artsymail.com>

--- src/schema/v2/artwork/index.ts
+++ src/schema/v2/artwork/index.ts
@@ -745,6 +745,7 @@ export const ArtworkType = new GraphQLObjectType<any, ResolverContext>({
           "The price paid for the artwork in a user's 'my collection'",
         resolve: (artwork) => {
           const { price_paid_cents } = artwork
+          if (!price_paid_cents) return null
           const price_paid_currency = artwork.price_paid_currency || "USD"
           return {
             cents: price_paid_cents,
