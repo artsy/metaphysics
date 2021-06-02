759e8504fd25c49ea9fc74e7bfafad294f33e2c7
Ole <ole.richter91@gmail.com>
2021-06-02 16:52:30 +0200

    use diffusion graphql

--- src/schema/v2/me/auction_results_by_followed_artists.ts
+++ src/schema/v2/me/auction_results_by_followed_artists.ts
@@ -1,9 +1,6 @@
 import { GraphQLFieldConfig } from "graphql"
-import { connectionFromArraySlice } from "graphql-relay"
-import { convertConnectionArgsToGravityArgs } from "lib/helpers"
-import { merge } from "lodash"
+import gql from "lib/gql"
 import { pageable } from "relay-cursor-paging"
-import { createPageCursors } from "schema/v1/fields/pagination"
 import { params } from "schema/v1/home/add_generic_genes"
 import { ResolverContext } from "types/graphql"
 import { auctionResultConnection } from "../auction_result"
@@ -17,11 +14,16 @@ const AuctionResultsByFollowedArtists: GraphQLFieldConfig<
   description: "A list of the auction results by followed artists",
   resolve: async (
     _root,
-    options,
-    { followedArtistsLoader, auctionLotsLoader }
+    _options,
+    { followedArtistsLoader, auctionLotsLoader, diffusionGraphqlLoader }
   ) => {
     try {
-      if (!followedArtistsLoader || !auctionLotsLoader) return null
+      if (
+        !followedArtistsLoader ||
+        !auctionLotsLoader ||
+        !diffusionGraphqlLoader
+      )
+        return null
 
       const gravityArgs = {
         size: 50,
@@ -31,58 +33,56 @@ const AuctionResultsByFollowedArtists: GraphQLFieldConfig<
       }
       const { body: followedArtists } = await followedArtistsLoader(gravityArgs)
 
-      // TODO make it possible!
       const followedArtistIds = followedArtists.map((artist) => artist.id)
 
-      const {
-        page,
-        size,
-        offset,
-        sizes,
-        organizations,
-        categories,
-      } = convertConnectionArgsToGravityArgs(options)
-      const diffusionArgs = {
-        page,
-        size,
-        // artist_ids: followedArtistIds,
-        artist_id: "4dd1584de0091e000100207c",
-        organizations,
-        categories,
-        earliest_created_year: options.earliestCreatedYear,
-        latest_created_year: options.latestCreatedYear,
-        allow_empty_created_dates: options.allowEmptyCreatedDates,
-        sizes,
-        sort: options.sort,
-      }
-
-      const { total_count, _embedded } = await auctionLotsLoader(diffusionArgs)
-      const totalPages = Math.ceil(total_count / size)
-
-      return merge(
-        {
-          pageCursors: createPageCursors(
-            {
-              page,
-              size,
-            },
-            total_count
-          ),
+      console.log(followedArtistIds)
+      const auctionResults = await diffusionGraphqlLoader({
+        query: gql`
+          query AuctionResultsByArtistsConnection($artistIds: [ID!]!) {
+            auctionResultsByArtistsConnection(artistIds: $artistIds) {
+              edges {
+                node {
+                  artistId
+                  boughtIn
+                  categoryText
+                  currency
+                  date
+                  dateText
+                  depthCm
+                  description
+                  diameterCm
+                  dimensionText
+                  externalUrl
+                  hammerPriceCents
+                  hammerPriceCentsUsd
+                  heightCm
+                  highEstimateCents
+                  highEstimateCentsUsd
+                  id
+                  location
+                  lowEstimateCents
+                  lowEstimateCentsUsd
+                  mediumText
+                  organization
+                  priceRealizedCents
+                  priceRealizedCentsUsd
+                  saleDate
+                  saleDateText
+                  saleOverEstimatePercentage
+                  saleTitle
+                  title
+                  widthCm
+                }
+              }
+            }
+          }
+        `,
+        variables: {
+          artistIds: ["4df44138c75005000100331c"], //followedArtistIds,
         },
-        {
-          totalCount: total_count,
-        },
-        connectionFromArraySlice(_embedded.items, options, {
-          arrayLength: total_count,
-          sliceStart: offset,
-        }),
-        {
-          pageInfo: {
-            hasPreviousPage: page > 1,
-            hasNextPage: page < totalPages,
-          },
-        }
-      )
+      })
+
+      return auctionResults?.auctionResultsByArtistsConnection
     } catch (error) {
       console.log(error)
       throw new Error(error)
