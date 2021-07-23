36174f839b6a98b87d754d6d8ed15125e5c56cd0
Christopher Pappas <damassi.pappas@gmail.com>
2021-07-23 10:42:39 -0700

    Update scripts/setup.sh
    Co-authored-by: Ozzie Vasdi <29984068+ovasdi@users.noreply.github.com>

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -37,7 +37,7 @@ else
 fi
 
 echo 'Updating .env.shared file (for shared configuration)...'
-aws s3 cp s3://artsy-citadel/dev/.env.metaphysics .env.shared || return
+aws s3 cp s3://artsy-citadel/dev/.env.metaphysics .env.shared || 'Unable to download shared configuration, ensure you have S3 access!'
 
 echo 'Setup complete! To start the server, run:
   yarn start'
