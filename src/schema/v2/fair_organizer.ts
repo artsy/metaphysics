43ec87c401373dad484ce12a849593e4db1134ef
Devon Blandin <dblandin@gmail.com>
2021-08-04 17:59:19 +0200

    feat: add articlesConnection to FairOrganizer type
    Allow client to query for articles associated with any fairs associated with a
    fair organizer.
    
    Capped to support at most 100 fairs for a particular fair organizer which
    should be more than enough to support all current fair organizers.

--- src/schema/v2/fair_organizer.ts
+++ src/schema/v2/fair_organizer.ts
@@ -4,16 +4,20 @@ import {
   GraphQLString,
   GraphQLNonNull,
   GraphQLFieldConfig,
+  GraphQLInt,
+  GraphQLBoolean,
 } from "graphql"
 import { SlugAndInternalIDFields } from "./object_identification"
 import Profile from "./profile"
 import { ResolverContext } from "types/graphql"
-import { pageable } from "relay-cursor-paging"
+import { CursorPageable, pageable } from "relay-cursor-paging"
 import { convertConnectionArgsToGravityArgs } from "lib/helpers"
 import { createPageCursors } from "./fields/pagination"
 import { connectionFromArraySlice } from "graphql-relay"
 import { pick } from "lodash"
 import { fairConnection } from "./fair"
+import { articleConnection } from "./article"
+import ArticleSorts, { ArticleSort } from "./sorts/article_sorts"
 
 export const FairOrganizerType = new GraphQLObjectType<any, ResolverContext>({
   name: "FairOrganizer",
@@ -22,6 +26,69 @@ export const FairOrganizerType = new GraphQLObjectType<any, ResolverContext>({
       ...SlugAndInternalIDFields,
       about: {
         type: GraphQLString,
+      },
+      articlesConnection: {
+        description: "A connection of articles related to a partner.",
+        type: articleConnection.connectionType,
+        args: pageable({
+          sort: ArticleSorts,
+          page: { type: GraphQLInt },
+          inEditorialFeed: {
+            type: GraphQLBoolean,
+            description:
+              "Articles that are ready to be publicly viewed in the feed by everyone.",
+          },
+        }),
+        resolve: async (
+          { profile_id },
+          args: {
+            inEditorialFeed?: Boolean
+            sort?: ArticleSort
+          } & CursorPageable,
+          { articlesLoader, fairsLoader }
+        ) => {
+          const { page, size, offset } = convertConnectionArgsToGravityArgs(
+            args
+          )
+
+          const gravityOptions = {
+            fair_organizer_id: profile_id,
+            size: 100,
+          }
+
+          const { body } = await fairsLoader(gravityOptions)
+
+          interface ArticleArgs {
+            published: boolean
+            fair_ids: [string]
+            limit: number
+            count: boolean
+            offset: number
+            sort?: ArticleSort
+            in_editorial_feed?: Boolean
+          }
+
+          const articleArgs: ArticleArgs = {
+            published: true,
+            fair_ids: body.map((fair) => fair._id),
+            limit: size,
+            count: true,
+            offset,
+            sort: args.sort,
+            in_editorial_feed: args.inEditorialFeed,
+          }
+
+          const { results, count } = await articlesLoader(articleArgs)
+
+          return {
+            totalCount: count,
+            pageCursors: createPageCursors({ ...args, page, size }, count),
+            ...connectionFromArraySlice(results, args, {
+              arrayLength: count,
+              sliceStart: offset,
+            }),
+          }
+        },
       },
       fairsConnection: {
         type: fairConnection.connectionType,
