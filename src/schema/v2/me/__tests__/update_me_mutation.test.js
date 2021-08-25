80403afe2860f8906230435caf00bd78393b4866
dzucconi <mail@damonzucconi.com>
2021-08-25 12:06:59 -0400

    feat(me): adds additional fields to update me mutation

--- src/schema/v2/me/__tests__/update_me_mutation.test.js
+++ src/schema/v2/me/__tests__/update_me_mutation.test.js
@@ -8,19 +8,21 @@ describe("UpdateMeMutation", () => {
       mutation {
         updateMyUserProfile(
           input: {
-            collectorLevel: 1
             clientMutationId: "1232"
-            phone: "1234890"
+            collectorLevel: 1
             location: { address: "123 my street" }
-            priceRangeMin: -1
+            phone: "1234890"
             priceRangeMax: 1000000000000
-            receivePurchaseNotification: false
-            receiveOutbidNotification: false
+            priceRangeMin: -1
+            profession: "Juggler"
             receiveLotOpeningSoonNotification: false
-            receiveSaleOpeningClosingNotification: false
-            receiveNewWorksNotification: true
             receiveNewSalesNotification: false
+            receiveNewWorksNotification: true
+            receiveOutbidNotification: false
             receivePromotionNotification: false
+            receivePurchaseNotification: false
+            receiveSaleOpeningClosingNotification: false
+            shareFollows: false
           }
         ) {
           user {
@@ -62,46 +64,64 @@ describe("UpdateMeMutation", () => {
       }
     `
 
+    const mockUpdateMeLoader = jest.fn().mockReturnValue(
+      Promise.resolve({
+        id: "106",
+        location: { address: "123 my street" },
+        name: "andy-warhol",
+        phone: "1234890",
+        price_range: "-1:1000000000000",
+        receive_lot_opening_soon_notification: false,
+        receive_new_sales_notification: false,
+        receive_new_works_notification: true,
+        receive_outbid_notification: false,
+        receive_promotion_notification: false,
+        receive_purchase_notification: false,
+        receive_sale_opening_closing_notification: false,
+      })
+    )
+
     const context = {
       meLoader: () =>
         Promise.resolve({
           id: "106",
+          location: { address: "123 my street" },
           name: "andy-warhol",
           phone: "1234890",
-          location: {
-            address: "123 my street",
-          },
           price_range: "-1:1000000000000",
-          receive_purchase_notification: true,
-          receive_outbid_notification: true,
           receive_lot_opening_soon_notification: true,
-          receive_sale_opening_closing_notification: true,
-          receive_new_works_notification: true,
           receive_new_sales_notification: true,
-          receive_promotion_notification: true,
-        }),
-      updateMeLoader: () =>
-        Promise.resolve({
-          id: "106",
-          name: "andy-warhol",
-          phone: "1234890",
-          location: {
-            address: "123 my street",
-          },
-          price_range: "-1:1000000000000",
-          receive_purchase_notification: false,
-          receive_outbid_notification: false,
-          receive_lot_opening_soon_notification: false,
-          receive_sale_opening_closing_notification: false,
           receive_new_works_notification: true,
-          receive_new_sales_notification: false,
-          receive_promotion_notification: false,
+          receive_outbid_notification: true,
+          receive_promotion_notification: true,
+          receive_purchase_notification: true,
+          receive_sale_opening_closing_notification: true,
         }),
+      updateMeLoader: mockUpdateMeLoader,
     }
 
     await runAuthenticatedQuery(mutation, context).then((data) => {
       expect(data).toMatchSnapshot()
     })
-    expect.assertions(1)
+
+    expect(mockUpdateMeLoader).toBeCalledWith({
+      client_mutation_id: "1232",
+      collector_level: 1,
+      location: { address: "123 my street" },
+      phone: "1234890",
+      price_range_max: 1000000000000,
+      price_range_min: -1,
+      profession: "Juggler",
+      receive_lot_opening_soon_notification: false,
+      receive_new_sales_notification: false,
+      receive_new_works_notification: true,
+      receive_outbid_notification: false,
+      receive_promotion_notification: false,
+      receive_purchase_notification: false,
+      receive_sale_opening_closing_notification: false,
+      share_follows: false,
+    })
+
+    expect.assertions(2)
   })
 })
