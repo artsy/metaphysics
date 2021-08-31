8a646dcb433a34f79b256fbb00610532a5cbf9ff
dzucconi <mail@damonzucconi.com>
2021-08-31 15:52:22 -0400

    refactor(date): supports type-safe date field

--- src/schema/v2/fields/__tests__/date.test.ts
+++ src/schema/v2/fields/__tests__/date.test.ts
@@ -1,41 +1,43 @@
-import dateField, { date } from "schema/v2/fields/date"
+import dateField, { formatDate } from "schema/v2/fields/date"
 
-describe("date", () => {
+describe("formatDate", () => {
   const rawDate = "2020-12-31T12:00:00+00:00"
   const format = "M/D/YYYY h:mm Z"
   it("returns unformatted, UTC time if no timezone or format is specified", () => {
-    expect(date(rawDate)).toBe("2020-12-31T12:00:00+00:00")
+    expect(formatDate(rawDate)).toBe("2020-12-31T12:00:00+00:00")
   })
 
   it("returns formatted UTC time if no timezone is specified", () => {
-    expect(date(rawDate, format)).toBe("12/31/2020 12:00 +00:00")
+    expect(formatDate(rawDate, format)).toBe("12/31/2020 12:00 +00:00")
   })
 
   it("returns unformatted, local time if no format is specified", () => {
-    expect(date(rawDate, null, "America/Boise")).toBe(
+    expect(formatDate(rawDate, null, "America/Boise")).toBe(
       "2020-12-31T05:00:00-07:00"
     )
-    expect(date(rawDate, null, "Pacific/Fiji")).toBe(
+    expect(formatDate(rawDate, null, "Pacific/Fiji")).toBe(
       "2021-01-01T01:00:00+13:00"
     )
   })
 
   it("provides formatted, local time if timezone and format are specified", () => {
-    expect(date(rawDate, format, "America/Boise")).toBe(
+    expect(formatDate(rawDate, format, "America/Boise")).toBe(
       "12/31/2020 5:00 -07:00"
     )
-    expect(date(rawDate, format, "Pacific/Fiji")).toBe("1/1/2021 1:00 +13:00")
+    expect(formatDate(rawDate, format, "Pacific/Fiji")).toBe(
+      "1/1/2021 1:00 +13:00"
+    )
   })
 })
 
 describe("dateField", () => {
   it("should return null for a missing date", () => {
     expect(
-      dateField.resolve(
-        { date: null },
-        { timezone: "America/Boise" },
+      dateField.resolve?.(
         {},
-        { fieldName: "date" }
+        { timezone: "America/Boise" },
+        {} as any,
+        { fieldName: "date" } as any
       )
     ).toBeNull()
   })
--- src/schema/v2/fields/date.ts
+++ src/schema/v2/fields/date.ts
@@ -3,44 +3,65 @@ import { GraphQLString, GraphQLFieldConfig } from "graphql"
 import { ResolverContext } from "types/graphql"
 import { snakeCase } from "lodash"
 
-export function date(rawDate, format, timezone) {
+export const formatDate = (
+  rawDate: string,
+  format?: string | null,
+  timezone?: string
+) => {
+  if (timezone && format) {
+    return moment(rawDate).tz(timezone).format(format)
+  }
+
   if (timezone) {
-    if (format) {
-      return moment(rawDate).tz(timezone).format(format)
-    }
     return moment(rawDate).tz(timezone).format()
   }
-  if (format) return moment.utc(rawDate).format(format)
+
+  if (format) {
+    return moment.utc(rawDate).format(format)
+  }
+
   return rawDate
 }
 
-export interface DateSource {
-  format: string
-  timezone: string
+type Value = string | null | undefined
+
+export const date = <T>(
+  fn?: (response: T) => Value
+): GraphQLFieldConfig<T, ResolverContext> => {
+  return {
+    type: GraphQLString,
+    args: {
+      format: { type: GraphQLString },
+      timezone: {
+        type: GraphQLString,
+        description:
+          'A tz database time zone, otherwise falls back to "X-TIMEZONE" header. See http://www.iana.org/time-zones, https://en.wikipedia.org/wiki/List_of_tz_database_time_zones',
+      },
+    },
+    resolve: (
+      obj,
+      { format, timezone },
+      { defaultTimezone },
+      { fieldName }
+    ) => {
+      const value: Value = fn
+        ? fn(obj)
+        : obj[snakeCase(fieldName)] ?? obj[fieldName]
+
+      if (!value) {
+        return null
+      }
+
+      const timezoneString = timezone ? timezone : defaultTimezone
+
+      return formatDate(value, format, timezoneString)
+    },
+  }
 }
 
-const dateField: GraphQLFieldConfig<DateSource, ResolverContext> = {
-  type: GraphQLString,
-  args: {
-    format: {
-      type: GraphQLString,
-    },
-    // Accepts a tz database timezone string. See http://www.iana.org/time-zones,
-    // https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
-    timezone: {
-      type: GraphQLString,
-      description:
-        "A tz database time zone, otherwise falls back to `X-TIMEZONE` header",
-    },
-  },
-  resolve: (obj, { format, timezone }, { defaultTimezone }, { fieldName }) => {
-    const rawDate = obj[fieldName] || obj[snakeCase(fieldName)]
-    if (!rawDate) {
-      return null
-    }
-    const timezoneString = timezone ? timezone : defaultTimezone
-    return date(rawDate, format, timezoneString)
-  },
-}
+/**
+ * @deprecated: Use the type-safe date function
+ */
+const dateField = date()
 
 export default dateField
