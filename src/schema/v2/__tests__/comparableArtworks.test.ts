6a053f13b84bd0ad17ab8de3187cdcb44167c320
Gon4ar5 <stas.gon4ar@rambler.ru>
2021-10-12 17:39:18 +0300

    add test

---
+++ src/schema/v2/__tests__/comparableArtworks.test.ts
@@ -0,0 +1,33 @@
+/* eslint-disable promise/always-return */
+import { runQuery } from "schema/v2/test/utils"
+
+const mockComparableArtworkresult = {
+  id: "foo-bar",
+  sale_date_text: "10-12-2020",
+  currency: "EUR",
+}
+
+describe("ComparableArtwork type", () => {
+  it("fetches an comparableArtwork by ID", () => {
+    const query = `
+      {
+        comparableArtworks(id: "foo-bar") {
+          currency
+          saleDateText
+        }
+      }
+    `
+
+    const context = {
+      comparableArtworksLoader: jest.fn(() =>
+        Promise.resolve(mockComparableArtworkresult)
+      ),
+    }
+
+    return runQuery(query, context!).then((data) => {
+      console.log(data)
+      expect(data.auctionResult.currency).toBe("EUR")
+      expect(data.auctionResult.saleDateText).toEqual("10-12-2020")
+    })
+  })
+})
--- src/schema/v2/artwork/index.ts
+++ src/schema/v2/artwork/index.ts
@@ -59,7 +59,6 @@ import { getMicrofunnelDataByArtworkInternalID } from "../artist/targetSupply/ut
 import { InquiryQuestionType } from "../inquiry_question"
 import { priceDisplayText } from "lib/moneyHelpers"
 import { LocationType } from "schema/v2/location"
-import { ComparableArtworks } from "./comparableArtworks"
 
 const has_price_range = (price) => {
   return new RegExp(/-/).test(price)
@@ -1118,7 +1117,6 @@ export const ArtworkType = new GraphQLObjectType<any, ResolverContext>({
         type: GraphQLFloat,
         resolve: ({ size_score }) => size_score,
       },
-      comparableArtworks: ComparableArtworks,
       sizeBucket: {
         description:
           "size bucket assigned to an artwork based on its dimensions",
