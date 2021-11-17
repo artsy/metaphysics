f34fc2bc53a3042cd034718dbc2f4e8697f10c7a
dzucconi <mail@damonzucconi.com>
2021-11-17 15:30:27 -0500

    fix(me): fixes pagination count, cursors, and page/size support

--- src/schema/v2/me/followed_artists.ts
+++ src/schema/v2/me/followed_artists.ts
@@ -1,16 +1,16 @@
 import Artist from "schema/v2/artist"
 import { InternalIDFields } from "schema/v2/object_identification"
-
 import { pageable } from "relay-cursor-paging"
-import { connectionDefinitions } from "graphql-relay"
 import {
   GraphQLObjectType,
   GraphQLBoolean,
   GraphQLFieldConfig,
   GraphQLString,
+  GraphQLInt,
 } from "graphql"
 import { ResolverContext } from "types/graphql"
 import followArtistsResolver from "lib/shared_resolvers/followedArtistsResolver"
+import { connectionWithCursorInfo } from "../fields/pagination"
 
 const FollowArtistType = new GraphQLObjectType<any, ResolverContext>({
   name: "FollowArtist",
@@ -26,11 +26,11 @@ const FollowArtistType = new GraphQLObjectType<any, ResolverContext>({
 })
 
 const FollowedArtists: GraphQLFieldConfig<void, ResolverContext> = {
-  type: connectionDefinitions({ nodeType: FollowArtistType }).connectionType,
+  type: connectionWithCursorInfo({ nodeType: FollowArtistType }).connectionType,
   args: pageable({
-    fairID: {
-      type: GraphQLString,
-    },
+    fairID: { type: GraphQLString },
+    page: { type: GraphQLInt },
+    size: { type: GraphQLInt },
   }),
   description: "A Connection of followed artists by current user",
   resolve: (_parent, { fairID, ...connectionArgs }, context) =>
