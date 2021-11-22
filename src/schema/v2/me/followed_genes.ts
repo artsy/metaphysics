ad2fa849aa4e3f68e5857713415d9ba5050dc35a
dzucconi <mail@damonzucconi.com>
2021-11-22 16:50:22 -0500

    fix(followed-genes): completes pagination implementation

--- src/schema/v2/me/followed_genes.ts
+++ src/schema/v2/me/followed_genes.ts
@@ -1,10 +1,13 @@
 import Gene from "schema/v2/gene"
 import { InternalIDFields } from "schema/v2/object_identification"
-
-import { pageable, getPagingParameters } from "relay-cursor-paging"
-import { connectionDefinitions, connectionFromArraySlice } from "graphql-relay"
-import { GraphQLObjectType, GraphQLFieldConfig } from "graphql"
+import { pageable } from "relay-cursor-paging"
+import { GraphQLObjectType, GraphQLFieldConfig, GraphQLInt } from "graphql"
 import { ResolverContext } from "types/graphql"
+import {
+  connectionWithCursorInfo,
+  paginationResolver,
+} from "../fields/pagination"
+import { convertConnectionArgsToGravityArgs } from "lib/helpers"
 
 export const FollowGeneType = new GraphQLObjectType<any, ResolverContext>({
   name: "FollowGene",
@@ -17,25 +20,26 @@ export const FollowGeneType = new GraphQLObjectType<any, ResolverContext>({
 })
 
 const FollowedGenes: GraphQLFieldConfig<void, ResolverContext> = {
-  type: connectionDefinitions({ nodeType: FollowGeneType }).connectionType,
-  args: pageable({}),
+  type: connectionWithCursorInfo({ nodeType: FollowGeneType }).connectionType,
+  args: pageable({
+    page: { type: GraphQLInt },
+    size: { type: GraphQLInt },
+  }),
   description: "A list of the current user’s inquiry requests",
-  resolve: (_root, options, { followedGenesLoader }) => {
+  resolve: async (_root, args, { followedGenesLoader }) => {
     if (!followedGenesLoader) return null
 
-    const { limit: size, offset } = getPagingParameters(options)
-    const gravityArgs = {
+    const { page, size, offset } = convertConnectionArgsToGravityArgs(args)
+
+    const { body, headers } = await followedGenesLoader({
       size,
       offset,
       total_count: true,
-    }
-
-    return followedGenesLoader(gravityArgs).then(({ body, headers }) => {
-      return connectionFromArraySlice(body, options, {
-        arrayLength: parseInt(headers["x-total-count"] || "0", 10),
-        sliceStart: offset,
-      })
     })
+
+    const totalCount = parseInt(headers["x-total-count"] || "0", 10)
+
+    return paginationResolver({ totalCount, offset, page, size, body, args })
   },
 }
 
