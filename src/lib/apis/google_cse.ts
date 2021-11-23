1171790b148b73771383d87684a01e184f292902
Ozzie Vasdi <29984068+ovasdi@users.noreply.github.com>
2021-11-23 17:13:40 -0500

    remove GOOGLE_CSE remnants

--- src/lib/apis/google_cse.ts
+++
@@ -1,7 +0,0 @@
-import urljoin from "url-join"
-import fetch from "./fetch"
-import config from "config"
-
-const { GOOGLE_CSE_API_BASE } = config
-
-export default (path) => fetch(urljoin(GOOGLE_CSE_API_BASE, path))
