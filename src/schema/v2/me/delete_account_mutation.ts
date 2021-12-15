01e3f8b075f7c980708248fc620add98ffd03234
Ana Lisa Sutherland <sutherlandanalisa@gmail.com>
2021-12-15 13:30:25 -0800

    Add basic account delete mutation
    Co-authored-by: Ana Lisa Sutherland <sutherlandanalisa@gmail.com>

---
+++ src/schema/v2/me/delete_account_mutation.ts
@@ -0,0 +1,74 @@
+import { GraphQLBoolean, GraphQLObjectType, GraphQLUnionType } from "graphql"
+import { mutationWithClientMutationId } from "graphql-relay"
+import {
+  formatGravityError,
+  GravityMutationErrorType,
+} from "lib/gravityErrorHandler"
+import { ResolverContext } from "types/graphql"
+
+const AccountMutationDeleteSuccessType = new GraphQLObjectType<
+  any,
+  ResolverContext
+>({
+  name: "AccountMutationSuccess",
+  isTypeOf: (data) => {
+    return data._type !== "GravityMutationError"
+  },
+  fields: () => ({
+    success: {
+      type: GraphQLBoolean,
+      resolve: () => true,
+    },
+  }),
+})
+
+const AccountMutationFailureType = new GraphQLObjectType<any, ResolverContext>({
+  name: "AccountMutationFailure",
+  isTypeOf: (data) => {
+    return data._type === "GravityMutationError"
+  },
+  fields: () => ({
+    mutationError: {
+      type: GravityMutationErrorType,
+      resolve: (err) => (typeof err.message === "object" ? err.message : err),
+    },
+  }),
+})
+
+export const AccountMutationType = new GraphQLUnionType({
+  name: "AccountMutationType",
+  types: [AccountMutationDeleteSuccessType, AccountMutationFailureType],
+})
+
+export const deleteUserAccountMutation = mutationWithClientMutationId<
+  any,
+  any,
+  ResolverContext
+>({
+  name: "DeleteAccount",
+  description: "Delete User Artsy Account",
+  inputFields: {},
+  outputFields: {
+    userAccountOrError: {
+      type: AccountMutationType,
+      resolve: (result) => result,
+    },
+  },
+  mutateAndGetPayload: (_args, { deleteUserAccountLoader }) => {
+    if (!deleteUserAccountLoader) {
+      throw new Error("You need to be signed in to perform this action")
+    }
+
+    return deleteUserAccountLoader()
+      .then((result) => result)
+      .catch((error) => {
+        const formattedErr = formatGravityError(error)
+
+        if (formattedErr) {
+          return { ...formattedErr, _type: "GravityMutationError" }
+        } else {
+          throw new Error(error)
+        }
+      })
+  },
+})
