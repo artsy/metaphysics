c68d2c939b104bf5d6e9cfff822e35f9f17b1300
Cameron Rollheiser <icirellik@gmail.com>
2021-12-16 10:29:32 -0500

    chore: Remove Appmetrics (#3616)
    `appmetrics` is blocking the upgrade to Node 16 and after a
    comprehensive investigation it was determined that all of hte
    metrics (except loop latency) were already tracked by Datadog and the
    information reported by loop latency was still available albeit by
    combining existing metrics.
    
    If it turns out we still need loop latency Node 16 has built in support
    for tracking this value.

--- src/lib/stats.js
+++ src/lib/stats.js
@@ -1,5 +1,4 @@
 import os from "os"
-import _ from "lodash"
 import StatsD from "hot-shots"
 import config from "config"
 import { error } from "./loggers"
@@ -15,22 +14,6 @@ export function createStatsClient() {
 
   const isProd = NODE_ENV === "production"
   const enableMetrics = ENABLE_METRICS === "true"
-  const appMetricsDisable = [
-    "http",
-    "http-outbound",
-    "mongo",
-    "socketio",
-    "mqlight",
-    "postgresql",
-    "mqtt",
-    "mysql",
-    "redis",
-    "riak",
-    "memcached",
-    "oracledb",
-    "oracle",
-    "strong-oracle",
-  ]
 
   const statsClient = new StatsD({
     host: STATSD_HOST,
@@ -43,42 +26,6 @@ export function createStatsClient() {
   })
 
   if (enableMetrics && isProd) {
-    const appmetrics = require("appmetrics")
-    appmetrics.configure({
-      mqtt: "off",
-    })
-    const monitoring = appmetrics.monitor()
-    _.forEach(appMetricsDisable, (val, idx) => {
-      appmetrics.disable(val)
-    })
-
-    monitoring.on("loop", (loopMetrics) => {
-      statsClient.timing("loop.count_per_five_seconds", loopMetrics.count)
-      statsClient.timing("loop.minimum_loop_duration", loopMetrics.minimum)
-      statsClient.timing("loop.maximum_loop_duration", loopMetrics.maximum)
-      statsClient.timing("loop.cpu_usage_in_userland", loopMetrics.cpu_user)
-      statsClient.timing("loop.cpu_usage_in_system", loopMetrics.cpu_system)
-    })
-
-    monitoring.on("eventloop", (eventloopMetrics) => {
-      statsClient.timing("eventloop.latency.min", eventloopMetrics.latency.min)
-      statsClient.timing("eventloop.latency.max", eventloopMetrics.latency.max)
-      statsClient.timing("eventloop.latency.avg", eventloopMetrics.latency.avg)
-    })
-
-    monitoring.on("memory", (memoryMetrics) => {
-      statsClient.gauge("memory.physical", memoryMetrics.physical)
-      statsClient.gauge("memory.virtual", memoryMetrics.virtual)
-    })
-
-    monitoring.on("gc", (gcMetrics) => {
-      statsClient.gauge("gc.heap_size", gcMetrics.size)
-      statsClient.gauge("gc.heap_used", gcMetrics.used)
-      statsClient.timing("gc.sweep_duration", gcMetrics.duration, {
-        sweep_type: gcMetrics.type,
-      })
-    })
-
     setInterval(() => {
       statsClient.gauge(
         "process.active_handles",
