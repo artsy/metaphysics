2ee08c26f44cc58ce6218880c2096936feea1708
Matt Jones <matt.jones@artsymail.com>
2021-12-17 12:12:05 -0500

    fix: update master to main in force schema url

--- peril/compareForceSchema.ts
+++ peril/compareForceSchema.ts
@@ -7,21 +7,23 @@ import { warn, danger } from "danger"
 // and the current Force one, warn.
 export default async () => {
   const forceSchemaUrl =
-    "https://github.com/artsy/force/raw/master/data/schema.graphql"
+    "https://github.com/artsy/force/raw/main/data/schema.graphql"
 
   const forceSchema = await (await fetch(forceSchemaUrl)).text()
   const repo = danger.github.pr.head.repo.full_name
   const sha = danger.github.pr.head.sha
-  const localSchema = await (await fetch(
-    `https://raw.githubusercontent.com/${repo}/${sha}/_schemaV2.graphql`
-  )).text()
+  const localSchema = await (
+    await fetch(
+      `https://raw.githubusercontent.com/${repo}/${sha}/_schemaV2.graphql`
+    )
+  ).text()
 
   const allChanges = schemaDiff(
     buildSchema(forceSchema),
     buildSchema(localSchema)
   )
-  const breakings = allChanges.filter(c => c.criticality.level === "BREAKING")
-  const messages = breakings.map(c => c.message)
+  const breakings = allChanges.filter((c) => c.criticality.level === "BREAKING")
+  const messages = breakings.map((c) => c.message)
   if (messages.length) {
     warn(
       `The V2 schema in this PR has breaking changes with Force. Remember to update the Force schema if necessary.
