cc331f1de2de4431f75db9ea91ad790ab9f09539
Jon Allured <jon.allured@gmail.com>
2022-02-02 14:20:35 -0600

    Add api key to geolocation calls

--- src/schema/v2/requestLocation.ts
+++ src/schema/v2/requestLocation.ts
@@ -31,7 +31,8 @@ export const RequestLocationField: GraphQLFieldConfig<void, ResolverContext> = {
     },
   },
   resolve: (_root, args) => {
-    return fetch(`https://freegeoip.app/json/${args.ip}`)
+    const url = `https://freegeoip.app/json/${args.ip}?apikey=${config.FREEGEOIP_API_KEY}`
+    return fetch(url)
       .then((response) => {
         if (config.ENABLE_GEOLOCATION_LOGGING) {
           const headers = response.headers
