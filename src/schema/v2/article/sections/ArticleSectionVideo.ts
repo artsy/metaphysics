91cfab5a06ccc44421df0a3a830d0086f26270b8
dzucconi <mail@damonzucconi.com>
2022-02-07 17:16:38 -0500

    feat(article): fleshes out video sections

--- src/schema/v2/article/sections/ArticleSectionVideo.ts
+++ src/schema/v2/article/sections/ArticleSectionVideo.ts
@@ -1,31 +1,65 @@
-import { GraphQLObjectType, GraphQLString } from "graphql"
+import {
+  GraphQLBoolean,
+  GraphQLEnumType,
+  GraphQLNonNull,
+  GraphQLObjectType,
+  GraphQLString,
+} from "graphql"
+import { ImageType } from "schema/v2/image"
 import { ResolverContext } from "types/graphql"
+import { extractEmbed } from "../lib/extractEmbed"
 
 export const ArticleSectionVideo = new GraphQLObjectType<any, ResolverContext>({
   name: "ArticleSectionVideo",
-  isTypeOf: (data) => {
-    return data.type === "video"
+  isTypeOf: (section) => {
+    return section.type === "video"
   },
   fields: () => ({
-    type: {
-      type: GraphQLString,
-    },
     url: {
-      type: GraphQLString,
+      type: new GraphQLNonNull(GraphQLString),
     },
     caption: {
       type: GraphQLString,
     },
-    coverImageUrl: {
-      type: GraphQLString,
-      resolve: ({ cover_image_url }) => cover_image_url,
+    image: {
+      type: ImageType,
+      resolve: ({ cover_image_url }) => {
+        if (!cover_image_url) return null
+
+        // We don't currently save image dimensions, unfortunately
+        return {
+          image_url: cover_image_url,
+        }
+      },
     },
+
     layout: {
-      type: GraphQLString,
+      type: new GraphQLEnumType({
+        name: "ArticleSectionVideoLayout",
+        values: {
+          COLUMN_WIDTH: { value: "column_width" },
+          OVERFLOW_FILLWIDTH: { value: "overflow_fillwidth" },
+          FILLWIDTH: { value: "fillwidth" },
+        },
+      }),
     },
     backgroundColor: {
       type: GraphQLString,
       resolve: ({ background_color }) => background_color,
+    },
+    embed: {
+      description: "Only YouTube and Vimeo are supported",
+      args: {
+        autoPlay: {
+          type: GraphQLBoolean,
+          defaultValue: false,
+        },
+      },
+      type: GraphQLString,
+      resolve: ({ url }, { autoPlay }) => {
+        const options = { autoplay: autoPlay ? 1 : 0 }
+        return extractEmbed(url, options)
+      },
     },
   }),
 })
