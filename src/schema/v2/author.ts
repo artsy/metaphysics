5a9031d44323871fd0533f0de6ca9a1ff33c55ab
dzucconi <mail@damonzucconi.com>
2022-02-15 12:11:59 -0500

    feat(article): fleshes out author type

--- src/schema/v2/author.ts
+++ src/schema/v2/author.ts
@@ -1,6 +1,8 @@
 import { IDFields } from "./object_identification"
 import { GraphQLString, GraphQLObjectType } from "graphql"
 import { ResolverContext } from "types/graphql"
+import { ImageType } from "./image"
+import { markdown } from "schema/v2/fields/markdown"
 
 const AuthorType = new GraphQLObjectType<any, ResolverContext>({
   name: "Author",
@@ -9,9 +11,17 @@ const AuthorType = new GraphQLObjectType<any, ResolverContext>({
     name: {
       type: GraphQLString,
     },
-    profileHandle: {
+    image: {
+      type: ImageType,
+      resolve: ({ image_url }) => {
+        if (!image_url) return null
+        return { image_url }
+      },
+    },
+    bio: markdown(({ bio }) => bio),
+    twitterHandle: {
       type: GraphQLString,
-      resolve: ({ profile_handle }) => profile_handle,
+      resolve: ({ twitter_handle }) => twitter_handle,
     },
   },
 })
