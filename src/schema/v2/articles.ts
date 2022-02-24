e9158b4aace8f4fa4f7543f9cb7dc52888bb1295
dzucconi <mail@damonzucconi.com>
2022-02-24 10:55:54 -0500

    feat(articles): expands args

--- src/schema/v2/articles.ts
+++ src/schema/v2/articles.ts
@@ -5,15 +5,18 @@ import {
   GraphQLBoolean,
   GraphQLList,
   GraphQLFieldConfig,
+  GraphQLNonNull,
+  GraphQLInt,
 } from "graphql"
 import { ResolverContext } from "types/graphql"
 import { ArticleLayoutEnum } from "./article/models"
 
 const Articles: GraphQLFieldConfig<void, ResolverContext> = {
-  type: new GraphQLList(Article.type),
+  type: new GraphQLNonNull(new GraphQLList(new GraphQLNonNull(Article.type))),
   description: "A list of Articles",
   args: {
     auctionID: { type: GraphQLString },
+    channelID: { type: GraphQLString },
     featured: { type: GraphQLBoolean },
     ids: {
       type: new GraphQLList(GraphQLString),
@@ -23,17 +26,21 @@ const Articles: GraphQLFieldConfig<void, ResolverContext> = {
       `,
     },
     layout: { type: ArticleLayoutEnum },
+    limit: { type: GraphQLInt },
+    offset: { type: GraphQLInt },
+    omit: { type: new GraphQLList(new GraphQLNonNull(GraphQLString)) },
     published: { type: GraphQLBoolean, defaultValue: true },
     showID: { type: GraphQLString },
     sort: ArticleSorts,
   },
   resolve: async (
     _root,
-    { auctionID, showID, ...rest },
+    { auctionID, channelID, showID, ...rest },
     { articlesLoader }
   ) => {
     const articles = await articlesLoader({
       auction_id: auctionID,
+      channel_id: channelID,
       show_id: showID,
       ...rest,
     })
