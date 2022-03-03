697810577862e417721372200f81eb19c5ab69c7
George Kartalis <g.kartalis7@gmail.com>
2022-03-03 19:27:31 +0100

    feat(alerts): add support for new colors (#3824)
    * feat(colors): add support for new colors
    
    * test(colors): remove obsolete color names
    
    Co-authored-by: Anandaroop Roy <anandaroop.roy+github@gmail.com>

--- src/lib/colors.ts
+++ src/lib/colors.ts
@@ -1,4 +1,5 @@
-export const COLORS = [
+// TODO: delete after all artworks are reindexed to use new colors only
+export const OLD_COLORS = [
   {
     value: "black-and-white",
     name: "Black and White",
@@ -76,5 +77,62 @@ export const COLORS = [
     name: "Dark Green",
     backgroundColor: "#004600",
     foregroundColor: "#fff",
+  },
+]
+
+export const COLORS = [
+  {
+    value: "red",
+    name: "Red",
+    backgroundColor: "#BB392D",
+    foregroundColor: "#fff",
+  },
+  {
+    value: "orange",
+    name: "Orange",
+    backgroundColor: "#EA6B1F",
+    foregroundColor: "#000",
+  },
+  {
+    value: "yellow",
+    name: "Yellow",
+    backgroundColor: "#E2B929",
+    foregroundColor: "#000",
+  },
+  {
+    value: "green",
+    name: "Green",
+    backgroundColor: "#00674A",
+    foregroundColor: "#fff",
+  },
+  {
+    value: "blue",
+    name: "Blue",
+    backgroundColor: "#0A1AB4",
+    foregroundColor: "#fff",
+  },
+  {
+    value: "purple",
+    name: "Purple",
+    backgroundColor: "#7B3D91",
+    foregroundColor: "#fff",
+  },
+  {
+    value: "black-and-white",
+    name: "Black and White",
+    backgroundColor: "#000",
+    foregroundColor: "#f00",
+  },
+  {
+    value: "gray",
+    name: "Gray",
+    backgroundColor: "#C2C2C2",
+    foregroundColor: "#000",
+  },
+  {
+    value: "pink",
+    name: "Pink",
+    backgroundColor: "#E1ADCD",
+    foregroundColor: "#000",
   },
 ]
