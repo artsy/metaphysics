4b7e9a028c461af4c078bb8b9db1f20dc5f64ac3
Sultan Al-Maari <sultan.al-maari@artsymail.com>
2022-04-05 19:19:06 +0200

    update vortex loader to accept variables

--- src/lib/loaders/loaders_with_authentication/vortex.ts
+++ src/lib/loaders/loaders_with_authentication/vortex.ts
@@ -1,7 +1,15 @@
 import factories from "../api"
 import config from "config"
+import { GraphQLError } from "graphql"
+import fetch from "node-fetch"
+import urljoin from "url-join"
 
-const { VORTEX_APP_ID } = config
+const { VORTEX_APP_ID, VORTEX_API_BASE } = config
+
+interface GraphQLArgs {
+  query: string
+  variables: any
+}
 
 export default (accessToken, opts) => {
   const gravityAccessTokenLoader = () => Promise.resolve(accessToken)
@@ -11,8 +19,15 @@ export default (accessToken, opts) => {
     vortexLoaderWithAuthenticationFactory,
   } = factories(opts)
 
+  const vortexAccessTokenLoader = () =>
+    vortexTokenLoader().then((data) => data.token)
+
   const gravityLoader = gravityLoaderWithAuthenticationFactory(
     gravityAccessTokenLoader
+  )
+
+  const vortexLoader = vortexLoaderWithAuthenticationFactory(
+    vortexAccessTokenLoader
   )
 
   // This generates a token with a lifetime of 1 minute, which should be plenty of time to fulfill a full query.
@@ -22,15 +37,42 @@ export default (accessToken, opts) => {
     { method: "POST" }
   )
 
-  const vortexAccessTokenLoader = () =>
-    vortexTokenLoader().then((data) => data.token)
+  const vortexGraphQLLoader = async <T = unknown>({
+    query,
+    variables,
+  }: GraphQLArgs): Promise<Record<string, T>> => {
+    const { token } = await vortexTokenLoader()
 
-  const vortexLoader = vortexLoaderWithAuthenticationFactory(
-    vortexAccessTokenLoader
-  )
+    const body = JSON.stringify({ query, variables })
+
+    const response = await fetch(urljoin(VORTEX_API_BASE, "graphql"), {
+      method: "POST",
+      body,
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    })
+
+    const json = await response.json()
+    const { data: vortexData, error, errors: vortexErrors } = json
+
+    if (error) {
+      throw new Error(`[loaders/vortex.ts] ${error.message}`)
+    } else if (vortexErrors) {
+      const errors = vortexErrors.reduce((acc, error) => {
+        return acc + " " + error["message"]
+      }, "[vortex]: Error ")
+
+      throw new GraphQLError(errors)
+    } else {
+      return vortexData
+    }
+  }
 
   return {
     vortexTokenLoader,
+    vortexGraphqlLoaderWithVariables: vortexGraphQLLoader,
     vortexGraphqlLoader: (body) =>
       vortexLoader("/graphql", body, {
         method: "POST",
