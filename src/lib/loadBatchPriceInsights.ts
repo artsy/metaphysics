ab417bbfd7d2e86b602334602fbed9fa6dc49e84
Sultan Al-Maari <sultan.al-maari@artsymail.com>
2022-04-07 18:07:27 +0200

    resolve comments

--- src/lib/loadBatchPriceInsights.ts
+++ src/lib/loadBatchPriceInsights.ts
@@ -1,16 +1,17 @@
 import gql from "lib/gql"
 import { extractNodes } from "lib/helpers"
+import { StaticPathLoader } from "./loaders/api/loader_interface"
 
 const MAX_MARKET_PRICE_INSIGHTS = 50
 
 export const loadBatchPriceInsights = async (
-  artistIDMediumTuples: any[],
-  vortexGraphQLLoader: any
+  artistIDMediumTuples: { artistID: string; medium: string }[],
+  vortexGraphQLLoader: ({ query, variables }) => StaticPathLoader<any>
 ) => {
   try {
     const vortexResult = await vortexGraphQLLoader({
       query: gql`
-        query marketPriceInsightsBatchQuery($artistIDMediumTuples: [ArtistIdMediumTupleType!]!) {
+        query MarketPriceInsightsBatchQuery($artistIDMediumTuples: [ArtistIdMediumTupleType!]!) {
           marketPriceInsightsBatch(input: $artistIDMediumTuples, first: ${MAX_MARKET_PRICE_INSIGHTS}) {
             totalCount
             edges {
