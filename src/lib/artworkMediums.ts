bd70bd40a0903671a733ff4514c51e49cb036a99
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2022-04-08 12:27:38 -0400

    feat: expose a medium type's corresponding filter gene (#3937)

--- src/lib/artworkMediums.ts
+++ src/lib/artworkMediums.ts
@@ -4,12 +4,14 @@ export default {
     long_description:
       "Includes architectural models; buildings (e.g., house, temple).",
     name: "Architecture",
+    mediumFilterGeneSlug: "architecture-1",
   },
   "Books and Portfolios": {
     id: "Books and Portfolios",
     long_description:
       "Includes albums; artist books; illustrated manuscripts; portfolios (i.e., a group of works such as photographs or prints).",
     name: "Books and Portfolios",
+    mediumFilterGeneSlug: "books-and-portfolios",
   },
   "Design/Decorative Art": {
     id: "Design/Decorative Art",
@@ -18,6 +20,7 @@ export default {
       "Includes containers (e.g., basket, bowl, box, vase); furniture (e.g., cabinet, chair, table); lighting; mirrors; tableware (e.g., flatware, plates).",
     ].join(" "),
     name: "Design/Decorative Art",
+    mediumFilterGeneSlug: "design",
   },
   "Drawing, Collage or other Work on Paper": {
     id: "Drawing, Collage or other Work on Paper",
@@ -26,6 +29,7 @@ export default {
       "This also includes paintings where paper is the support.",
     ].join(" "),
     name: "Drawing, Collage or other Work on Paper",
+    mediumFilterGeneSlug: "work-on-paper",
   },
   "Ephemera or Merchandise": {
     id: "Ephemera or Merchandise",
@@ -34,57 +38,67 @@ export default {
       "This includes exhibition materials, memorabilia, autographs, skateboards, toys, and other items made with branded collaborators.",
     ].join(" "),
     name: "Ephemera or Merchandise",
+    mediumFilterGeneSlug: "ephemera-or-merchandise",
   },
   "Fashion Design and Wearable Art": {
     id: "Fashion Design and Wearable Art",
     long_description: "Includes costumes; garments (e.g., dress, jacket).",
     name: "Fashion Design and Wearable Art",
+    mediumFilterGeneSlug: "fashion-design-and-wearable-art",
   },
   Installation: {
     id: "Installation",
     long_description: "Includes land art; installation art; site-specific art.",
     name: "Installation",
+    mediumFilterGeneSlug: "installation",
   },
   Jewelry: {
     id: "Jewelry",
     long_description:
       "Includes bracelets; brooches; earrings; necklaces; rings; costume accessories.",
     name: "Jewelry",
+    mediumFilterGeneSlug: "jewelry",
   },
   "Mixed Media": {
     id: "Mixed Media",
     long_description:
       "Intermedia; Multimedia work that does not fall into another category.",
     name: "Mixed Media",
+    mediumFilterGeneSlug: "mixed-media",
   },
   NFT: {
     id: "NFT",
     long_description:
       "Non-fungible token. Indicates that the primary medium of the work is the NFT.",
     name: "NFT",
+    mediumFilterGeneSlug: "nft",
   },
   Other: {
     id: "Other",
     long_description:
       "Includes items that do not fall into any other categories (e.g., armor; musical instruments; tools).",
     name: "Other",
+    mediumFilterGeneSlug: null,
   },
   Painting: {
     id: "Painting",
     long_description:
       "Includes gouache; fresco; ink and wash; oil painting; screen painting; scroll painting; tempera; watercolor.",
     name: "Painting",
+    mediumFilterGeneSlug: "painting",
   },
   "Performance Art": {
     id: "Performance Art",
     long_description: "Includes body art; endurance art; live performance.",
     name: "Performance Art",
+    mediumFilterGeneSlug: "performance-art",
   },
   Photography: {
     id: "Photography",
     long_description:
       "Includes chromogenic color prints (c-prints); polaroids; silver gelatin prints; photograms.",
     name: "Photography",
+    mediumFilterGeneSlug: "photography",
   },
   Posters: {
     id: "Posters",
@@ -93,12 +107,14 @@ export default {
       "Please note that this does not include exhibition posters (see “Ephemera or Merchandise”).",
     ].join(" "),
     name: "Posters",
+    mediumFilterGeneSlug: "poster",
   },
   Print: {
     id: "Print",
     long_description:
       "Includes etchings; engravings; lithographs; monoprints; screen prints; woodcuts.",
     name: "Print",
+    mediumFilterGeneSlug: "prints",
   },
   Reproduction: {
     id: "Reproduction",
@@ -108,6 +124,7 @@ export default {
       "Please note that the artist may not have been directly involved in the work’s production.",
     ].join(" "),
     name: "Reproduction",
+    mediumFilterGeneSlug: "reproduction",
   },
   Sculpture: {
     id: "Sculpture",
@@ -116,17 +133,20 @@ export default {
       "For figurines made with branded collaborators (e.g., BE@RBRICK, Medicom), please see “Ephemera or Merchandise.”",
     ].join(" "),
     name: "Sculpture",
+    mediumFilterGeneSlug: "sculpture",
   },
   "Textile Arts": {
     id: "Textile Arts",
     long_description:
       "Includes banners; carpets; rugs; samplers; tapestries; weavings.",
     name: "Textile Arts",
+    mediumFilterGeneSlug: "textile-arts",
   },
   "Video/Film/Animation": {
     id: "Video/Film/Animation",
     long_description:
       "Includes 16mm film; computer animation; video recordings.",
     name: "Video/Film/Animation",
+    mediumFilterGeneSlug: "film-slash-video",
   },
 }
