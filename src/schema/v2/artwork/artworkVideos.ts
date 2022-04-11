bc1a060d25e8626796b593a41e8c316e00103d07
Laura Bhayani <laura@artsymail.com>
2022-04-11 17:49:35 -0400

    (artwork page) replace urls to point to vimeo player

--- src/schema/v2/artwork/artworkVideos.ts
+++ src/schema/v2/artwork/artworkVideos.ts
@@ -1,51 +1,61 @@
 export const videoList = {
   "keith-grafton-new-paradigm": {
-    src: "https://vimeo.com/696646345/cb5e5856b6",
+    src:
+      "https://player.vimeo.com/video/696646345?h=cb5e5856b6&amp;app_id=122963",
     height: 360,
     width: 360,
   },
   "keith-grafton-it-is-essence-time": {
-    src: "https://vimeo.com/696646494/1065d39609",
+    src:
+      "https://player.vimeo.com/video/696646494?h=1065d39609&amp;app_id=122963",
     height: 360,
     width: 360,
   },
   "keith-grafton-embracing-it-all": {
-    src: "https://vimeo.com/696673255/6a0d62aab5",
+    src:
+      "https://player.vimeo.com/video/696646494?h=1065d39609&amp;app_id=122963",
     height: 360,
     width: 360,
   },
   "keith-grafton-wholeness": {
-    src: "https://vimeo.com/696646582/99c83b0b8b",
+    src:
+      "https://player.vimeo.com/video/696646582?h=99c83b0b8b&amp;app_id=122963",
     height: 360,
     width: 360,
   },
   "keith-grafton-together-by-nature": {
-    src: "https://vimeo.com/696646093/ee9f16cb63",
+    src:
+      "https://player.vimeo.com/video/696646093?h=ee9f16cb63&amp;app_id=122963",
     height: 360,
     width: 360,
   },
   "hazel-reeves-blow-the-clouds-away": {
-    src: "https://vimeo.com/696649176/d0233b38a7",
+    src:
+      "https://player.vimeo.com/video/696649176?h=d0233b38a7&amp;app_id=122963",
     height: 360,
     width: 640,
   },
   "laura-beaumont-heroes-and-heroines-1": {
-    src: "https://vimeo.com/696647919/70ff73bdf1",
+    src:
+      "https://player.vimeo.com/video/696647919?h=70ff73bdf1&amp;app_id=122963",
     height: 360,
     width: 640,
   },
   "johannes-von-stumm-contemplation-ii": {
-    src: "https://vimeo.com/696646093/ee9f16cb63",
+    src:
+      "https://player.vimeo.com/video/696646093?h=ee9f16cb63&amp;app_id=122963",
     height: 360,
     width: 640,
   },
   "richard-chapman-symbiosis": {
-    src: "https://vimeo.com/696647483/117d28a4dc",
+    src:
+      "https://player.vimeo.com/video/696647483?h=117d28a4dc&amp;app_id=122963",
     height: 360,
     width: 640,
   },
   "rosie-jones-the-white-goddess-ii": {
-    src: "https://vimeo.com/696673641/980eb58e61",
+    src:
+      "https://player.vimeo.com/video/696673641?h=980eb58e61&amp;app_id=122963",
     height: 360,
     width: 640,
   },
