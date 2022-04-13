3c8644365946bb1614b189b1199972f3cdb562c7
Laura Bhayani <laura@artsymail.com>
2022-04-13 13:19:48 -0400

    add artworkVideos data and Video type.

---
+++ src/schema/v2/artwork/artworkVideos.ts
@@ -0,0 +1,62 @@
+export const videoList = {
+  "keith-grafton-new-paradigm": {
+    src:
+      "https://player.vimeo.com/video/696646345?h=cb5e5856b6&amp;app_id=122963",
+    height: 360,
+    width: 360,
+  },
+  "keith-grafton-it-is-essence-time": {
+    src:
+      "https://player.vimeo.com/video/696646494?h=1065d39609&amp;app_id=122963",
+    height: 360,
+    width: 360,
+  },
+  "keith-grafton-embracing-it-all": {
+    src:
+      "https://player.vimeo.com/video/696646494?h=1065d39609&amp;app_id=122963",
+    height: 360,
+    width: 360,
+  },
+  "keith-grafton-wholeness": {
+    src:
+      "https://player.vimeo.com/video/696646582?h=99c83b0b8b&amp;app_id=122963",
+    height: 360,
+    width: 360,
+  },
+  "keith-grafton-together-by-nature": {
+    src:
+      "https://player.vimeo.com/video/696646093?h=ee9f16cb63&amp;app_id=122963",
+    height: 360,
+    width: 360,
+  },
+  "hazel-reeves-blow-the-clouds-away": {
+    src:
+      "https://player.vimeo.com/video/696649176?h=d0233b38a7&amp;app_id=122963",
+    height: 360,
+    width: 640,
+  },
+  "laura-beaumont-heroes-and-heroines-1": {
+    src:
+      "https://player.vimeo.com/video/696647919?h=70ff73bdf1&amp;app_id=122963",
+    height: 360,
+    width: 640,
+  },
+  "johannes-von-stumm-contemplation-ii": {
+    src:
+      "https://player.vimeo.com/video/696646093?h=ee9f16cb63&amp;app_id=122963",
+    height: 360,
+    width: 640,
+  },
+  "richard-chapman-symbiosis": {
+    src:
+      "https://player.vimeo.com/video/696647483?h=117d28a4dc&amp;app_id=122963",
+    height: 360,
+    width: 640,
+  },
+  "rosie-jones-the-white-goddess-ii": {
+    src:
+      "https://player.vimeo.com/video/696673641?h=980eb58e61&amp;app_id=122963",
+    height: 360,
+    width: 640,
+  },
+}
