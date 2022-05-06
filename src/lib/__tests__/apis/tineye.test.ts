7be9f2e83cf9be1c89d3a59ffd7304c934035f71
Dima Tretyak <itretyak@ya.ru>
2022-05-06 16:47:58 +0300

    chore: add tineye api tests

---
+++ src/lib/__tests__/apis/tineye.test.ts
@@ -0,0 +1,33 @@
+import tineye from "lib/apis/tineye"
+import config from "config"
+
+jest.mock("lib/apis/fetch", () => jest.fn())
+
+import fetch from "lib/apis/fetch"
+
+describe("TinEye", () => {
+  config.TINEYE_API_USERNAME = "username"
+  config.TINEYE_API_PASSWORD = "password"
+
+  it("makes a request with correct url and options", async () => {
+    const url =
+      "https://username:password@mobileengine.tineye.com/artsy/rest/search"
+    const options = {
+      method: "POST",
+      formData: {
+        my_field: "my_value",
+      },
+    }
+
+    await tineye("/search", options)
+
+    expect(fetch).toBeCalledWith(url, options)
+  })
+
+  it("resolves when there is a successful response", async () => {
+    ;(fetch as jest.Mock).mockResolvedValue({ foo: "bar" })
+    const response = await tineye("search", { method: "POST" })
+
+    expect(response.foo).toBe("bar")
+  })
+})
--- src/lib/apis/tineye.ts
+++ src/lib/apis/tineye.ts
@@ -2,10 +2,8 @@ import config from "../../config"
 import urljoin from "url-join"
 import fetch from "./fetch"
 
-const { TINEYE_API_PASSWORD, TINEYE_API_USERNAME } = config
-
 export default (path: string, fetchOptions) => {
-  const baseApi = `https://${TINEYE_API_USERNAME}:${TINEYE_API_PASSWORD}@mobileengine.tineye.com/artsy/rest`
+  const baseApi = `https://${config.TINEYE_API_USERNAME}:${config.TINEYE_API_PASSWORD}@mobileengine.tineye.com/artsy/rest`
   const url = urljoin(baseApi, path)
 
   return fetch(url, fetchOptions)
