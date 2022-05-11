80c3b349d8e26a4a55ae32dfbb0a0bddc8862e8a
Dima Tretyak <itretyak@ya.ru>
2022-05-11 15:14:08 +0300

    chore: update reverseImageSearch tests

--- src/schema/v2/__tests__/reverseImageSearch.test.ts
+++ src/schema/v2/__tests__/reverseImageSearch.test.ts
@@ -1,14 +1,35 @@
-import { reverseImageSearchResolver } from "../reverseImageSearch"
 import { Readable } from "stream"
 import { tineyeSearch } from "lib/apis/tineye"
-
-const _ = {}
+import { Upload } from "graphql-upload"
+import { runQuery } from "schema/v2/test/utils"
+import gql from "lib/gql"
 
 jest.mock("lib/apis/tineye")
 
-describe("reverseImageSearchResolver", () => {
+describe("reverseImageSearch", () => {
+  const mockTineyeSearch = tineyeSearch as jest.Mock
+  let upload: Upload
+  let file
+
+  beforeEach(() => {
+    mockTineyeSearch.mockClear()
+
+    file = Readable.from(Buffer.from("helloWorld"))
+    upload = new Upload()
+
+    upload.file = file
+    upload.promise = new Promise((resolve) =>
+      resolve({
+        filename: "filename.jpg",
+        mimetype: "image/jpeg",
+        encoding: "7bit",
+        createReadStream: () => file,
+      })
+    )
+  })
+
   it("should throw error if user is not logged in", async () => {
-    await expect(reverseImageSearchResolver(_, _, _)).rejects.toThrow(
+    await expect(runQuery(query, {}, { file: upload })).rejects.toThrow(
       "You need to be signed in to perform this action"
     )
   })
@@ -18,110 +39,91 @@ describe("reverseImageSearchResolver", () => {
       meLoader: jest.fn().mockRejectedValue(new Error("Error Message")),
     }
 
-    await expect(reverseImageSearchResolver(_, _, context)).rejects.toThrow(
+    await expect(runQuery(query, context, { file: upload })).rejects.toThrow(
       "You need to be signed in to perform this action"
     )
   })
 
   it("should return results", async () => {
-    const mockTineyeSearch = tineyeSearch as jest.Mock
-    const stream = Readable.from(Buffer.from("helloWorld"))
-    const args = {
-      image: Promise.resolve({
-        filename: "filename.jpg",
-        mimetype: "image/jpeg",
-        encoding: "7bit",
-        createReadStream: () => stream,
-      }),
-    }
     const context = {
       meLoader: jest.fn().mockResolvedValue({}),
     }
 
-    mockTineyeSearch.mockResolvedValue({
-      status: "ok",
-      count: "1",
-      count_total: "1",
-      result: [
-        {
-          target_overlap_percent: 99.76,
-          query_overlap_percent: 99.95,
-          filepath: "artwork/artwork-id/image/image-id",
-          target_match_rect: {
-            top: 29.29,
-            bottom: 71.64,
-            right: 84.53,
-            left: 21.25,
-          },
-          score: 72.83,
-          match_percent: 97.26,
-          query_match_rect: {
-            top: 29.17,
-            bottom: 71.43,
-            right: 84.5,
-            left: 21,
-          },
-        },
-      ],
-      error: [],
-      query_image: { filepath: "filename.jpg" },
-      method: "search",
-    })
+    mockTineyeSearch.mockResolvedValue(TinEyeSuccessResponse)
 
-    const result = await reverseImageSearchResolver(_, args, context)
+    const result = await runQuery(query, context, { file: upload })
 
     expect(result).toMatchInlineSnapshot(`
       Object {
-        "__typename": "ReverseImageSearchResults",
-        "results": Array [
-          Object {
-            "filepath": "artwork/artwork-id/image/image-id",
-            "match_percent": 97.26,
-            "query_match_rect": Object {
-              "bottom": 71.43,
-              "left": 21,
-              "right": 84.5,
-              "top": 29.17,
+        "reverseImageSearch": Object {
+          "results": Array [
+            Object {
+              "filepath": "artwork/artwork-id/image/image-id",
+              "score": 72.83,
             },
-            "query_overlap_percent": 99.95,
-            "score": 72.83,
-            "target_match_rect": Object {
-              "bottom": 71.64,
-              "left": 21.25,
-              "right": 84.53,
-              "top": 29.29,
-            },
-            "target_overlap_percent": 99.76,
-          },
-        ],
+          ],
+        },
       }
     `)
   })
 
   it("should return errors when something went wrong", async () => {
-    const mockTineyeSearch = tineyeSearch as jest.Mock
-    const stream = Readable.from(Buffer.from("helloWorld"))
-    const args = {
-      image: Promise.resolve({
-        filename: "filename.jpg",
-        mimetype: "image/jpeg",
-        encoding: "7bit",
-        createReadStream: () => stream,
-      }),
-    }
     const context = {
       meLoader: jest.fn().mockResolvedValue({}),
     }
 
-    mockTineyeSearch.mockResolvedValue({
-      status: "fail",
-      error: ["Error message"],
-      method: "search",
-      result: [],
-    })
+    mockTineyeSearch.mockResolvedValue(TinEyeFailResponse)
 
-    await expect(reverseImageSearchResolver(_, args, context)).rejects.toThrow(
+    await expect(runQuery(query, context, { file: upload })).rejects.toThrow(
       "Error message"
     )
   })
 })
+
+const query = gql`
+  query($file: Upload!) {
+    reverseImageSearch(image: $file) {
+      results {
+        filepath
+        score
+      }
+    }
+  }
+`
+
+const TinEyeSuccessResponse = {
+  status: "ok",
+  count: "1",
+  count_total: "1",
+  result: [
+    {
+      target_overlap_percent: 99.76,
+      query_overlap_percent: 99.95,
+      filepath: "artwork/artwork-id/image/image-id",
+      target_match_rect: {
+        top: 29.29,
+        bottom: 71.64,
+        right: 84.53,
+        left: 21.25,
+      },
+      score: 72.83,
+      match_percent: 97.26,
+      query_match_rect: {
+        top: 29.17,
+        bottom: 71.43,
+        right: 84.5,
+        left: 21,
+      },
+    },
+  ],
+  error: [],
+  query_image: { filepath: "filename.jpg" },
+  method: "search",
+}
+
+const TinEyeFailResponse = {
+  status: "fail",
+  error: ["Error message"],
+  method: "search",
+  result: [],
+}
