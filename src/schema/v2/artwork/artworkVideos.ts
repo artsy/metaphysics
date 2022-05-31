5a65cc31b474135c355b3a01b3c5e549d15832f6
Laura Bhayani <laura@artsymail.com>
2022-05-31 11:41:09 -0400

    add mute query param to video urls

--- src/schema/v2/artwork/artworkVideos.ts
+++ src/schema/v2/artwork/artworkVideos.ts
@@ -1,61 +1,61 @@
 export const VIDEOS = {
   "keith-grafton-new-paradigm": {
     url:
-      "https://player.vimeo.com/video/696646345?h=cb5e5856b6&amp;app_id=122963",
+      "https://player.vimeo.com/video/696646345?h=cb5e5856b6&amp;muted=1&amp;app_id=122963",
     height: 360,
     width: 360,
   },
   "keith-grafton-it-is-essence-time": {
     url:
-      "https://player.vimeo.com/video/696646494?h=1065d39609&amp;app_id=122963",
+      "https://player.vimeo.com/video/696646494?h=1065d39609&amp;muted=1&amp;app_id=122963",
     height: 360,
     width: 360,
   },
   "keith-grafton-embracing-it-all": {
     url:
-      "https://player.vimeo.com/video/696646494?h=1065d39609&amp;app_id=122963",
+      "https://player.vimeo.com/video/696646494?h=1065d39609&amp;muted=1&amp;app_id=122963",
     height: 360,
     width: 360,
   },
   "keith-grafton-wholeness": {
     url:
-      "https://player.vimeo.com/video/696646582?h=99c83b0b8b&amp;app_id=122963",
+      "https://player.vimeo.com/video/696646582?h=99c83b0b8b&amp;muted=1&amp;app_id=122963",
     height: 360,
     width: 360,
   },
   "keith-grafton-together-by-nature": {
     url:
-      "https://player.vimeo.com/video/696646093?h=ee9f16cb63&amp;app_id=122963",
+      "https://player.vimeo.com/video/696646093?h=ee9f16cb63&amp;muted=1&amp;app_id=122963",
     height: 360,
     width: 360,
   },
   "hazel-reeves-blow-the-clouds-away": {
     url:
-      "https://player.vimeo.com/video/696649176?h=d0233b38a7&amp;app_id=122963",
+      "https://player.vimeo.com/video/696649176?h=d0233b38a7&amp;muted=1&amp;app_id=122963",
     height: 360,
     width: 640,
   },
   "laura-beaumont-heroes-and-heroines-1": {
     url:
-      "https://player.vimeo.com/video/696647919?h=70ff73bdf1&amp;app_id=122963",
+      "https://player.vimeo.com/video/696647919?h=70ff73bdf1&amp;muted=1&amp;app_id=122963",
     height: 360,
     width: 640,
   },
   "johannes-von-stumm-contemplation-ii": {
     url:
-      "https://player.vimeo.com/video/696646093?h=ee9f16cb63&amp;app_id=122963",
+      "https://player.vimeo.com/video/696646093?h=ee9f16cb63&amp;muted=1&amp;app_id=122963",
     height: 360,
     width: 640,
   },
   "richard-chapman-symbiosis": {
     url:
-      "https://player.vimeo.com/video/696647483?h=117d28a4dc&amp;app_id=122963",
+      "https://player.vimeo.com/video/696647483?h=117d28a4dc&amp;muted=1&amp;app_id=122963",
     height: 360,
     width: 640,
   },
   "rosie-jones-the-white-goddess-ii": {
     url:
-      "https://player.vimeo.com/video/696673641?h=980eb58e61&amp;app_id=122963",
+      "https://player.vimeo.com/video/696673641?h=980eb58e61&amp;muted=1&amp;app_id=122963",
     height: 360,
     width: 640,
   },
