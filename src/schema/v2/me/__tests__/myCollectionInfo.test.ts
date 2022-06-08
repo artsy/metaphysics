2da7f5fae9819eec6159bc220e462bc8344c0310
Sultan Al-Maari <sultan.al-maari@artsymail.com>
2022-06-08 18:11:55 +0200

    add tests

--- src/schema/v2/me/__tests__/myCollectionInfo.test.ts
+++ src/schema/v2/me/__tests__/myCollectionInfo.test.ts
@@ -39,4 +39,101 @@ describe("me.myCollectionInfo", () => {
       }
     `)
   })
+
+  describe("collectedArtistsConnection", () => {
+    it("returns a connection for the artists in the users' collection", async () => {
+      const query = gql`
+        {
+          me {
+            myCollectionInfo {
+              collectedArtistsConnection(first: 5) {
+                totalCount
+                edges {
+                  node {
+                    name
+                  }
+                }
+              }
+            }
+          }
+        }
+      `
+
+      const context: Partial<ResolverContext> = {
+        collectionLoader: async () => ({}),
+        meLoader: async () => ({
+          id: "some-user-id",
+        }),
+        collectionArtistsLoader: async () => ({
+          headers: { "x-total-count": "3" },
+          body: [
+            {
+              name: "Artist 1",
+            },
+            {
+              name: "Artist 2",
+            },
+            {
+              name: "Artist 3",
+            },
+          ],
+        }),
+      }
+
+      const data = await runAuthenticatedQuery(query, context)
+
+      expect(data).toEqual(collectedArtistsConnectionData)
+
+      expect(data.me.myCollectionInfo.collectedArtistsConnection)
+        .toMatchInlineSnapshot(`
+        Object {
+          "edges": Array [
+            Object {
+              "node": Object {
+                "name": "Artist 1",
+              },
+            },
+            Object {
+              "node": Object {
+                "name": "Artist 2",
+              },
+            },
+            Object {
+              "node": Object {
+                "name": "Artist 3",
+              },
+            },
+          ],
+          "totalCount": 3,
+        }
+      `)
+    })
+  })
 })
+
+const collectedArtistsConnectionData = {
+  me: {
+    myCollectionInfo: {
+      collectedArtistsConnection: {
+        totalCount: 3,
+        edges: [
+          {
+            node: {
+              name: "Artist 1",
+            },
+          },
+          {
+            node: {
+              name: "Artist 2",
+            },
+          },
+          {
+            node: {
+              name: "Artist 3",
+            },
+          },
+        ],
+      },
+    },
+  },
+}
