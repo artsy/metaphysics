c80d616a7a85007ba2d2fd1419e48cb02ec39ed0
leamotta <leamotta@msn.com>
2022-06-30 16:40:19 +0200

    Replace continental europe with European union (#4197)

--- src/schema/v2/artwork/index.ts
+++ src/schema/v2/artwork/index.ts
@@ -947,7 +947,7 @@ export const ArtworkType = new GraphQLObjectType<any, ResolverContext>({
             artwork.international_shipping_fee_cents == null
           )
             return artwork.eu_shipping_origin
-              ? "Free shipping within Continental Europe only"
+              ? "Free shipping within European Union only"
               : "Free domestic shipping only"
 
           if (
@@ -973,7 +973,7 @@ export const ArtworkType = new GraphQLObjectType<any, ResolverContext>({
           })
 
           const shippingRegion = artwork.eu_shipping_origin
-            ? "within Continental Europe"
+            ? "within European Union"
             : "domestic"
 
           if (
