19214300e50fd0ce27dc6e6b2eef845b382f48bc
Dima Tretyak (Dzmitry Tratsiak) <itretyak@ya.ru>
2022-07-22 15:18:24 +0300

    feat: add `publicUrl` field for partner artist and partner show documents (#4236)
    * feat: add publicUrl field for documents
    
    * chore: add tests

--- src/schema/v2/__tests__/partnerShowDocumentsConnection.test.ts
+++ src/schema/v2/__tests__/partnerShowDocumentsConnection.test.ts
@@ -8,14 +8,17 @@ describe("partnerShowDocumentsConnection", () => {
   beforeEach(() => {
     response = [
       {
+        uri: "partner/name/filename-one.pdf",
         filename: "filename-one.pdf",
         title: "File One",
       },
       {
+        uri: "partner/name/filename-two.pdf",
         filename: "filename-two.png",
         title: "File Two",
       },
       {
+        uri: "partner/name/filename-three.pdf",
         filename: "filename-three.jpg",
         title: "File Three",
       },
@@ -151,5 +154,36 @@ describe("partnerShowDocumentsConnection", () => {
         totalCount: 3,
       },
     })
+  })
+
+  it("returns public links", async () => {
+    const query = gql`
+      {
+        partnerShowDocumentsConnection(
+          showID: "showID"
+          partnerID: "partnerID"
+          first: 3
+        ) {
+          edges {
+            node {
+              publicUrl
+            }
+          }
+        }
+      }
+    `
+
+    const data = await runQuery(query, context)
+    const edges = data.partnerShowDocumentsConnection.edges
+
+    expect(edges[0].node.publicUrl).toBe(
+      "https://api.artsy.test/api/v1/partner/name/filename-one.pdf"
+    )
+    expect(edges[1].node.publicUrl).toBe(
+      "https://api.artsy.test/api/v1/partner/name/filename-two.pdf"
+    )
+    expect(edges[2].node.publicUrl).toBe(
+      "https://api.artsy.test/api/v1/partner/name/filename-three.pdf"
+    )
   })
 })
