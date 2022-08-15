215f1f87f5ed2afa44767fcc9705e1f44c4e269f
Matt Jones <matt.jones@artsymail.com>
2022-08-15 15:39:27 -0400

    feat: add updateAdminNotesLoader to update user mutation

--- src/schema/v2/users/updateUserMutation.ts
+++ src/schema/v2/users/updateUserMutation.ts
@@ -9,6 +9,7 @@ interface Input {
   email: string
   name: string
   phone?: string
+  adminNote?: string
 }
 
 interface GravityInput {
@@ -37,24 +38,35 @@ export const updateUserMutation = mutationWithClientMutationId<
     email: { type: GraphQLString },
     name: { type: GraphQLString },
     phone: { type: GraphQLString },
+    adminNote: { type: GraphQLString },
   },
   outputFields: {},
-  mutateAndGetPayload: async (args, { updateUserLoader }) => {
-    if (!updateUserLoader) {
+  mutateAndGetPayload: async (
+    args,
+    { updateUserLoader, updateAdminNotesLoader }
+  ) => {
+    if (!updateUserLoader || !updateAdminNotesLoader) {
       throw new Error(
         "You need to be signed in as an admin to perform this action"
       )
     }
 
-    const gravityOptions = Object.keys(args)
-      .filter((key) => key !== "id")
+    const updateUserLoaderPayload = Object.keys(args)
+      .filter((key) => key !== "id" && key !== "adminNote")
       .reduce(
         (acc, key) => ({ ...acc, [snakeCase(key)]: args[key] }),
         {} as GravityInput
       )
 
+    const updateAdminNotesLoaderPayload = {
+      body: args.adminNote,
+    }
+
     try {
-      return await updateUserLoader?.(args.id, gravityOptions)
+      if (args.adminNote) {
+        await updateAdminNotesLoader(args.id, updateAdminNotesLoaderPayload)
+      }
+      return await updateUserLoader?.(args.id, updateUserLoaderPayload)
     } catch (err) {
       if ("body" in (err as any)) {
         const e = err as GravityError
