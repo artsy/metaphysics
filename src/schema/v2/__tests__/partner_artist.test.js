1e5c9682847a2505904f23045c3baa1e21eff594
brainbicycle <brian.beckerle@artsymail.com>
2022-08-23 17:05:22 -0400

    fix failing test

--- src/schema/v2/__tests__/partner_artist.test.js
+++ src/schema/v2/__tests__/partner_artist.test.js
@@ -207,7 +207,7 @@ describe("partnerArtist", () => {
         },
       ]
       context = {
-        partnerArtistArtworksLoader: () =>
+        partnerArtistPartnerArtistArtworksLoader: () =>
           Promise.resolve({
             body: partnerArtistArtworksResponse,
             headers: {
