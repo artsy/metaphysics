5e341125dceba02d869e1854c8f90bb7b14577f9
Dima Tretyak <itretyak@ya.ru>
2022-09-15 12:55:38 +0300

    feat: use common format for mutation response

--- src/schema/v2/me/__tests__/mark_all_notifications_as_read_mutation.test.ts
+++ src/schema/v2/me/__tests__/mark_all_notifications_as_read_mutation.test.ts
@@ -3,13 +3,23 @@ import { runAuthenticatedQuery } from "schema/v2/test/utils"
 const mutation = `
   mutation {
     markAllNotificationsAsRead(input: {}) {
-      success
+      responseOrError {
+        ... on MarkAllNotificationsAsReadSuccess {
+          success
+        }
+        
+        ... on MarkAllNotificationsAsReadFailure {
+          mutationError {
+            message
+          }
+        }
+      }
     }
   }
 `
 
 describe("markAllNotificationsAsReadMutation", () => {
-  it("should return success = true when all unread notifications are marked as read", async () => {
+  it("should return success response when all unread notifications are marked as read", async () => {
     const context = {
       updateNotificationsLoader: jest.fn().mockResolvedValue(true),
     }
@@ -19,14 +29,17 @@ describe("markAllNotificationsAsReadMutation", () => {
     expect(result).toMatchInlineSnapshot(`
       Object {
         "markAllNotificationsAsRead": Object {
-          "success": true,
+          "responseOrError": Object {
+            "success": true,
+          },
         },
       }
     `)
   })
 
-  it("should return success = false when something went wrong", async () => {
-    const error = new Error("Some Error")
+  it("should return failure response when something went wrong", async () => {
+    const message = `https://stagingapi.artsy.net/api/v1/me/notifications - {"error":"Something went wrong"}`
+    const error = new Error(message)
     const context = {
       updateNotificationsLoader: jest.fn().mockRejectedValue(error),
     }
@@ -36,7 +49,11 @@ describe("markAllNotificationsAsReadMutation", () => {
     expect(result).toMatchInlineSnapshot(`
       Object {
         "markAllNotificationsAsRead": Object {
-          "success": false,
+          "responseOrError": Object {
+            "mutationError": Object {
+              "message": "Something went wrong",
+            },
+          },
         },
       }
     `)
--- src/schema/v2/me/mark_all_notifications_as_read_mutation.ts
+++ src/schema/v2/me/mark_all_notifications_as_read_mutation.ts
@@ -1,6 +1,41 @@
+import { GraphQLObjectType } from "graphql"
+import { GraphQLUnionType } from "graphql"
 import { GraphQLBoolean } from "graphql"
 import { mutationWithClientMutationId } from "graphql-relay"
+import {
+  formatGravityError,
+  GravityMutationErrorType,
+} from "lib/gravityErrorHandler"
 import { ResolverContext } from "types/graphql"
+
+const SuccessType = new GraphQLObjectType<any, ResolverContext>({
+  name: "MarkAllNotificationsAsReadSuccess",
+  isTypeOf: (data) => data.success,
+  fields: () => ({
+    success: {
+      type: GraphQLBoolean,
+      resolve: (result) => result.success,
+    },
+  }),
+})
+
+const ErrorType = new GraphQLObjectType<any, ResolverContext>({
+  name: "MarkAllNotificationsAsReadFailure",
+  isTypeOf: (data) => {
+    return data._type === "GravityMutationError"
+  },
+  fields: () => ({
+    mutationError: {
+      type: GravityMutationErrorType,
+      resolve: (err) => (typeof err.message === "object" ? err.message : err),
+    },
+  }),
+})
+
+const ResponseOrErrorType = new GraphQLUnionType({
+  name: "MarkAllNotificationsAsReadResponseOrError",
+  types: [SuccessType, ErrorType],
+})
 
 export const markAllNotificationsAsReadMutation = mutationWithClientMutationId<
   any,
@@ -11,9 +46,9 @@ export const markAllNotificationsAsReadMutation = mutationWithClientMutationId<
   description: "Mark all unread notifications as read",
   inputFields: {},
   outputFields: {
-    success: {
-      type: GraphQLBoolean,
-      resolve: (result) => result.success,
+    responseOrError: {
+      type: ResponseOrErrorType,
+      resolve: (result) => result,
     },
   },
   mutateAndGetPayload: async (_, { updateNotificationsLoader }) => {
@@ -27,9 +62,13 @@ export const markAllNotificationsAsReadMutation = mutationWithClientMutationId<
       return {
         success: true,
       }
-    } catch {
-      return {
-        success: false,
+    } catch (error) {
+      const formattedErr = formatGravityError(error)
+
+      if (formattedErr) {
+        return { ...formattedErr, _type: "GravityMutationError" }
+      } else {
+        throw new Error(error)
       }
     }
   },
