bc436081cce5dfa02fd4bb15ff29d27688190b87
Jon Allured <jon.allured@gmail.com>
2022-09-21 09:52:44 -0500

    Respect timezones for event time ranges

--- src/schema/v2/show_event.ts
+++ src/schema/v2/show_event.ts
@@ -24,8 +24,10 @@ const ShowEventType = new GraphQLObjectType<any, ResolverContext>({
     dateTimeRange: {
       type: GraphQLString,
       description: "A formatted description of the dates with hours",
-      resolve: ({ start_at, end_at }) =>
-        dateTimeRange(start_at, end_at, "UTC", true),
+      resolve: ({ start_at, end_at }, { timezone }, { defaultTimezone }) => {
+        const timezoneString = timezone ? timezone : defaultTimezone
+        return dateTimeRange(start_at, end_at, timezoneString, true)
+      },
     },
     exhibitionPeriod: {
       type: GraphQLString,
