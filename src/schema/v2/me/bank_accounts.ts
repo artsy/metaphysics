292d2d524c39ecc948437313b1368cdf5dbc862a
kiryl-zubarau <79979820+kiryl-zubarau@users.noreply.github.com>
2022-09-23 18:05:55 +0300

    feat: allow filtering bank accounts by the bank account type (#4361)
    Co-authored-by: Kirill <kiry.zubarau@artsymail.com>

--- src/schema/v2/me/bank_accounts.ts
+++ src/schema/v2/me/bank_accounts.ts
@@ -2,17 +2,29 @@ import { BankAccountConnection } from "schema/v2/bank_account"
 import { pageable } from "relay-cursor-paging"
 import { connectionFromArraySlice } from "graphql-relay"
 import { convertConnectionArgsToGravityArgs } from "lib/helpers"
-import { GraphQLFieldConfig } from "graphql/type"
+import { GraphQLEnumType, GraphQLFieldConfig } from "graphql/type"
 import { ResolverContext } from "types/graphql"
+
+export const BankAccountTypes = {
+  type: new GraphQLEnumType({
+    name: "BankAccountTypes",
+    values: {
+      US_BANK_ACCOUNT: { value: "us_bank_account" },
+      SEPA_DEBIT: { value: "sepa_debit" },
+    },
+  }),
+}
 
 export const BankAccounts: GraphQLFieldConfig<void, ResolverContext> = {
   type: BankAccountConnection,
-  args: pageable({}),
+  args: pageable({
+    type: BankAccountTypes,
+  }),
   description: "A list of the current user's bank accounts",
   resolve: (_root, options, { meBankAccountsLoader }) => {
     if (!meBankAccountsLoader) return null
     const { page, size, offset } = convertConnectionArgsToGravityArgs(options)
-    const gravityArgs = { page, size, total_count: true }
+    const gravityArgs = { page, size, total_count: true, type: options.type }
 
     return meBankAccountsLoader(gravityArgs).then(({ body, headers }) => {
       return connectionFromArraySlice(body, options, {
