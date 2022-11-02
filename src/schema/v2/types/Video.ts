e9dc20e2569962caf6b018b0e4295b0e31ee422c
dzucconi <mail@damonzucconi.com>
2022-11-02 16:31:20 -0400

    feat(video): exposes id field

--- src/schema/v2/types/Video.ts
+++ src/schema/v2/types/Video.ts
@@ -1,15 +1,23 @@
 import {
+  GraphQLID,
   GraphQLInt,
   GraphQLNonNull,
   GraphQLObjectType,
   GraphQLString,
 } from "graphql"
+import uuid from "uuid/v5"
 import { ResolverContext } from "types/graphql"
 
 export const VideoType = new GraphQLObjectType<any, ResolverContext>({
   name: "Video",
   description: "An object containing video metadata",
   fields: {
+    id: {
+      type: new GraphQLNonNull(GraphQLID),
+      resolve: ({ url }) => {
+        return uuid(url, uuid.URL)
+      },
+    },
     url: {
       description: "The url of the video",
       type: GraphQLNonNull(GraphQLString),
