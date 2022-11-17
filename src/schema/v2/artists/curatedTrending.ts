b9c7eef4d24570245e5578b7a73fd90312d6c1da
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2022-11-17 17:32:08 -0500

    feat: add curatedTrendingArtists connection

---
+++ src/schema/v2/artists/curatedTrending.ts
@@ -0,0 +1,36 @@
+import { GraphQLFieldConfig } from "graphql"
+import type { ResolverContext } from "types/graphql"
+import { connectionArgs, connectionFromArray } from "graphql-relay"
+
+import { dailyShuffle } from "lib/shuffle"
+import { ArtworksAggregation } from "../aggregations/filter_artworks_aggregation"
+import { artistConnection } from "../artist"
+
+export const CuratedTrendingArtists: GraphQLFieldConfig<
+  void,
+  ResolverContext
+> = {
+  type: artistConnection.connectionType,
+  description:
+    "A list of trending artists. Inferred from a manually curated collection of trending artworks.",
+  args: connectionArgs,
+  resolve: async (_parent, args, context) => {
+    const { artistsLoader, filterArtworksLoader, defaultTimezone } = context
+    const artworks = await filterArtworksLoader({
+      size: 0,
+      marketing_collection_id: "trending-this-week",
+      aggregations: [
+        ArtworksAggregation.getValue("MERCHANDISABLE_ARTISTS")!.value,
+      ],
+    })
+
+    const artists = artworks.aggregations.merchandisable_artists
+    const artistIDs = Object.keys(artists)
+    const shuffledIDs = dailyShuffle(artistIDs, defaultTimezone)
+    const { body: artistRecords } = await artistsLoader({
+      ids: shuffledIDs,
+    })
+
+    return connectionFromArray(artistRecords, args)
+  },
+}
