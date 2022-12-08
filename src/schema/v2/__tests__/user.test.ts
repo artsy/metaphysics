8e8439a700facfc465c0fa81f758aec819279bfa
kajatiger <ksantro@gmx.de>
2022-12-08 15:11:48 +0100

    test: add sale date to query and expectations

--- src/schema/v2/__tests__/user.test.ts
+++ src/schema/v2/__tests__/user.test.ts
@@ -450,6 +450,7 @@ describe("User", () => {
               edges {
                 ownerType
                 salePrice
+                saleDate
                 source
                 node {
                   __typename
@@ -496,6 +497,7 @@ describe("User", () => {
         {
           owner_type: "ArtworkInquiryRequest",
           sale_price: 600.1234,
+          sale_date: "2018-09-26 04:00:00 UTC",
           source: "inquiry",
           artwork: {
             __typename: "Artwork",
@@ -542,6 +544,7 @@ describe("User", () => {
       expect(edges[0]).toEqual({
         ownerType: "SaleArtwork",
         salePrice: 1000.9999,
+        saleDate: "2018-09-26 04:00:00 UTC",
         source: "auction",
         node: {
           __typename: "Artwork",
@@ -560,6 +563,7 @@ describe("User", () => {
       expect(edges[1]).toEqual({
         ownerType: "ArtworkInquiryRequest",
         salePrice: 600.1234,
+        saleDate: "2018-09-26 04:00:00 UTC",
         source: "inquiry",
         node: {
           __typename: "Artwork",
