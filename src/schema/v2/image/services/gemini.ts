52d8c27230e873663f68d96f172610df6848ddca
dzucconi <mail@damonzucconi.com>
2022-12-09 13:34:57 -0500

    refactor(gemini): uses @artsy/img to mint urls

--- src/schema/v2/image/services/gemini.ts
+++ src/schema/v2/image/services/gemini.ts
@@ -1,40 +1,21 @@
-import urljoin from "url-join"
-import { isExisty, toQueryString } from "lib/helpers"
 import config from "config"
+import { configureImageServices, ResizeMode } from "@artsy/img"
 
 const { GEMINI_ENDPOINT } = config
+
+const services = configureImageServices({
+  gemini: {
+    endpoint: GEMINI_ENDPOINT!,
+  },
+})
 
 const DEFAULT_1X_QUALITY = 80
 const DEFAULT_2X_QUALITY = 50
 export const DEFAULT_SRCSET_QUALITY = [DEFAULT_1X_QUALITY, DEFAULT_2X_QUALITY]
 
-type Mode = "resize" | "crop"
-
-interface ResizeTo {
-  mode: Mode
-  width?: number
-  height?: number
-}
-
-const resizeTo = ({ mode, width, height }: ResizeTo) => {
-  if (mode === "crop") {
-    return "fill"
-  }
-
-  if (isExisty(width) && !isExisty(height)) {
-    return "width"
-  }
-
-  if (isExisty(height) && !isExisty(width)) {
-    return "height"
-  }
-
-  return "fit"
-}
-
 interface Gemini {
   src: string
-  mode: Mode
+  mode: ResizeMode
   width?: number
   height?: number
   quality?: number
@@ -47,15 +28,5 @@ export const gemini = ({
   height,
   quality = DEFAULT_1X_QUALITY,
 }: Gemini): string => {
-  const options = {
-    resize_to: resizeTo({ mode, width, height }),
-    width,
-    height,
-    quality,
-    src,
-  }
-
-  const queryParams = toQueryString(options)
-
-  return urljoin(GEMINI_ENDPOINT, `?${queryParams}`)
+  return services.gemini.exec(mode, src, { width, height, quality })
 }
