7a0213896a9d5d44d5ecf2b7c376ae0ebd0added
kajatiger <ksantro@gmx.de>
2022-12-13 13:05:08 +0100

    chore: format sale date string into date

--- src/schema/v2/userPurchases.ts
+++ src/schema/v2/userPurchases.ts
@@ -8,6 +8,7 @@ import {
 import { IDFields } from "./object_identification"
 import { ArtworkType } from "./artwork"
 import { connectionWithCursorInfo } from "schema/v2/fields/pagination"
+import { date } from "./fields/date"
 
 export const edgeFields: Thunk<GraphQLFieldConfigMap<
   any,
@@ -16,7 +17,7 @@ export const edgeFields: Thunk<GraphQLFieldConfigMap<
   ...IDFields,
   ownerType: { type: GraphQLString, resolve: ({ owner_type }) => owner_type },
   salePrice: { type: GraphQLFloat, resolve: ({ sale_price }) => sale_price },
-  saleDate: { type: GraphQLString, resolve: ({ sale_date }) => sale_date },
+  saleDate: date(({ sale_date }) => sale_date),
   source: { type: GraphQLString },
 })
 
