c04ae45951fafbb31fcddcb5356d3fcfc7fea7d6
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2023-01-18 15:02:36 -0500

    feat: add collection field under `me` (#4674)
    * chore: discard old, orphaned, pre-v2 implementation
    
    * feat: add collection field, nested under `me`
    
    * feat: add artworks_count

---
+++ src/schema/v2/me/collection.ts
@@ -0,0 +1,60 @@
+import {
+  GraphQLFieldConfig,
+  GraphQLObjectType,
+  GraphQLNonNull,
+  GraphQLString,
+  GraphQLBoolean,
+  GraphQLInt,
+} from "graphql"
+import { ResolverContext } from "types/graphql"
+import { InternalIDFields } from "../object_identification"
+
+export const Collection: GraphQLFieldConfig<any, ResolverContext> = {
+  description: "A collection belonging to the current user",
+  type: new GraphQLObjectType<any, ResolverContext>({
+    name: "Collection",
+    description: "A collection of artworks",
+    fields: () => ({
+      ...InternalIDFields,
+      name: {
+        type: new GraphQLNonNull(GraphQLString),
+        description:
+          "Name of the collection. Has a predictable value for 'standard' collections such as Saved Artwork, My Collection, etc. Can be provided by user otherwise.",
+      },
+      default: {
+        type: new GraphQLNonNull(GraphQLBoolean),
+        description:
+          "True if this is the default collection for this user, i.e. the default Saved Artwork collection.",
+      },
+      saves: {
+        type: new GraphQLNonNull(GraphQLBoolean),
+        description:
+          "True if this collection represents artworks explicitly saved by the user, false otherwise.",
+      },
+      artworksCount: {
+        type: new GraphQLNonNull(GraphQLInt),
+        description: "Number of artworks associated with this collection.",
+        resolve: ({ artworks_count }) => artworks_count,
+      },
+    }),
+  }),
+  args: {
+    id: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+  },
+  resolve: async (parent, args, context, _info) => {
+    const { id: meID } = parent
+    const { id } = args
+    const { collectionLoader } = context
+
+    if (!collectionLoader) return null
+
+    const response = await collectionLoader(id, {
+      user_id: meID,
+      private: true,
+    })
+
+    return response
+  },
+}
