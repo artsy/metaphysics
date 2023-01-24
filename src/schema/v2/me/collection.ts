6d9d18fc365c13a806ad8adc56d13e95932a85ba
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2023-01-24 01:30:43 -0500

    feat(wishlists): add artworksConnection to me.collection (#4704)
    * chore: alphabetize fields
    
    * feat: add artworksConnection to me.collection
    
    * fix: pageable args, casing

--- src/schema/v2/me/collection.ts
+++ src/schema/v2/me/collection.ts
@@ -6,8 +6,16 @@ import {
   GraphQLBoolean,
   GraphQLInt,
 } from "graphql"
+import {
+  CatchCollectionNotFoundException,
+  convertConnectionArgsToGravityArgs,
+} from "lib/helpers"
+import { pageable } from "relay-cursor-paging"
 import { ResolverContext } from "types/graphql"
+import { artworkConnection } from "../artwork"
+import { paginationResolver } from "../fields/pagination"
 import { InternalIDFields } from "../object_identification"
+import CollectionSorts from "../sorts/collection_sorts"
 
 export const Collection: GraphQLFieldConfig<any, ResolverContext> = {
   description: "A collection belonging to the current user",
@@ -16,25 +24,74 @@ export const Collection: GraphQLFieldConfig<any, ResolverContext> = {
     description: "A collection of artworks",
     fields: () => ({
       ...InternalIDFields,
-      name: {
-        type: new GraphQLNonNull(GraphQLString),
-        description:
-          "Name of the collection. Has a predictable value for 'standard' collections such as Saved Artwork, My Collection, etc. Can be provided by user otherwise.",
+      artworksConnection: {
+        type: artworkConnection.connectionType,
+        args: {
+          ...pageable({
+            sort: {
+              type: CollectionSorts,
+              defaultValue: CollectionSorts.getValue("SAVED_AT_DESC")!.value,
+            },
+          }),
+        },
+        resolve: async (parent, args, context, _info) => {
+          const { collectionArtworksLoader } = context
+          if (!collectionArtworksLoader) return null
+
+          const { id, userID } = parent
+          const { page, size, offset } = convertConnectionArgsToGravityArgs(
+            args
+          )
+
+          const gravityOptions = {
+            page,
+            size,
+            user_id: userID,
+            private: true,
+            sort: args.sort,
+            total_count: true,
+          }
+
+          try {
+            const { headers, body } = await collectionArtworksLoader(
+              id,
+              gravityOptions
+            )
+
+            const totalCount = parseInt(headers["x-total-count"] || "0", 10)
+
+            return paginationResolver({
+              totalCount,
+              offset,
+              page,
+              size,
+              body,
+              args,
+            })
+          } catch (error) {
+            return CatchCollectionNotFoundException(error)
+          }
+        },
+      },
+      artworksCount: {
+        type: new GraphQLNonNull(GraphQLInt),
+        description: "Number of artworks associated with this collection.",
+        resolve: ({ artworks_count }) => artworks_count,
       },
       default: {
         type: new GraphQLNonNull(GraphQLBoolean),
         description:
           "True if this is the default collection for this user, i.e. the default Saved Artwork collection.",
       },
+      name: {
+        type: new GraphQLNonNull(GraphQLString),
+        description:
+          "Name of the collection. Has a predictable value for 'standard' collections such as Saved Artwork, My Collection, etc. Can be provided by user otherwise.",
+      },
       saves: {
         type: new GraphQLNonNull(GraphQLBoolean),
         description:
           "True if this collection represents artworks explicitly saved by the user, false otherwise.",
-      },
-      artworksCount: {
-        type: new GraphQLNonNull(GraphQLInt),
-        description: "Number of artworks associated with this collection.",
-        resolve: ({ artworks_count }) => artworks_count,
       },
     }),
   }),
@@ -55,6 +112,9 @@ export const Collection: GraphQLFieldConfig<any, ResolverContext> = {
       private: true,
     })
 
-    return response
+    return {
+      ...response,
+      userID: meID,
+    }
   },
 }
