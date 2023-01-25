4a85cc7fbbbb8b8a2b59761d78953dfccf5f5433
Nikita Skalkin <nikskalkin@gmail.com>
2023-01-25 17:40:11 +0100

    feat(FX-4523): me.collectionsConnection

---
+++ src/schema/v2/me/collectionsConnection.ts
@@ -0,0 +1,49 @@
+import {
+  connectionWithCursorInfo,
+  paginationResolver,
+} from "schema/v2/fields/pagination"
+import { convertConnectionArgsToGravityArgs } from "lib/helpers"
+import { CollectionType } from "./collection"
+import { pageable } from "relay-cursor-paging"
+import { GraphQLFieldConfig, GraphQLInt } from "graphql"
+import { ResolverContext } from "types/graphql"
+
+export const CollectionsConnectionType = connectionWithCursorInfo({
+  name: "Collections",
+  nodeType: CollectionType,
+}).connectionType
+
+export const CollectionsConnection: GraphQLFieldConfig<any, ResolverContext> = {
+  type: CollectionsConnectionType,
+  args: pageable({
+    page: { type: GraphQLInt },
+    size: { type: GraphQLInt },
+  }),
+  resolve: async (parent, args, context, _info) => {
+    const { id: meID } = parent
+    const { collectionsLoader } = context
+    if (!collectionsLoader) return null
+
+    const { page, size, offset } = convertConnectionArgsToGravityArgs(args)
+    const { body, headers } = await collectionsLoader({
+      user_id: meID,
+      private: true,
+      size,
+      offset,
+      total_count: true,
+    })
+    const bodyWithMe = (body ?? []).map((obj) => {
+      return { ...obj, userID: meID }
+    })
+    const totalCount = parseInt((headers ?? {})["x-total-count"] || "0", 10)
+
+    return paginationResolver({
+      totalCount,
+      offset,
+      page,
+      size,
+      body: bodyWithMe,
+      args,
+    })
+  },
+}
