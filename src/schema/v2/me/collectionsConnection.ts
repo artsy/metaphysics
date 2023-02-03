4f344255853307c5367b1c5ea8bc3e1ea0d2e86a
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2023-02-03 15:59:39 -0500

    refactor: better arg handling
    To avoid problem of JS complaining about use of reserved word

--- src/schema/v2/me/collectionsConnection.ts
+++ src/schema/v2/me/collectionsConnection.ts
@@ -27,13 +27,12 @@ export const CollectionsConnection: GraphQLFieldConfig<any, ResolverContext> = {
     if (!collectionsLoader) return null
 
     const { page, size, offset } = convertConnectionArgsToGravityArgs(args)
-    const { default: default_, saves } = args // 'default' is a reserved word in JS
 
     const { body, headers } = await collectionsLoader({
       user_id: meID,
       private: true,
-      default: default_,
-      saves,
+      default: args.default,
+      saves: args.saves,
       size,
       offset,
       total_count: true,
