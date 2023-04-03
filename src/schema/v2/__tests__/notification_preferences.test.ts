722ef10c895617a12f6579231d7951f4b212db86
tmcmeans <tanjmcmeans01@gmail.com>
2023-04-03 13:30:47 -0500

    not getting anywhere on tests (yet)

--- src/schema/v2/__tests__/notification_preferences.test.ts
+++ src/schema/v2/__tests__/notification_preferences.test.ts
@@ -15,12 +15,12 @@ describe("convertSubGroups", () => {
 })
 
 describe("notificationPreferenceType", () => {
-  // test for each notification field
+  // recommendedByArtsy
   it("can update push notification preferences for recommendedByArtsy", () => {
     const pushSubGroups = [
       {
         id: "abc",
-        name: "recommendedByArtsy",
+        name: "recommendedByArtsyPush",
         channel: "push",
         status: "Subscribed",
       },
@@ -28,11 +28,116 @@ describe("notificationPreferenceType", () => {
 
     expect(pushSubGroups).toBeTrue()
   })
+
   it("can update email notification preferences recommendedByArtsy", () => {
     const pushSubGroups = [
       {
         id: "abc",
-        name: "recommendedByArtsy",
+        name: " recommendedByArtsyEmail",
+        channel: "email",
+        status: "Subscribed",
+      },
+    ]
+
+    expect(pushSubGroups).toBeTrue()
+  })
+
+  // artWorldInsights
+  it("can update push notification preferences for artWorldInsights", () => {
+    const pushSubGroups = [
+      {
+        id: "abc",
+        name: "artWorldInsightsPush",
+        channel: "push",
+        status: "Subscribed",
+      },
+    ]
+
+    expect(pushSubGroups).toBeTrue()
+  })
+  it("can update email notification preferences artWorldInsights", () => {
+    const pushSubGroups = [
+      {
+        id: "abc",
+        name: "artWorldInsightsEmail",
+        channel: "email",
+        status: "Subscribed",
+      },
+    ]
+
+    expect(pushSubGroups).toBeTrue()
+  })
+
+  // productUpdates
+  it("can update push notification preferences for productUpdates", () => {
+    const pushSubGroups = [
+      {
+        id: "abc",
+        name: "productUpdatesPush",
+        channel: "push",
+        status: "Subscribed",
+      },
+    ]
+
+    expect(pushSubGroups).toBeTrue()
+  })
+  it("can update email notification preferences productUpdates", () => {
+    const pushSubGroups = [
+      {
+        id: "abc",
+        name: "productUpdatesEmail",
+        channel: "email",
+        status: "Subscribed",
+      },
+    ]
+
+    expect(pushSubGroups).toBeTrue()
+  })
+
+  // guidanceOnCollecting
+  it("can update push notification preferences for guidanceOnCollecting", () => {
+    const pushSubGroups = [
+      {
+        id: "abc",
+        name: "guidanceOnCollectingPush",
+        channel: "push",
+        status: "Subscribed",
+      },
+    ]
+
+    expect(pushSubGroups).toBeTrue()
+  })
+  it("can update email notification preferences guidanceOnCollecting", () => {
+    const pushSubGroups = [
+      {
+        id: "abc",
+        name: "guidanceOnCollectingEmail",
+        channel: "email",
+        status: "Subscribed",
+      },
+    ]
+
+    expect(pushSubGroups).toBeTrue()
+  })
+
+  // customAlerts
+  it("can update push notification preferences for customAlerts", () => {
+    const pushSubGroups = [
+      {
+        id: "abc",
+        name: "customAlertsPush",
+        channel: "push",
+        status: "Subscribed",
+      },
+    ]
+
+    expect(pushSubGroups).toBeTrue()
+  })
+  it("can update email notification preferences customAlerts", () => {
+    const pushSubGroups = [
+      {
+        id: "abc",
+        name: "customAlertsEmail",
         channel: "email",
         status: "Subscribed",
       },
