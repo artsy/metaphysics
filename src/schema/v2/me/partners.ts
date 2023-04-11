7b2114c889a4018825aec9d638feab502cc5644e
Christopher Pappas <damassi.pappas@gmail.com>
2023-04-11 12:13:13 -0700

    feat(me): Add size arg to partner field

--- src/schema/v2/me/partners.ts
+++ src/schema/v2/me/partners.ts
@@ -1,12 +1,20 @@
 import { GraphQLFieldConfig } from "graphql/type"
 import { ResolverContext } from "types/graphql"
 import { Partners } from "schema/v2/partner/partners"
+import { GraphQLInt } from "graphql"
 
-export const ManagedPartners: GraphQLFieldConfig<void, ResolverContext> = {
+export const ManagedPartners: GraphQLFieldConfig<any, ResolverContext> = {
   type: Partners.type,
   description: "A list of the current user’s managed partners",
-  resolve: async (_root, _option, { partnerLoader, mePartnersLoader }) => {
-    const partners = await mePartnersLoader?.()
+  args: {
+    size: {
+      type: GraphQLInt,
+    },
+  },
+  resolve: async (_root, args, { partnerLoader, mePartnersLoader }) => {
+    const partners = await mePartnersLoader?.({
+      size: args.size ?? 10,
+    })
     return Promise.all(
       partners.map((partner) => partnerLoader(partner.id))
     ).catch(() => {})
