60d430ea272809369d7fac878c5044d5a2b2f4d1
Joey Aghion <joey@aghion.com>
2023-04-18 18:57:15 -0400

    fix: restore access to identity and email verification status via collector profiles

--- src/schema/v2/CollectorProfile/collectorProfile.ts
+++ src/schema/v2/CollectorProfile/collectorProfile.ts
@@ -85,21 +85,21 @@ export const CollectorProfileFields: GraphQLFieldConfigMap<
     type: GraphQLBoolean,
     deprecationReason:
       "emailConfirmed is going to be removed, use isEmailConfirmed instead",
-    resolve: ({ owner }) => !!owner.confirmed_at,
+    resolve: ({ email_confirmed_at }) => !!email_confirmed_at,
   },
   isEmailConfirmed: {
     type: GraphQLBoolean,
-    resolve: ({ owner }) => !!owner.confirmed_at,
+    resolve: ({ email_confirmed_at }) => !!email_confirmed_at,
   },
   identityVerified: {
     type: GraphQLBoolean,
     deprecationReason:
       "identityVerified is going to be removed, use isIdentityVerified instead",
-    resolve: ({ owner }) => owner.identity_verified,
+    resolve: ({ identity_verified }) => identity_verified,
   },
   isIdentityVerified: {
     type: GraphQLBoolean,
-    resolve: ({ owner }) => owner.identity_verified,
+    resolve: ({ identity_verified }) => identity_verified,
   },
   isActiveInquirer: {
     type: GraphQLBoolean,
