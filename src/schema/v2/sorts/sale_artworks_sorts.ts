93c15c7b91073dbc3c7e52195d5bee02ea86424a
Kizito Egeonu <kizito.egeonu@gmail.com>
2023-05-05 07:08:05 +0200

    unify sort params

--- src/schema/v2/sorts/sale_artworks_sorts.ts
+++ src/schema/v2/sorts/sale_artworks_sorts.ts
@@ -1,6 +1,12 @@
 import { GraphQLEnumType } from "graphql"
 
 export const SALE_ARTWORKS_SORTS = {
+  BIDDER_POSITIONS_COUNT_ASC: {
+    value: "bidder_positions_count",
+  },
+  BIDDER_POSITIONS_COUNT_DESC: {
+    value: "-bidder_positions_count",
+  },
   END_AT_ASC: {
     value: "end_at",
   },
@@ -10,7 +16,16 @@ export const SALE_ARTWORKS_SORTS = {
   POSITION_ASC: {
     value: "position",
   },
-} as const
+  POSITION_DESC: {
+    value: "-position",
+  },
+  SEARCHABLE_ESTIMATE_ASC: {
+    value: "searchable_estimate",
+  },
+  SEARCHABLE_ESTIMATE_DESC: {
+    value: "-searchable_estimate",
+  },
+}
 
 const SaleArtworksSorts = {
   type: new GraphQLEnumType({
