6758977f69b7065d992e6aa34d9ac0c26af7ca33
pam- <pamela.assogba@artsymail.com>
2023-05-19 12:51:03 -0700

    add: enum for artist tab meta tags
    rm obsolete arguments
    
    rm obsolete arguments
    
    cleanup

--- src/schema/v2/artist/meta.ts
+++ src/schema/v2/artist/meta.ts
@@ -1,6 +1,11 @@
 import { stripTags, truncate, markdownToText } from "lib/helpers"
 import { compact } from "lodash"
-import { GraphQLString, GraphQLObjectType, GraphQLFieldConfig } from "graphql"
+import {
+  GraphQLString,
+  GraphQLObjectType,
+  GraphQLFieldConfig,
+  GraphQLEnumType,
+} from "graphql"
 import { ResolverContext } from "types/graphql"
 
 export const metaName = (artist) => {
@@ -12,63 +17,62 @@ export const formatDescription = (description: string, blurb?: string) => {
   return truncate(compact([description, blurb]).join("."), 157)
 }
 
+const ArtistTabEnumType = new GraphQLEnumType({
+  name: "ArtistTab",
+  values: {
+    ABOUT: { value: "ABOUT" },
+    ARTWORKS: { value: "ARTWORKS" },
+    AUCTION_RESULTS: { value: "AUCTION_RESULTS" },
+  },
+})
+
 const ArtistMetaType = new GraphQLObjectType<any, ResolverContext>({
   name: "ArtistMeta",
   fields: {
-    artworksDescription: {
-      type: GraphQLString,
-      resolve: (artist) => {
-        const blurb = artist.blurb.length
-          ? markdownToText(artist.blurb)
-          : undefined
-
-        return formatDescription(
-          `Discover and purchase ${metaName(
-            artist
-          )}’s artworks, available for sale. Browse our selection of paintings, prints, and sculptures by the artist, and find art you love.`,
-          blurb
-        )
-      },
-    },
-    artworksTitle: {
-      type: GraphQLString,
-      resolve: (artist) => {
-        return `${metaName(artist)} - Artworks for Sale & More | Artsy`
-      },
-    },
-    auctionDescription: {
-      type: GraphQLString,
-      resolve: (artist) => {
-        return `Find out about ${metaName(
-          artist
-        )}’s auction history, past sales, and current market value. Browse Artsy’s Price Database for recent auction results from the artist.`
-      },
-    },
-    auctionTitle: {
-      type: GraphQLString,
-      resolve: (artist) => {
-        return `${metaName(artist)} - Auction Results and Sales Data | Artsy`
-      },
-    },
     description: {
       type: GraphQLString,
-      resolve: (artist) => {
+      resolve: ({ artist, tab }) => {
         const blurb = artist.blurb.length
           ? markdownToText(artist.blurb)
           : undefined
 
-        return formatDescription(
-          `Explore ${metaName(
-            artist
-          )}'s biography, achievements, artworks, auction results, and shows on Artsy.`,
-          blurb
-        )
+        switch (tab) {
+          case "ABOUT":
+            return formatDescription(
+              `Explore ${metaName(
+                artist
+              )}'s biography, achievements, artworks, auction results, and shows on Artsy.`,
+              blurb
+            )
+          case "ARTWORKS":
+            return formatDescription(
+              `Discover and purchase ${metaName(
+                artist
+              )}’s artworks, available for sale. Browse our selection of paintings, prints, and sculptures by the artist, and find art you love.`,
+              blurb
+            )
+          case "AUCTION_RESULTS":
+            return `Find out about ${metaName(
+              artist
+            )}’s auction history, past sales, and current market value. Browse Artsy’s Price Database for recent auction results from the artist.`
+        }
       },
     },
     title: {
       type: GraphQLString,
-      resolve: (artist) => {
-        return `${metaName(artist)} - Biography, Shows, Articles & More | Artsy`
+      resolve: ({ artist, tab }) => {
+        switch (tab) {
+          case "ABOUT":
+            return `${metaName(
+              artist
+            )} - Biography, Shows, Articles & More | Artsy`
+          case "ARTWORKS":
+            return `${metaName(artist)} - Artworks for Sale & More | Artsy`
+          case "AUCTION_RESULTS":
+            return `${metaName(
+              artist
+            )} - Auction Results and Sales Data | Artsy`
+        }
       },
     },
   },
@@ -76,7 +80,12 @@ const ArtistMetaType = new GraphQLObjectType<any, ResolverContext>({
 
 const Meta: GraphQLFieldConfig<void, ResolverContext> = {
   type: ArtistMetaType,
-  resolve: (x) => x,
+  args: {
+    tab: { type: ArtistTabEnumType },
+  },
+  resolve: (artist, { tab }) => {
+    return { artist, tab }
+  },
 }
 
 export default Meta
