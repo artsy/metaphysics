0cd1f03a5d7db38a46382496a70e5a663ef686bf
Sultan <sultan.al-maari@artsymail.com>
2023-07-05 12:54:52 +0200

    update gravityArgs, use paginationResolver in interestConnection

--- src/schema/v2/me/interestsConnection.ts
+++ src/schema/v2/me/interestsConnection.ts
@@ -1,13 +1,11 @@
 import { GraphQLFieldConfig, GraphQLInt } from "graphql"
-import { connectionFromArraySlice } from "graphql-relay"
 import { convertConnectionArgsToGravityArgs } from "lib/helpers"
-import { snakeCase } from "lodash"
 import { pageable } from "relay-cursor-paging"
 import { ResolverContext } from "types/graphql"
-import { createPageCursors } from "../fields/pagination"
+import { paginationResolver } from "../fields/pagination"
 import {
-  userInterestCategoryEnum,
   UserInterestConnection,
+  userInterestCategoryEnum,
   userInterestInterestTypeEnum,
 } from "../userInterests"
 
@@ -36,10 +34,7 @@ export const InterestsConnection: GraphQLFieldConfig<void, ResolverContext> = {
       args
     )
 
-    const gravityArgs = Object.keys(rest).reduce(
-      (acc, key) => ({ ...acc, [snakeCase(key)]: rest[key] }),
-      {} as { category?: string; interestType?: string }
-    )
+    const gravityArgs = { ...rest, interest_type: rest.interestType }
 
     const { body, headers } = await meUserInterestsLoader({
       ...gravityArgs,
@@ -50,14 +45,14 @@ export const InterestsConnection: GraphQLFieldConfig<void, ResolverContext> = {
 
     const totalCount = parseInt(headers["x-total-count"] || "0", 10)
 
-    return {
+    return paginationResolver({
+      args,
+      body,
+      offset,
+      page,
+      size,
       totalCount,
-      pageCursors: createPageCursors({ page, size }, totalCount),
-      ...connectionFromArraySlice(body, args, {
-        arrayLength: totalCount,
-        sliceStart: offset,
-        resolveNode: (node) => node.interest,
-      }),
-    }
+      resolveNode: (node) => node.interest,
+    })
   },
 }
