b978eb0f77a1c1a1f282f8e4802e4d14765eed25
Erik <9088720+erikdstock@users.noreply.github.com>
2023-07-11 12:34:20 -0400

    fix: incorrect & inconsistent address validation fields (#5131)
    * chore: verifyAddress query updates
    
    - correct underscore/camel casing
    - put all address query fields inside an argument

--- src/schema/v2/__tests__/verify_address.test.ts
+++ src/schema/v2/__tests__/verify_address.test.ts
@@ -6,11 +6,13 @@ describe("verifyAddressQuery", () => {
     const query = gql`
       {
         verifyAddress(
-          addressLine1: "Lausitzer Str. 46"
-          city: "Berlin"
-          country: "DE"
-          postalCode: "10999"
-          region: "Berlin"
+          address: {
+            addressLine1: "Lausitzer Str. 46"
+            city: "Berlin"
+            country: "DE"
+            postalCode: "10999"
+            region: "Berlin"
+          }
         ) {
           verificationStatus
           inputAddress {
@@ -18,9 +20,9 @@ describe("verifyAddressQuery", () => {
               city
               country
               region
-              postal_code
-              address_line1
-              address_line2
+              postalCode
+              addressLine1
+              addressLine2
             }
             lines
           }
@@ -29,9 +31,9 @@ describe("verifyAddressQuery", () => {
               city
               country
               region
-              postal_code
-              address_line_1
-              address_line_2
+              postalCode
+              addressLine1
+              addressLine2
             }
             lines
           }
@@ -51,10 +53,10 @@ describe("verifyAddressQuery", () => {
       verificationStatus: "VERIFIED_WITH_CHANGES",
       inputAddress: {
         address: {
-          address_line_1: "Lausitzer Straße 46",
+          addressLine1: "Lausitzer Straße 46",
           city: "Berlin",
           region: "Berlin",
-          postal_code: "10999",
+          postalCode: "10999",
           country: "DE",
         },
         lines: [
@@ -67,11 +69,11 @@ describe("verifyAddressQuery", () => {
       suggestedAddresses: [
         {
           address: {
-            address_line1: "Lausitzer Straße 46",
-            address_line2: "Kreuzberg",
+            addressLine1: "Lausitzer Straße 46",
+            addressLine2: "Kreuzberg",
             city: "Berlin",
             region: "Berlin",
-            postal_code: "10999",
+            postalCode: "10999",
             country: "DE",
           },
           lines: [
@@ -98,11 +100,13 @@ describe("verifyAddressQuery", () => {
     const query = gql`
       {
         verifyAddress(
-          addressLine1: "1251 John Calvin Drive"
-          city: "Harvey"
-          country: "US"
-          postalCode: "60426"
-          region: "Illinois"
+          address: {
+            addressLine1: "1251 John Calvin Drive"
+            city: "Harvey"
+            country: "US"
+            postalCode: "60426"
+            region: "Illinois"
+          }
         ) {
           verificationStatus
           inputAddress {
@@ -110,9 +114,9 @@ describe("verifyAddressQuery", () => {
               city
               country
               region
-              postal_code
-              address_line1
-              address_line2
+              postalCode
+              addressLine1
+              addressLine2
             }
             lines
           }
@@ -121,9 +125,9 @@ describe("verifyAddressQuery", () => {
               city
               country
               region
-              postal_code
-              address_line_1
-              address_line_2
+              postalCode
+              addressLine1
+              addressLine2
             }
             lines
           }
@@ -158,7 +162,7 @@ describe("verifyAddressQuery", () => {
       suggestedAddresses: [
         {
           address: {
-            address_line1: "1251 John Calvin Drive",
+            address_line_1: "1251 John Calvin Drive",
             city: "Harvey",
             region: "Illinois",
             postal_code: "60426",
--- src/schema/v2/verifyAddress.ts
+++ src/schema/v2/verifyAddress.ts
@@ -1,3 +1,4 @@
+import { GraphQLFieldConfigArgumentMap, GraphQLInputObjectType } from "graphql"
 import {
   GraphQLEnumType,
   GraphQLFieldConfig,
@@ -16,14 +17,47 @@ type VerifyAddressTypeSource = {
 
 type AddressTypeSource = {
   address: {
-    address_line1: string
-    address_line2: string
+    address_line_1: string
+    address_line_2: string
     city: string
     region: string
     postal_code: string
     country: string
   }
   lines: string[]
+}
+
+const AddressVerificationInput: GraphQLFieldConfigArgumentMap = {
+  address: {
+    type: new GraphQLNonNull(
+      new GraphQLInputObjectType({
+        name: "AddressInput",
+        fields: {
+          addressLine1: { type: new GraphQLNonNull(GraphQLString) },
+          addressLine2: { type: GraphQLString },
+          city: { type: GraphQLString },
+          country: { type: new GraphQLNonNull(GraphQLString) },
+          postalCode: { type: new GraphQLNonNull(GraphQLString) },
+          region: { type: GraphQLString },
+        },
+      })
+    ),
+  },
+}
+
+const addressFieldsFromGravity = {
+  addressLine1: {
+    type: new GraphQLNonNull(GraphQLString),
+    resolve: (source) => source.address_line_1,
+  },
+  addressLine2: {
+    type: new GraphQLNonNull(GraphQLString),
+    resolve: (source) => source.address_line_2,
+  },
+  city: { type: new GraphQLNonNull(GraphQLString) },
+  country: { type: new GraphQLNonNull(GraphQLString) },
+  postalCode: { type: new GraphQLNonNull(GraphQLString) },
+  region: { type: GraphQLString },
 }
 
 const VerificationStatuses = {
@@ -54,14 +88,7 @@ const VerifyAddressType: GraphQLObjectType<
           address: {
             type: new GraphQLObjectType<any, ResolverContext>({
               name: "InputAddress",
-              fields: {
-                address_line1: { type: new GraphQLNonNull(GraphQLString) },
-                address_line2: { type: new GraphQLNonNull(GraphQLString) },
-                city: { type: new GraphQLNonNull(GraphQLString) },
-                country: { type: new GraphQLNonNull(GraphQLString) },
-                postal_code: { type: new GraphQLNonNull(GraphQLString) },
-                region: { type: GraphQLString },
-              },
+              fields: addressFieldsFromGravity,
             }),
           },
           lines: {
@@ -79,14 +106,7 @@ const VerifyAddressType: GraphQLObjectType<
             address: {
               type: new GraphQLObjectType<any, ResolverContext>({
                 name: "SuggestedAddress",
-                fields: {
-                  address_line_1: { type: new GraphQLNonNull(GraphQLString) },
-                  address_line_2: { type: new GraphQLNonNull(GraphQLString) },
-                  city: { type: new GraphQLNonNull(GraphQLString) },
-                  country: { type: new GraphQLNonNull(GraphQLString) },
-                  postal_code: { type: new GraphQLNonNull(GraphQLString) },
-                  region: { type: GraphQLString },
-                },
+                fields: addressFieldsFromGravity,
               }),
             },
             lines: {
@@ -103,17 +123,19 @@ const VerifyAddressType: GraphQLObjectType<
 export const VerifyAddress: GraphQLFieldConfig<any, ResolverContext> = {
   type: VerifyAddressType,
   description: "Verify a given address.",
-  args: {
-    addressLine1: { type: new GraphQLNonNull(GraphQLString) },
-    addressLine2: { type: GraphQLString },
-    city: { type: new GraphQLNonNull(GraphQLString) },
-    country: { type: new GraphQLNonNull(GraphQLString) },
-    postalCode: { type: new GraphQLNonNull(GraphQLString) },
-    region: { type: GraphQLString },
-  },
+  args: AddressVerificationInput,
   resolve: async (
     _,
-    { addressLine1, addressLine2, city, country, postalCode, region },
+    {
+      address: {
+        addressLine1,
+        addressLine2,
+        city,
+        country,
+        postalCode,
+        region,
+      },
+    },
     { verifyAddressLoader }
   ) => {
     if (!verifyAddressLoader) {
