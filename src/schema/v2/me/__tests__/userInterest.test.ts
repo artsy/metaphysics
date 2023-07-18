8b028ec2d756a1571d390dde251bcfc74fa90319
Ole <ole.richter91@gmail.com>
2023-07-18 12:52:41 +0200

    feat: me userInterest field (#5150)
    * feat: me userInterest field
    
    * remove fixture

---
+++ src/schema/v2/me/__tests__/userInterest.test.ts
@@ -0,0 +1,59 @@
+import gql from "lib/gql"
+import { runAuthenticatedQuery } from "schema/v2/test/utils"
+import { ResolverContext } from "types/graphql"
+
+describe("me.userInterest", () => {
+  it("returns user interest", async () => {
+    const query = gql`
+      {
+        me {
+          userInterest(id: "user-interest-id") {
+            internalID
+            interest {
+              __typename
+              ... on Artist {
+                internalID
+                name
+              }
+            }
+          }
+        }
+      }
+    `
+    const meUserInterestLoader = jest.fn(async () => ({
+      interest: {
+        _id: "artist-id",
+        name: "Artist Name",
+        id: "yayoi-kusama",
+        birthday: "10.10.2002",
+      },
+      id: "user-interest-id",
+    }))
+    const meLoader = jest.fn(async () => ({ id: "some-user-id" }))
+
+    const context: Partial<ResolverContext> = {
+      meUserInterestLoader,
+      meLoader,
+    }
+
+    const data = await runAuthenticatedQuery(query, context)
+
+    expect(meLoader).toHaveBeenCalled()
+    expect(meUserInterestLoader).toHaveBeenCalledWith("user-interest-id")
+
+    expect(data).toMatchInlineSnapshot(`
+      Object {
+        "me": Object {
+          "userInterest": Object {
+            "interest": Object {
+              "__typename": "Artist",
+              "internalID": "artist-id",
+              "name": "Artist Name",
+            },
+            "internalID": "user-interest-id",
+          },
+        },
+      }
+    `)
+  })
+})
---
+++ src/schema/v2/me/userInterest.ts
@@ -0,0 +1,21 @@
+import { GraphQLFieldConfig, GraphQLString } from "graphql"
+import { ResolverContext } from "types/graphql"
+import { userInterestType } from "../userInterests"
+
+export const UserInterest: GraphQLFieldConfig<any, ResolverContext> = {
+  type: userInterestType,
+  args: {
+    id: {
+      type: GraphQLString,
+      description: "The ID of the UserInterest",
+    },
+  },
+  description: "Get a user interest",
+  resolve: (_, { id }, { meUserInterestLoader }) => {
+    if (!meUserInterestLoader) {
+      return null
+    }
+
+    return meUserInterestLoader(id)
+  },
+}
