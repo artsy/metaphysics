78d4e74941b67e9b1fbfdbdee9721ff6cf29cd5b
Jon Allured <jon@jonallured.com>
2023-08-24 07:41:54 -0500

    Add not null to verified rep type

--- src/schema/v2/verifiedRepresentative/verifiedRepresentative.ts
+++ src/schema/v2/verifiedRepresentative/verifiedRepresentative.ts
@@ -1,4 +1,4 @@
-import { GraphQLObjectType } from "graphql"
+import { GraphQLNonNull, GraphQLObjectType } from "graphql"
 import { IDFields, NodeInterface } from "schema/v2/object_identification"
 import { ArtistType } from "schema/v2/artist"
 import { PartnerType } from "schema/v2/partner/partner"
@@ -10,13 +10,13 @@ export const VerifiedRepresentativeType = new GraphQLObjectType<any, any>({
     return {
       ...IDFields,
       artist: {
-        type: ArtistType,
+        type: new GraphQLNonNull(ArtistType),
         resolve: ({ artist_id }, {}, { artistLoader }) => {
           return artistLoader(artist_id)
         },
       },
       partner: {
-        type: PartnerType,
+        type: new GraphQLNonNull(PartnerType),
         resolve: ({ partner_id }, {}, { partnerLoader }) => {
           return partnerLoader(partner_id)
         },
