e5d4391fa8c6ba5278584e19d8bd24ed5800aa66
Christopher Pappas <damassi.pappas@gmail.com>
2023-08-28 16:00:06 -0700

    feat(searchCriteria): Stitch in connection to root `Viewer`

--- src/lib/stitching/gravity/v2/stitching.ts
+++ src/lib/stitching/gravity/v2/stitching.ts
@@ -221,6 +221,17 @@ export const gravityStitchingEnvironment = (
       }
 
       extend type Viewer {
+        searchCriteriaConnection(
+          first: Int,
+          last: Int,
+          before: String,
+          after: String,
+          artistID: ID,
+          highQuality: Boolean,
+          partnerID: String,
+          previewByArtist: Boolean
+          since: Int
+        ): SearchCriteriaConnection
         viewingRoomsConnection(first: Int, after: String, statuses: [ViewingRoomStatusEnum!], partnerID: ID): ViewingRoomsConnection
         marketingCollections(
           slugs: [String!]
@@ -1073,6 +1084,19 @@ export const gravityStitchingEnvironment = (
         },
       },
       Viewer: {
+        searchCriteriaConnection: {
+          resolve: (_fragments, args, context, info) => {
+            return info.mergeInfo.delegateToSchema({
+              schema: gravitySchema,
+              operation: "query",
+              fieldName: "_unused_gravity_searchCriteriaConnection",
+              args,
+              context,
+              info,
+            })
+          },
+        },
+
         viewingRoomsConnection: {
           fragment: `
           ... on Viewer {
