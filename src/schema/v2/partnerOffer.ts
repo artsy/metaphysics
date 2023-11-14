b7e13b54d752aed7d86ef51e880db575caf684dc
Kazunari Okuda <saori.cube@gmail.com>
2023-11-14 16:30:20 +0100

    chore: create a new type called PartnerOfferType

---
+++ src/schema/v2/partnerOffer.ts
@@ -0,0 +1,34 @@
+import { date } from "./fields/date"
+import { GraphQLList, GraphQLObjectType, GraphQLString } from "graphql"
+import { ResolverContext } from "types/graphql"
+import { IDFields } from "./object_identification"
+
+const PartnerOfferType = new GraphQLObjectType<any, ResolverContext>({
+  name: "PartnerOffer",
+  fields: () => ({
+    ...IDFields,
+    artworkId: {
+      type: GraphQLString,
+      resolve: ({ artwork_id }) => artwork_id,
+    },
+    createdAt: date(),
+    currency: {
+      type: GraphQLString,
+      description: `Currency abbreviation (e.g. "USD")`,
+    },
+    endAt: date(),
+    id: {
+      type: GraphQLString,
+    },
+    partnerId: {
+      type: GraphQLString,
+      resolve: ({ partner_id }) => partner_id,
+    },
+    priceMinor: {
+      type: GraphQLString,
+    },
+    userIds: {
+      type: new GraphQLList(GraphQLString),
+    },
+  }),
+})
