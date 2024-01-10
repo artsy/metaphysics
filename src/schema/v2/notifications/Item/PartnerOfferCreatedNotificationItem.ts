1c91201180f001976ae66627d56f184a102e63eb
Nikita Skalkin <nikskalkin@gmail.com>
2024-01-10 14:25:58 +0100

    feat(onyx-637): Add PartnerOfferCreated activity event (#5498)
    * feat(onyx-637): Add PartnerOfferCreated activity event
    
    * align with gravity changes
    
    * expires_at to expiresAt
    
    * Align with gravity changes: PartnerOffer as an actor for PartnerOfferCreatedActivity
    
    * use designated date format for experisAt field

---
+++ src/schema/v2/notifications/Item/PartnerOfferCreatedNotificationItem.ts
@@ -0,0 +1,43 @@
+import { GraphQLObjectType } from "graphql"
+import { connectionFromArray } from "graphql-relay"
+import { convertConnectionArgsToGravityArgs } from "lib/helpers"
+import { pageable } from "relay-cursor-paging"
+import { artworkConnection } from "schema/v2/artwork"
+import { date } from "schema/v2/fields/date"
+import { createPageCursors } from "schema/v2/fields/pagination"
+import { ResolverContext } from "types/graphql"
+
+export const PartnerOfferCreatedNotificationItemType = new GraphQLObjectType<
+  any,
+  ResolverContext
+>({
+  name: "PartnerOfferCreatedNotificationItem",
+  fields: {
+    artworksConnection: {
+      type: artworkConnection.connectionType,
+      args: pageable(),
+      resolve: async ({ object_ids }, args, { artworksLoader }) => {
+        const { page, size } = convertConnectionArgsToGravityArgs(args)
+        const body = await artworksLoader({ ids: object_ids })
+        const totalCount = body.length
+
+        return {
+          totalCount,
+          pageCursors: createPageCursors({ page, size }, totalCount),
+          ...connectionFromArray(body, args),
+        }
+      },
+    },
+    expiresAt: {
+      ...date(({ date }) => date),
+      resolve: async ({ actor_ids }, _, { mePartnerOfferLoader }) => {
+        if (!mePartnerOfferLoader) return null
+        if (actor_ids.length === 0) return null
+
+        const { end_at } = await mePartnerOfferLoader(actor_ids[0])
+
+        return end_at
+      },
+    },
+  },
+})
