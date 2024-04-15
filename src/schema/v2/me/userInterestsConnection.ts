4144e1dc542006e00d633c240636e4f01851fd30
dzucconi <mail@damonzucconi.com>
2024-04-15 09:10:09 -0400

    feat(me): adds ability to "search" user interests

--- src/schema/v2/me/userInterestsConnection.ts
+++ src/schema/v2/me/userInterestsConnection.ts
@@ -32,10 +32,44 @@ export const UserInterestsConnection: GraphQLFieldConfig<
       description:
         "Id of the user interests to return if found. Can be an 'Artist' Id or a 'Gene' Id",
     },
+    term: {
+      type: GraphQLString,
+      description:
+        "If present, will search by term. Will ignore page/size. This will not give accurate results if there's more than 100 user interests.",
+    },
   }),
   resolve: async (_, args, { meUserInterestsLoader, userID }) => {
     if (!meUserInterestsLoader) {
       throw new Error("You need to be signed in to perform this action")
+    }
+
+    // We don't have the ability to actually search these results, so just load the first 100
+    // and filter them manually.
+    if (args.term) {
+      const { body } = await meUserInterestsLoader({
+        category: args.category,
+        interest_type: args.interestType,
+        interest_id: args.interestID,
+        page: 1,
+        size: 100,
+        ...(userID && { user_id: userID }),
+      })
+
+      const results = body.filter((node) => {
+        return node.interest.name
+          .toLowerCase()
+          .includes(args.term.toLowerCase())
+      })
+
+      return paginationResolver({
+        args,
+        body: results,
+        offset: 0,
+        page: 1,
+        size: 100,
+        totalCount: results.length,
+        resolveNode: (node) => node.interest,
+      })
     }
 
     const { page, size, offset } = convertConnectionArgsToGravityArgs(args)
