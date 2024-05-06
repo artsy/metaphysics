ef27de77442ad5d305ac2579dc461e8e7b369e16
Christopher Pappas <damassi.pappas@gmail.com>
2024-05-06 13:54:10 -0700

    feat(sync-schema): Update schema sync to also account for Artemis - take 2

--- scripts/push-schema-changes.js
+++ scripts/push-schema-changes.js
@@ -74,6 +74,10 @@ async function main() {
       { repo: "prediction" },
       { repo: "force" },
       { repo: "forque" },
+      // We're pushing two changes to volt; one for Relay and one for Artemis,
+      // our server-side GraphQL client. They take different schema formats.
+      // Keeping this entry separate from the other volt to avoid race condition.
+      { repo: "volt", dest: "vendor/graphql/schema/metaphysics.json" },
       { repo: "volt-v2" },
       { repo: "pulse", dest: "vendor/graphql/schema/metaphysics.json" },
       { repo: "volt", dest: "vendor/graphql/schema/schema.graphql" },
