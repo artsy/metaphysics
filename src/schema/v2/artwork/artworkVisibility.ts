9d30fd39c94c7b6d24c65713affc42b37c0818dd
Jackie Potts <jacqueline.a.potts@gmail.com>
2024-05-28 12:20:46 -0400

    Adjust to use constants

--- src/schema/v2/artwork/artworkVisibility.ts
+++ src/schema/v2/artwork/artworkVisibility.ts
@@ -1,14 +1,14 @@
 import { GraphQLEnumType } from "graphql"
 
-export const ArtworkVisibility = new GraphQLEnumType({
-  name: "Visibility",
-  values: {
-    UNLISTED: { value: "unlisted" },
-    LISTED: { value: "listed" },
-  },
-})
-
 export const ArtworkVisibilityEnumValues = {
   UNLISTED: "unlisted",
   LISTED: "listed",
 }
+
+export const ArtworkVisibility = new GraphQLEnumType({
+  name: "Visibility",
+  values: {
+    UNLISTED: { value: ArtworkVisibilityEnumValues.UNLISTED },
+    LISTED: { value: ArtworkVisibilityEnumValues.LISTED },
+  },
+})
