af4b819e94500bd5645ad78028ec84136d48c83a
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2024-07-17 17:20:56 -0400

    fix: non-nullability

--- src/schema/v2/homeView/index.ts
+++ src/schema/v2/homeView/index.ts
@@ -3,13 +3,14 @@ import {
   GraphQLFieldConfig,
   GraphQLString,
   GraphQLList,
+  GraphQLNonNull,
 } from "graphql"
 import { ResolverContext } from "types/graphql"
 
 // homeView.hello -- hello world
 
 const Hello: GraphQLFieldConfig<void, ResolverContext> = {
-  type: GraphQLString,
+  type: GraphQLNonNull(GraphQLString),
   description: "Hello, world",
   resolve: () => "world",
 }
@@ -21,14 +22,14 @@ const SectionType = new GraphQLObjectType<any, ResolverContext>({
   description: "A generic section in the home view",
   fields: {
     title: {
-      type: GraphQLString,
+      type: GraphQLNonNull(GraphQLString),
       description: "The title of the section",
     },
   },
 })
 
 const Sections: GraphQLFieldConfig<void, ResolverContext> = {
-  type: new GraphQLList(SectionType),
+  type: GraphQLNonNull(GraphQLList(GraphQLNonNull(SectionType))),
   description: "A list of sections on the home view",
   resolve: () => {
     return Array.from({ length: 10 }).map((_, i) => {
@@ -51,7 +52,7 @@ const HomeViewType = new GraphQLObjectType<any, ResolverContext>({
 })
 
 export const HomeView: GraphQLFieldConfig<void, ResolverContext> = {
-  type: HomeViewType,
+  type: GraphQLNonNull(HomeViewType),
   description: "Home view content",
   resolve: () => {
     // dummy response object, otherwise the nested fields won’t work
