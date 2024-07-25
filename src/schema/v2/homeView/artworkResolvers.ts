9f7f4f39e2f859b334604b03f61de28bc57da97a
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2024-07-25 15:50:17 -0400

    feat: wire up placeholder logic for resolving sections' artworks

---
+++ src/schema/v2/homeView/artworkResolvers.ts
@@ -0,0 +1,88 @@
+import type { GraphQLFieldResolver } from "graphql"
+import type { HomeViewSectionKey } from "./getSectionsForUser"
+import type { ResolverContext } from "types/graphql"
+import { convertConnectionArgsToGravityArgs } from "lib/helpers"
+import { paginationResolver } from "../fields/pagination"
+
+/*
+ * Resolvers for home view artwork sections
+ */
+
+// the resolvers
+
+const newWorksForYouResolver: GraphQLFieldResolver<any, ResolverContext> = (
+  _parent,
+  args,
+  _context,
+  _info
+) => {
+  // TODO: use actual loader
+
+  const stubData = [{ id: "TODO-new_works_for_you" }]
+  const { page, size, offset } = convertConnectionArgsToGravityArgs(args)
+  const data = stubData.slice(offset, offset + size)
+  const totalCount = stubData.length
+
+  return paginationResolver({
+    totalCount,
+    offset,
+    page,
+    size,
+    body: data,
+    args,
+  })
+}
+
+const recentlyViewedArtworksResolver: GraphQLFieldResolver<
+  any,
+  ResolverContext
+> = (_parent, args, _context, _info) => {
+  // TODO: use actual loader
+
+  const stubData = [{ id: "TODO-recently_viewed_artworks" }]
+  const { page, size, offset } = convertConnectionArgsToGravityArgs(args)
+  const data = stubData.slice(offset, offset + size)
+  const totalCount = stubData.length
+
+  return paginationResolver({
+    totalCount,
+    offset,
+    page,
+    size,
+    body: data,
+    args,
+  })
+}
+
+const auctionLotsForYouResolver: GraphQLFieldResolver<
+  any,
+  ResolverContext
+> = async (_parent, args, _context, _info) => {
+  // TODO: use actual loader
+
+  const stubData = [{ id: "TODO-auction_lots_for_you" }]
+  const { page, size, offset } = convertConnectionArgsToGravityArgs(args)
+  const data = stubData.slice(offset, offset + size)
+  const totalCount = stubData.length
+
+  return paginationResolver({
+    totalCount,
+    offset,
+    page,
+    size,
+    body: data,
+    args,
+  })
+}
+
+// the section-to-resolver mapping
+
+type SectionResolverMap = Partial<
+  Record<HomeViewSectionKey, GraphQLFieldResolver<any, ResolverContext>>
+>
+
+export const ARTWORK_RESOLVERS: SectionResolverMap = {
+  NEW_WORKS_FOR_YOU: newWorksForYouResolver,
+  AUCTION_LOTS_FOR_YOU: auctionLotsForYouResolver,
+  RECENTLY_VIEWED_ARTWORKS: recentlyViewedArtworksResolver,
+}
