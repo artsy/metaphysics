4eda3207b50ebff371c99e61f71bd5cdb1033b8f
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2024-07-26 14:32:26 -0400

    feat: fulfill 'new works for you' via existing resolver

--- src/schema/v2/homeView/artworkResolvers.ts
+++ src/schema/v2/homeView/artworkResolvers.ts
@@ -1,6 +1,7 @@
 import type { GraphQLFieldResolver } from "graphql"
 import type { HomeViewSectionKey } from "./getSectionsForUser"
 import type { ResolverContext } from "types/graphql"
+import { artworksForUser } from "../artworksForUser"
 import { convertConnectionArgsToGravityArgs } from "lib/helpers"
 import { paginationResolver } from "../fields/pagination"
 
@@ -10,27 +11,30 @@ import { paginationResolver } from "../fields/pagination"
 
 // the resolvers
 
-const newWorksForYouResolver: GraphQLFieldResolver<any, ResolverContext> = (
-  _parent,
-  args,
-  _context,
-  _info
-) => {
-  // TODO: use actual loader
+const newWorksForYouResolver: GraphQLFieldResolver<
+  any,
+  ResolverContext
+> = async (parent, args, context, info) => {
+  const finalArgs = {
+    // formerly specified client-side
+    maxWorksPerArtist: 3,
+    includeBackfill: true,
+    first: args.first,
+    version: "C",
+    excludeDislikedArtworks: true,
+    excludeArtworkIds: [],
 
-  const stubData = [{ id: "TODO-new_works_for_you" }]
-  const { page, size, offset } = convertConnectionArgsToGravityArgs(args)
-  const data = stubData.slice(offset, offset + size)
-  const totalCount = stubData.length
+    ...args,
+  }
 
-  return paginationResolver({
-    totalCount,
-    offset,
-    page,
-    size,
-    body: data,
-    args,
-  })
+  const result = await artworksForUser.resolve!(
+    parent,
+    finalArgs,
+    context,
+    info
+  )
+
+  return result
 }
 
 const recentlyViewedArtworksResolver: GraphQLFieldResolver<
