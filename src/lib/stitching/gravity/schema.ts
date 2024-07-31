cc0b7c352df2e006d8cc2dbd50b136ef48fc0bf5
egdbear <egdfer@gmail.com>
2024-07-31 12:03:42 +0200

    feat: add unstitched marketing categories

--- src/lib/stitching/gravity/schema.ts
+++ src/lib/stitching/gravity/schema.ts
@@ -59,6 +59,7 @@ export const executableGravitySchema = () => {
   if (config.USE_UNSTITCHED_MARKETING_COLLECTION_SCHEMA) {
     duplicatedTypes.push("MarketingCollection")
     duplicatedTypes.push("MarketingCollectionGroup")
+    duplicatedTypes.push("MarketingCollectionCategory")
   }
   // Types which come from Gravity that are not (yet) needed in MP.
   // In the future, these can be removed from this list as they are needed.
---
+++ src/schema/v2/__tests__/marketingCategories.test.ts
@@ -0,0 +1,88 @@
+import gql from "lib/gql"
+import { runQuery } from "../test/utils"
+import config from "config"
+
+const marketingCategoriesData = [
+  {
+    name: "Contemporary",
+    collections: [
+      {
+        id: "percys-z-collection-1",
+        slug: "percys-z-collection-1",
+        title: "Percy Z Collection",
+      },
+      {
+        id: "fiby-z-collection-2",
+        slug: "fiby-z-collection-2",
+        title: "Fiby Z Collection 2",
+      },
+    ],
+  },
+  {
+    name: "Impressionism",
+    collections: [
+      {
+        id: "fiby-z-collection-2",
+        slug: "fiby-z-collection-2",
+        title: "Fiby Z Collection 2",
+      },
+    ],
+  },
+]
+
+beforeAll(() => {
+  config.USE_UNSTITCHED_MARKETING_COLLECTION_SCHEMA = true
+})
+
+afterAll(() => {
+  config.USE_UNSTITCHED_MARKETING_COLLECTION_SCHEMA = false
+})
+
+describe("MarketingCategories", () => {
+  it("returns a list of marketing categories", async () => {
+    const query = gql`
+      {
+        marketingCategories {
+          name
+          collections {
+            slug
+            title
+          }
+        }
+      }
+    `
+    const context = {
+      marketingCategoriesLoader: () =>
+        Promise.resolve({ body: marketingCategoriesData }),
+    } as any
+
+    const data = await runQuery(query, context)
+
+    expect(data).toEqual({
+      marketingCategories: [
+        {
+          name: "Contemporary",
+          collections: [
+            {
+              slug: "percys-z-collection-1",
+              title: "Percy Z Collection",
+            },
+            {
+              slug: "fiby-z-collection-2",
+              title: "Fiby Z Collection 2",
+            },
+          ],
+        },
+        {
+          name: "Impressionism",
+          collections: [
+            {
+              slug: "fiby-z-collection-2",
+              title: "Fiby Z Collection 2",
+            },
+          ],
+        },
+      ],
+    })
+  })
+})
