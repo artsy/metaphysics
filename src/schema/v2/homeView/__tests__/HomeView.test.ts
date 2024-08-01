8e21ecbaac17ea3b45885aa3f3ccc351e5a79215
devon blandin <dblandin@gmail.com>
2024-08-01 09:03:02 -0400

    feat: add homeView > section query field (#5874)
    **Example**
    
    ```graphql
    {
      homeView {
        section(id: "home-view-section-auction-lots-for-you") {
          __typename
          ... on GenericHomeViewSection {
            component {
              title
            }
          }
        }
      }
    }
    ```

--- src/schema/v2/homeView/__tests__/HomeView.test.ts
+++ src/schema/v2/homeView/__tests__/HomeView.test.ts
@@ -2,58 +2,99 @@ import gql from "lib/gql"
 import { runQuery } from "schema/v2/test/utils"
 
 describe("homeView", () => {
-  const query = gql`
-    {
-      homeView {
-        sectionsConnection(first: 2) {
-          edges {
-            node {
-              ... on GenericHomeViewSection {
-                component {
-                  title
+  describe("sectionsConnection", () => {
+    const query = gql`
+      {
+        homeView {
+          sectionsConnection(first: 2) {
+            edges {
+              node {
+                __typename
+                ... on GenericHomeViewSection {
+                  component {
+                    title
+                  }
                 }
               }
             }
           }
         }
       }
+    `
+
+    const context = {
+      authenticatedLoaders: {
+        meLoader: jest.fn().mockReturnValue({ type: "User" }),
+      },
     }
-  `
 
-  const context = {
-    authenticatedLoaders: {
-      meLoader: jest.fn().mockReturnValue({ type: "User" }),
-    },
-  }
+    it("returns a connection of home view sections", async () => {
+      const { homeView } = await runQuery(query, context)
 
-  it("returns a connection of home view sections", async () => {
-    const { homeView } = await runQuery(query, context)
+      expect(homeView.sectionsConnection.edges).toHaveLength(2)
+    })
 
-    expect(homeView.sectionsConnection.edges).toHaveLength(2)
+    it("returns requested data for each section", async () => {
+      const { homeView } = await runQuery(query, context)
+
+      expect(homeView.sectionsConnection).toMatchInlineSnapshot(`
+        Object {
+          "edges": Array [
+            Object {
+              "node": Object {
+                "__typename": "ArtworksRailHomeViewSection",
+                "component": Object {
+                  "title": "New works for you",
+                },
+              },
+            },
+            Object {
+              "node": Object {
+                "__typename": "ArtworksRailHomeViewSection",
+                "component": Object {
+                  "title": "Auction lots for you",
+                },
+              },
+            },
+          ],
+        }
+      `)
+    })
   })
 
-  it("returns requested data for each section", async () => {
-    const { homeView } = await runQuery(query, context)
-
-    expect(homeView.sectionsConnection).toMatchInlineSnapshot(`
-      Object {
-        "edges": Array [
-          Object {
-            "node": Object {
-              "component": Object {
-                "title": "New works for you",
-              },
-            },
-          },
-          Object {
-            "node": Object {
-              "component": Object {
-                "title": "Auction lots for you",
-              },
-            },
-          },
-        ],
+  describe("section", () => {
+    const query = gql`
+      {
+        homeView {
+          section(id: "home-view-section-auction-lots-for-you") {
+            __typename
+            ... on GenericHomeViewSection {
+              component {
+                title
+              }
+            }
+          }
+        }
       }
-    `)
+    `
+
+    const context = {
+      authenticatedLoaders: {
+        meLoader: jest.fn().mockReturnValue({ type: "User" }),
+      },
+    }
+
+    it("returns the requested section", async () => {
+      const { homeView } = await runQuery(query, context)
+
+      expect(homeView.section).toMatchInlineSnapshot(`
+        Object {
+          "__typename": "ArtworksRailHomeViewSection",
+          "component": Object {
+            "title": "Auction lots for you",
+          },
+        }
+      `)
+    })
   })
 })
--- src/schema/v2/homeView/getSectionsForUser.ts
+++ src/schema/v2/homeView/getSectionsForUser.ts
@@ -1,10 +1,11 @@
 import { ResolverContext } from "types/graphql"
 import {
-  AuctionLotsForYouResolver,
-  NewWorksForYouResolver,
-  RecentlyViewedArtworksResolver,
-} from "./artworkResolvers"
-import { GraphQLFieldResolver } from "graphql"
+  AuctionLotsForYou,
+  HomeViewSection,
+  NewWorksForYou,
+  RecentlyViewedArtworks,
+  SuggestedArtists,
+} from "./sections"
 
 export async function getSectionsForUser(
   context: ResolverContext
@@ -22,65 +23,19 @@ export async function getSectionsForUser(
 
   if (me.type === "Admin") {
     sections = [
-      RECENTLY_VIEWED_ARTWORKS,
-      SUGGESTED_ARTISTS,
-      AUCTION_LOTS_FOR_YOU,
-      NEW_WORKS_FOR_YOU,
+      RecentlyViewedArtworks,
+      SuggestedArtists,
+      AuctionLotsForYou,
+      NewWorksForYou,
     ]
   } else {
     sections = [
-      NEW_WORKS_FOR_YOU,
-      AUCTION_LOTS_FOR_YOU,
-      SUGGESTED_ARTISTS,
-      RECENTLY_VIEWED_ARTWORKS,
+      NewWorksForYou,
+      AuctionLotsForYou,
+      SuggestedArtists,
+      RecentlyViewedArtworks,
     ]
   }
 
   return sections
-}
-
-// stub sections
-
-type HomeViewSection = {
-  id: string
-  type: string
-  component: {
-    title: string
-  }
-  resolver?: GraphQLFieldResolver<any, ResolverContext>
-}
-
-const RECENTLY_VIEWED_ARTWORKS: HomeViewSection = {
-  id: "home-view-section-recently-viewed-artworks",
-  type: "ArtworksRailHomeViewSection",
-  component: {
-    title: "Recently viewed works",
-  },
-  resolver: RecentlyViewedArtworksResolver,
-}
-
-const SUGGESTED_ARTISTS: HomeViewSection = {
-  id: "home-view-section-suggested-artists",
-  type: "ArtistsRailHomeViewSection",
-  component: {
-    title: "Suggested artists for you",
-  },
-}
-
-const AUCTION_LOTS_FOR_YOU: HomeViewSection = {
-  id: "home-view-section-auction-lots-for-you",
-  type: "ArtworksRailHomeViewSection",
-  component: {
-    title: "Auction lots for you",
-  },
-  resolver: AuctionLotsForYouResolver,
-}
-
-const NEW_WORKS_FOR_YOU: HomeViewSection = {
-  id: "home-view-section-new-works-for-you",
-  type: "ArtworksRailHomeViewSection",
-  component: {
-    title: "New works for you",
-  },
-  resolver: NewWorksForYouResolver,
 }
