d1419db333d2959bd8b81e362dc247013f92bf9a
Erik <9088720+erikdstock@users.noreply.github.com>
2024-08-15 07:40:06 -0500

    implement auction time signals (#5889)
    - remove ifFutureDate() filtering helper
    - skip unnecessary requests if fields aren't requested

--- src/schema/v2/artwork/collectorSignals.ts
+++ src/schema/v2/artwork/collectorSignals.ts
@@ -1,8 +1,25 @@
-import { GraphQLFieldConfig } from "graphql"
+import { GraphQLBoolean, GraphQLFieldConfig, GraphQLString } from "graphql"
 import { GraphQLInt, GraphQLObjectType } from "graphql"
 import { ResolverContext } from "types/graphql"
 import { PartnerOfferToCollectorType } from "../partnerOfferToCollector"
 import { isFeatureFlagEnabled } from "lib/featureFlags"
+import { some } from "lodash"
+import { isFieldRequested } from "lib/isFieldRequested"
+
+const NOW = new Date()
+
+interface ActiveLotData {
+  saleArtwork: {
+    bidder_positions_count: number
+    extended_bidding_end_at?: string
+    end_at?: string
+  }
+  sale: {
+    registration_ends_at: string
+    live_start_at: string
+    ended_at?: string
+  }
+}
 
 export const CollectorSignals: GraphQLFieldConfig<any, ResolverContext> = {
   type: new GraphQLObjectType({
@@ -11,11 +28,32 @@ export const CollectorSignals: GraphQLFieldConfig<any, ResolverContext> = {
     fields: {
       bidCount: {
         type: GraphQLInt,
-        description: "Lot bid count",
+        description: "Bid count on lots open for bidding",
       },
       lotWatcherCount: {
         type: GraphQLInt,
-        description: "Lot watcher count",
+        description: "Lot watcher count on lots open for bidding",
+      },
+      registrationEndsAt: {
+        type: GraphQLString,
+        description: "Pending auction registration end time",
+      },
+      lotClosesAt: {
+        type: GraphQLString,
+        description: "Pending auction lot end time for bidding",
+      },
+      onlineBiddingExtended: {
+        type: GraphQLBoolean,
+        description:
+          "Auction lot bidding period extended due to last-minute bids",
+      },
+      liveStartAt: {
+        type: GraphQLString,
+        description: "Auction live bidding start time",
+      },
+      liveBiddingStarted: {
+        type: GraphQLBoolean,
+        description: "Live bidding has started on this lot's auction",
       },
       partnerOffer: {
         type: PartnerOfferToCollectorType,
@@ -25,8 +63,12 @@ export const CollectorSignals: GraphQLFieldConfig<any, ResolverContext> = {
   }),
   description: "Collector signals on artwork",
 
-  resolve: async (artwork, {}, ctx) => {
-    const collectorSignals = await collectorSignalsLoader(artwork, ctx)
+  resolve: async (artwork, {}, ctx, resolveInfo) => {
+    const collectorSignals = await collectorSignalsLoader(
+      artwork,
+      ctx,
+      resolveInfo
+    )
     return collectorSignals
   },
 }
@@ -35,13 +77,36 @@ interface CollectorSignals {
   bidCount?: number
   lotWatcherCount?: number
   partnerOffer?: { endAt: string }
+  registrationEndsAt?: string
+  liveStartAt?: string
+  liveBiddingStarted?: boolean
+  onlineBiddingExtended?: boolean
+  lotClosesAt?: string
 }
+
+const AUCTION_FIELDS = [
+  "bidCount",
+  "lotWatcherCount",
+  "registrationEndsAt",
+  "liveStartAt",
+  "liveBiddingStarted",
+  "lotClosesAt",
+  "onlineBiddingExtended",
+]
 
 const collectorSignalsLoader = async (
   artwork,
-  ctx
+  ctx,
+  resolveInfo
 ): Promise<CollectorSignals> => {
-  let bidCount, lotWatcherCount, partnerOffer
+  let bidCount,
+    lotWatcherCount,
+    partnerOffer,
+    registrationEndsAt,
+    liveStartAt,
+    liveBiddingStarted,
+    lotClosesAt,
+    onlineBiddingExtended
 
   const artworkId = artwork.id
 
@@ -59,9 +124,18 @@ const collectorSignalsLoader = async (
     unleashContext
   )
 
+  const auctionSignalsRequested =
+    auctionsCollectorSignalsEnabled &&
+    isInSale &&
+    some(AUCTION_FIELDS, (key) => isFieldRequested(key, resolveInfo))
+
+  const partnerOfferSignalsRequested =
+    partnerOfferCollectorSignalsEnabled &&
+    isFieldRequested("partnerOffer", resolveInfo)
+
   // Handle signals for auction artworks
-  if (isInSale && auctionsCollectorSignalsEnabled) {
-    const activeSaleArtwork = await getActiveSaleArtwork(
+  if (auctionSignalsRequested) {
+    const activeLotData = await getActiveSaleArtwork(
       {
         artworkId,
         saleIds: artwork.sale_ids,
@@ -69,18 +143,55 @@ const collectorSignalsLoader = async (
       ctx
     )
 
-    if (activeSaleArtwork) {
+    const activeAuctionLot = activeLotData && !activeLotData.sale.ended_at
+    if (activeAuctionLot) {
+      const { saleArtwork, sale } = activeLotData
+
       if (artwork.recent_saves_count) {
         lotWatcherCount = artwork.recent_saves_count
       }
-      if (activeSaleArtwork.bidder_positions_count) {
-        bidCount = activeSaleArtwork.bidder_positions_count
+
+      if (saleArtwork.bidder_positions_count) {
+        bidCount = saleArtwork.bidder_positions_count
+      }
+
+      const registrationEndAtDate =
+        sale.registration_ends_at && new Date(sale.registration_ends_at)
+
+      if (registrationEndAtDate && registrationEndAtDate > NOW) {
+        registrationEndsAt = registrationEndAtDate.toISOString()
+      }
+
+      const saleLiveStartAtDate =
+        sale.live_start_at && new Date(sale.live_start_at)
+      if (saleLiveStartAtDate) {
+        if (saleLiveStartAtDate > NOW) {
+          liveBiddingStarted = false
+          liveStartAt = saleLiveStartAtDate.toISOString()
+        } else {
+          liveBiddingStarted = true
+        }
+      }
+
+      const extendedBiddingEndAtDate =
+        saleArtwork.extended_bidding_end_at &&
+        new Date(saleArtwork.extended_bidding_end_at)
+
+      if (extendedBiddingEndAtDate && extendedBiddingEndAtDate > NOW) {
+        onlineBiddingExtended = true
+        lotClosesAt = extendedBiddingEndAtDate.toISOString()
+      } else {
+        onlineBiddingExtended = false
+
+        const lotClosesAtDate =
+          saleArtwork.end_at && new Date(saleArtwork.end_at)
+        lotClosesAt = lotClosesAtDate && lotClosesAtDate.toISOString()
       }
     }
   }
 
   // Handle signals for non-auction artworks
-  if (artwork.purchasable && partnerOfferCollectorSignalsEnabled) {
+  if (artwork.purchasable && partnerOfferSignalsRequested) {
     if (ctx.mePartnerOffersLoader) {
       const partnerOffers = await ctx.mePartnerOffersLoader({
         artwork_id: artworkId,
@@ -95,20 +206,23 @@ const collectorSignalsLoader = async (
   }
 
   return {
+    // auction signals
     bidCount,
+    liveBiddingStarted,
+    liveStartAt,
+    lotClosesAt,
     lotWatcherCount,
+    onlineBiddingExtended,
+    registrationEndsAt,
+    // purchasable signals
     partnerOffer,
   }
-}
-
-interface SaleArtwork {
-  bidder_positions_count: number
 }
 
 const getActiveSaleArtwork = async (
   { artworkId, saleIds },
   ctx
-): Promise<SaleArtwork | null> => {
+): Promise<ActiveLotData | null> => {
   if (!saleIds?.length) {
     return null
   }
@@ -118,7 +232,7 @@ const getActiveSaleArtwork = async (
     is_auction: true,
     live: true,
   })
-  const activeAuction = sales[0]
+  const activeAuction = sales?.[0]
 
   if (!activeAuction) {
     return null
@@ -130,5 +244,5 @@ const getActiveSaleArtwork = async (
       saleArtworkId: artworkId,
     })) ?? null
 
-  return saleArtwork
+  return { saleArtwork, sale: activeAuction }
 }
