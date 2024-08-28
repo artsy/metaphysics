2ea24abe7186b895533f32d40814d06e85326ef6
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2024-08-28 15:35:55 -0400

    feat: introduce a legacy zone for existing sections

--- src/schema/v2/homeView/getSectionsForUser.ts
+++ src/schema/v2/homeView/getSectionsForUser.ts
@@ -1,89 +1,14 @@
 import { ResolverContext } from "types/graphql"
-import {
-  LatestActivity,
-  AuctionLotsForYou,
-  CuratorsPicksEmerging,
-  FeaturedFairs,
-  HeroUnits,
-  HomeViewSection,
-  LatestArticles,
-  MarketingCollections,
-  NewWorksForYou,
-  NewWorksFromGalleriesYouFollow,
-  RecentlyViewedArtworks,
-  RecommendedArtists,
-  ShowsForYou,
-  SimilarToRecentlyViewedArtworks,
-  TrendingArtists,
-  ViewingRooms,
-  LatestAuctionResults,
-  News,
-  Auctions,
-  ActiveBids,
-  RecommendedArtworks,
-} from "./sections"
+import { HomeViewSection } from "./sections"
+import { getLegacyZoneSections } from "./zones/legacy"
 
 export async function getSectionsForUser(
   context: ResolverContext
 ): Promise<HomeViewSection[]> {
-  /*
-   * FAKE temporary placeholder logic for determining the sections that a user will see
-   */
-  const { meLoader } = context.authenticatedLoaders
+  const legacyZoneSections = await getLegacyZoneSections(context)
 
-  if (!meLoader) throw new Error("You must be signed in to see this content.")
-
-  const me = await meLoader()
-
-  let sections: HomeViewSection[] = []
-
-  if (me.type === "Admin") {
-    sections = [
-      LatestActivity,
-      LatestAuctionResults,
-      ActiveBids,
-      Auctions,
-      LatestArticles,
-      RecentlyViewedArtworks,
-      ShowsForYou,
-      MarketingCollections,
-      TrendingArtists,
-      FeaturedFairs,
-      CuratorsPicksEmerging,
-      SimilarToRecentlyViewedArtworks,
-      AuctionLotsForYou,
-      NewWorksForYou,
-      HeroUnits,
-      NewWorksFromGalleriesYouFollow,
-      RecommendedArtists,
-      ViewingRooms,
-      News,
-      RecommendedArtworks,
-    ]
-  } else {
-    sections = [
-      News,
-      LatestActivity,
-      ActiveBids,
-      Auctions,
-      CuratorsPicksEmerging,
-      SimilarToRecentlyViewedArtworks,
-      ShowsForYou,
-      MarketingCollections,
-      FeaturedFairs,
-      NewWorksForYou,
-      HeroUnits,
-      AuctionLotsForYou,
-      LatestArticles,
-      RecommendedArtists,
-      TrendingArtists,
-      NewWorksFromGalleriesYouFollow,
-      RecommendedArtworks,
-      RecentlyViewedArtworks,
-      LatestAuctionResults,
-      ViewingRooms,
-    ]
-  }
-
-  return sections
+  return [
+    ...legacyZoneSections,
+    // other zones’ sections TK
+  ]
 }
---
+++ src/schema/v2/homeView/zones/__tests__/legacy.test.ts
@@ -0,0 +1,63 @@
+import { ResolverContext } from "types/graphql"
+import { getLegacyZoneSections } from "../legacy"
+
+describe("getLegacyZoneSections", () => {
+  describe("with an authenticated user", () => {
+    it("returns the correct sections", async () => {
+      const context: Partial<ResolverContext> = {
+        accessToken: "some-token",
+      }
+
+      const sections = await getLegacyZoneSections(context as ResolverContext)
+      const sectionIds = sections.map((section) => section.id)
+
+      expect(sectionIds).toMatchInlineSnapshot(`
+        Array [
+          "home-view-section-latest-activity",
+          "home-view-section-new-works-for-you",
+          "home-view-section-hero-units",
+          "home-view-section-auction-lots-for-you",
+          "home-view-section-auctions",
+          "home-view-section-latest-auction-results",
+          "home-view-section-latest-articles",
+          "home-view-section-news",
+          "home-view-section-curators-picks-emerging",
+          "home-view-section-marketing-collections",
+          "home-view-section-new-works-from-galleries-you-follow",
+          "home-view-section-recommended-artists",
+          "home-view-section-trending-artists",
+          "home-view-section-recently-viewed-artworks",
+          "home-view-section-similar-to-recently-viewed-artworks",
+          "home-view-section-viewing-rooms",
+          "home-view-section-shows-for-you",
+          "home-view-section-featured-fairs",
+        ]
+      `)
+    })
+  })
+
+  describe("without an authenticated user", () => {
+    it("returns the correct sections", async () => {
+      const context: Partial<ResolverContext> = {
+        accessToken: undefined,
+      }
+
+      const sections = await getLegacyZoneSections(context as ResolverContext)
+      const sectionIds = sections.map((section) => section.id)
+
+      expect(sectionIds).toMatchInlineSnapshot(`
+        Array [
+          "home-view-section-hero-units",
+          "home-view-section-auctions",
+          "home-view-section-latest-articles",
+          "home-view-section-news",
+          "home-view-section-curators-picks-emerging",
+          "home-view-section-marketing-collections",
+          "home-view-section-trending-artists",
+          "home-view-section-viewing-rooms",
+          "home-view-section-featured-fairs",
+        ]
+      `)
+    })
+  })
+})
