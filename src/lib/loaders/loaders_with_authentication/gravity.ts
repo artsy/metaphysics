808d1cb52a339e8163127dff63719800f7982114
Janae Edwards <edwards.janae@gmail.com>
2024-09-19 15:54:30 -0400

    created commerce opt in loader

--- src/lib/loaders/loaders_with_authentication/gravity.ts
+++ src/lib/loaders/loaders_with_authentication/gravity.ts
@@ -62,6 +62,11 @@ export default (accessToken, userID, opts) => {
       { headers: true }
     ),
     collectorProfileSummaryLoader: gravityLoader("collector_profile_summary"),
+    commerceOptInLoader: gravityLoader(
+      (id) => `partner/${id}/opt_in_artworks_into_commerce`,
+      {},
+      { method: "PUT" }
+    ),
     createAccountRequestLoader: gravityLoader(
       "account_requests",
       {},
