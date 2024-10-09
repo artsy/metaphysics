afb5f5512a520b1021165e482028b1c7155d1600
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2024-10-09 07:03:56 -0400

    feat(homeView): add a showsConnection to HomeViewSectionShows (#6107)
    * feat: allow HomeViewSectionShows to return a showsConnection
    
    * feat: allow HomeViewSectionShows showsConnection to take a near arg
    
    This is a special case of a section's data connection taking an extra arg.
    Unavoidable in this case, because the geolocation data will be coming from
    the client device.
    
    * feat: resolve ShowsForYou data via meShowsLoader

--- src/schema/v2/homeView/sectionTypes/Shows.ts
+++ src/schema/v2/homeView/sectionTypes/Shows.ts
@@ -4,6 +4,10 @@ import { NodeInterface } from "../../object_identification"
 import { HomeViewGenericSectionInterface } from "./GenericSectionInterface"
 import { HomeViewSectionTypeNames } from "./names"
 import { standardSectionFields } from "./GenericSectionInterface"
+import { ShowsConnection } from "schema/v2/show"
+import { pageable } from "relay-cursor-paging"
+import { emptyConnection } from "schema/v2/fields/pagination"
+import Near from "schema/v2/input_fields/near"
 
 export const HomeViewShowsSectionType = new GraphQLObjectType<
   any,
@@ -14,5 +18,17 @@ export const HomeViewShowsSectionType = new GraphQLObjectType<
   interfaces: [HomeViewGenericSectionInterface, NodeInterface],
   fields: {
     ...standardSectionFields,
+
+    showsConnection: {
+      type: ShowsConnection.connectionType,
+      args: pageable({
+        near: {
+          type: Near,
+          description: "Include shows within a radius of the provided location",
+        },
+      }),
+      resolve: (parent, ...rest) =>
+        parent.resolver ? parent.resolver(parent, ...rest) : emptyConnection,
+    },
   },
 })
