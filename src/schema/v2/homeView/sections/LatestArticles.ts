35189608da893002ec7605c9edfea60e44988d6d
Mounir Dhahri <soussouheros@gmail.com>
2024-10-22 11:19:27 +0200

    feat: fix: latest articles broken sort

--- src/schema/v2/homeView/sections/LatestArticles.ts
+++ src/schema/v2/homeView/sections/LatestArticles.ts
@@ -3,6 +3,7 @@ import { HomeViewSection } from "."
 import { withHomeViewTimeout } from "../helpers/withHomeViewTimeout"
 import { HomeViewSectionTypeNames } from "../sectionTypes/names"
 import ArticlesConnection from "schema/v2/articlesConnection"
+import ArticleSorts from "schema/v2/sorts/article_sorts"
 
 export const LatestArticles: HomeViewSection = {
   id: "home-view-section-latest-articles",
@@ -19,5 +20,22 @@ export const LatestArticles: HomeViewSection = {
   },
   requiresAuthentication: false,
 
-  resolver: withHomeViewTimeout(ArticlesConnection.resolve!),
+  resolver: withHomeViewTimeout(async (parent, args, context, info) => {
+    const finalArgs = {
+      // formerly specified client-side
+      published: true,
+      sort: ArticleSorts.type.getValue("PUBLISHED_AT_DESC")?.value,
+      featured: true,
+      ...args,
+    }
+
+    const result = await ArticlesConnection.resolve!(
+      parent,
+      finalArgs,
+      context,
+      info
+    )
+
+    return result
+  }),
 }
