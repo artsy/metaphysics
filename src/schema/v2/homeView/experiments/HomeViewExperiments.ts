3995f2df761c3e7d24d5d9b9b48d8fc6a56e28f0
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2024-10-24 16:16:44 -0400

    refactor: HomeViewExperiment → UnleashFeatureFlag

--- src/schema/v2/homeView/experiments/HomeViewExperiments.ts
+++ src/schema/v2/homeView/experiments/HomeViewExperiments.ts
@@ -1,11 +1,4 @@
-import {
-  GraphQLObjectType,
-  GraphQLNonNull,
-  GraphQLString,
-  GraphQLFieldConfig,
-  GraphQLList,
-  GraphQLInt,
-} from "graphql"
+import { GraphQLNonNull, GraphQLFieldConfig, GraphQLList } from "graphql"
 import {
   FeatureFlag,
   getExperimentVariant,
@@ -13,54 +6,10 @@ import {
 } from "lib/featureFlags"
 import { ResolverContext } from "types/graphql"
 import { CURRENTLY_RUNNING_EXPERIMENTS } from "./experiments"
-
-const HomeViewExperiment = new GraphQLObjectType<any, ResolverContext>({
-  name: "HomeViewExperiment",
-  description: "An A/B test for home view content",
-  fields: {
-    name: {
-      type: GraphQLNonNull(GraphQLString),
-      description: "The name of the experiment",
-    },
-    enabled: {
-      type: GraphQLNonNull(GraphQLString),
-      description: "Whether the experiment is enabled",
-    },
-    service: {
-      type: GraphQLNonNull(GraphQLString),
-    },
-    description: {
-      type: GraphQLString,
-      description: "A description of the experiment",
-    },
-    variant: {
-      type: GraphQLString,
-      description: "The name of the experimental variant being shown currently",
-    },
-    variants: {
-      description: "The variants available within this experiment",
-      type: new GraphQLList(
-        new GraphQLObjectType<any, ResolverContext>({
-          name: "HomeViewExperimentVariant",
-          fields: {
-            name: {
-              type: GraphQLNonNull(GraphQLString),
-            },
-            weight: {
-              type: GraphQLNonNull(GraphQLInt),
-            },
-            stickiness: {
-              type: GraphQLString,
-            },
-          },
-        })
-      ),
-    },
-  },
-})
+import { UnleashFeatureFlag } from "schema/v2/unleashFeatureFlag"
 
 export const HomeViewExperiments: GraphQLFieldConfig<any, ResolverContext> = {
-  type: GraphQLNonNull(GraphQLList(HomeViewExperiment)),
+  type: GraphQLNonNull(GraphQLList(UnleashFeatureFlag)),
   description: "Currently running A/B tests for home view content",
   resolve: (_parent, _args, context, _info) => {
     const experiments = CURRENTLY_RUNNING_EXPERIMENTS.map(
@@ -70,7 +19,6 @@ export const HomeViewExperiments: GraphQLFieldConfig<any, ResolverContext> = {
 
         return {
           ...flag,
-          service: "unleash",
           variant: variant ? variant.name : null,
         }
       }
