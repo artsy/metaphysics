13d0911e39264c7c268689de25bc6831824fe21f
Dejan <1176374+egdbear@users.noreply.github.com>
2024-10-30 13:23:51 +0100

    feat: add custom sorting to marketing collections (#6187)
    * feat: add custom sorting to marketing collections
    
    * updates
    
    * updates

--- src/schema/v2/homeView/sections/ExploreByCategory.ts
+++ src/schema/v2/homeView/sections/ExploreByCategory.ts
@@ -2,42 +2,16 @@ import { ContextModule, OwnerType } from "@artsy/cohesion"
 import { HomeViewSection } from "."
 import { HomeViewSectionTypeNames } from "../sectionTypes/names"
 import { connectionFromArray } from "graphql-relay"
+import { marketingCollectionCategories } from "lib/marketingCollectionCategories"
 
-const marketingColletionCategories = {
-  Medium: {
-    id: "Medium",
-    title: "Medium",
-    imageUrl:
-      "https://files.artsy.net/images/collections-mediums-category.jpeg",
-  },
-  Movement: {
-    id: "Movement",
-    title: "Movement",
-    imageUrl:
-      "https://files.artsy.net/images/collections-movement-category.jpeg",
-  },
-  "Collect by Size": {
-    id: "Collect by Size",
-    title: "Size",
-    imageUrl: "https://files.artsy.net/images/collections-size-category.jpeg",
-  },
-  "Collect by Color": {
-    id: "Collect by Color",
-    title: "Color",
-    imageUrl: "https://files.artsy.net/images/collections-color-category.png",
-  },
-  "Collect by Price": {
-    id: "Collect by Price",
-    title: "Price",
-    imageUrl: "https://files.artsy.net/images/collections-price-category.jpeg",
-  },
-  Gallery: {
-    id: "Gallery",
-    title: "Gallery",
-    imageUrl:
-      "https://files.artsy.net/images/collections-gallery-category.jpeg",
-  },
-}
+const orderedCategoryKeys = [
+  "Medium",
+  "Movement",
+  "Collect by Size",
+  "Collect by Color",
+  "Collect by Price",
+  "Gallery",
+]
 
 export const ExploreByCategory: HomeViewSection = {
   id: "home-view-section-explore-by-category",
@@ -49,16 +23,16 @@ export const ExploreByCategory: HomeViewSection = {
   },
   requiresAuthentication: false,
   resolver: (_parent, args, _context, _info) => {
-    const cards = Object.values(marketingColletionCategories).map(
-      (category) => {
-        return {
-          ...category,
-          entityID: category.id,
-          entityType: OwnerType.collectionsCategory,
-          image_url: category.imageUrl,
-        }
+    const cards = orderedCategoryKeys.map((key) => {
+      const category = marketingCollectionCategories[key]
+
+      return {
+        entityID: category.id,
+        entityType: OwnerType.collectionsCategory,
+        image_url: category.imageUrl,
+        title: category.title,
       }
-    )
+    })
 
     return connectionFromArray(cards, args)
   },
