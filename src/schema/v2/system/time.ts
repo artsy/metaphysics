223b407bcdc6452fc68ae4c41d83bca65f44144e
Joey Aghion <joey@aghion.com>
2024-11-19 17:50:19 -0500

    feat: resolve time queries immediately instead of depending on main API

--- src/schema/v2/system/time.ts
+++ src/schema/v2/system/time.ts
@@ -32,10 +32,26 @@ const SystemTimeType = new GraphQLObjectType<any, ResolverContext>({
 
 const SystemTime: GraphQLFieldConfig<any, ResolverContext> = {
   type: SystemTimeType,
-  description:
-    "Gravity system time, necessary for synchronizing device clocks.",
-  resolve: (_root, _options, { systemTimeLoader }) => {
-    return systemTimeLoader()
+  description: "Core system time, necessary for synchronizing device clocks.",
+  resolve: () => {
+    const now = new Date()
+    return {
+      day: now.getDate(),
+      wday: now.getDay(),
+      month: now.getMonth() + 1, // convert from 0-indexed to 1-indexed
+      year: now.getFullYear(),
+      hour: now.getHours(),
+      min: now.getMinutes(),
+      sec: now.getSeconds(),
+      dst: false, // TODO
+      unix: Math.floor(now.getTime() / 1000),
+      utcOffset: now.getTimezoneOffset() * -60,
+      zone: now
+        .toLocaleString("en", { timeZoneName: "short" })
+        .split(" ")
+        .pop(),
+      iso8601: now.toISOString(),
+    }
   },
 }
 
