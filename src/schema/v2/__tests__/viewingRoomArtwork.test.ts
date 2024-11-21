db22f5d624e7d7203836b92abfe3f82098236bc8
Nikita Skalkin <nikskalkin@gmail.com>
2024-11-21 09:57:44 +0100

    refactor(onyx-1405): add viewing room artworks to viewing room type (#6250)
    * refactor(onyx-1405): add viewing room artworks to viewing room type
    
    * remove artworks from test file

---
+++ src/schema/v2/__tests__/viewingRoomArtwork.test.ts
@@ -0,0 +1,71 @@
+import { runQuery } from "schema/v2/test/utils"
+import gql from "lib/gql"
+import config from "config"
+
+describe("ViewingRoomArtwork", () => {
+  beforeAll(() => {
+    config.USE_UNSTITCHED_VIEWING_ROOM_SCHEMA = true
+  })
+
+  afterAll(() => {
+    config.USE_UNSTITCHED_VIEWING_ROOM_SCHEMA = false
+  })
+
+  const query = gql`
+    {
+      viewingRoom(id: "example-viewing-room") {
+        viewingRoomArtworks {
+          __typename
+          artworkID
+          internalID
+          published
+        }
+      }
+    }
+  `
+
+  it("fetches viewing room artworks", async () => {
+    const viewingRoomArtworksData = [
+      {
+        id: "example-viewing-room-artwork",
+        artwork_id: "example-artwork",
+        published: true,
+      },
+      {
+        id: "example-viewing-room-artwork-2",
+        artwork_id: "example-artwork-2",
+        published: false,
+      },
+    ]
+
+    const context = {
+      viewingRoomLoader: jest.fn().mockResolvedValue({ id: "viewing-room-id" }),
+      viewingRoomArtworksLoader: jest
+        .fn()
+        .mockResolvedValue(viewingRoomArtworksData),
+    }
+
+    const result = await runQuery(query, context)
+
+    expect(result).toMatchInlineSnapshot(`
+      {
+        "viewingRoom": {
+          "viewingRoomArtworks": [
+            {
+              "__typename": "ViewingRoomArtwork",
+              "artworkID": "example-artwork",
+              "internalID": "example-viewing-room-artwork",
+              "published": true,
+            },
+            {
+              "__typename": "ViewingRoomArtwork",
+              "artworkID": "example-artwork-2",
+              "internalID": "example-viewing-room-artwork-2",
+              "published": false,
+            },
+          ],
+        },
+      }
+    `)
+  })
+})
