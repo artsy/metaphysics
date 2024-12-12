4f3a34602b474f3258d369fda77dbfdc8f16857f
Nikita Skalkin <nikskalkin@gmail.com>
2024-12-12 15:51:52 +0100

    delete tasteProfileVector from the schema

--- src/schema/v2/infiniteDiscovery/infiniteDiscovery.ts
+++ src/schema/v2/infiniteDiscovery/infiniteDiscovery.ts
@@ -1,14 +1,12 @@
 import { GraphQLFieldConfig, GraphQLNonNull, GraphQLObjectType } from "graphql"
 import { ResolverContext } from "types/graphql"
 import { InfiniteDiscoveryArtworksConnection } from "./infiniteDiscoveryArtworksConnection"
-import { TasteProfileVector } from "./tasteProfileVector"
 
 const InfiniteDiscoveryType = new GraphQLObjectType<any, ResolverContext>({
   name: "InfiniteDiscovery",
   description: "Schema for everything infinite discovery-related",
   fields: {
     artworksConnection: InfiniteDiscoveryArtworksConnection,
-    tasteProfileVector: TasteProfileVector,
   },
 })
 
