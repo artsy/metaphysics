34438b16f24c3c597c1a5b64f189d1c0f855467a
Nikita Skalkin <nikskalkin@gmail.com>
2025-01-27 15:11:06 +0100

    add missing test

--- src/schema/v2/infiniteDiscovery/__tests__/discoverArtworks.test.ts
+++ src/schema/v2/infiniteDiscovery/__tests__/discoverArtworks.test.ts
@@ -53,4 +53,62 @@ describe("discoverArtworks", () => {
       `)
     })
   })
+
+  describe("with arguments", () => {
+    it("should call the artworksDiscoveryLoader with the provided arguments", async () => {
+      const artworksDiscoveryLoader = jest.fn().mockResolvedValue({
+        artworks: [{ id: "artwork-1" }, { id: "artwork-2" }],
+      })
+
+      const context = { artworksDiscoveryLoader }
+
+      const query = gql`
+        {
+          discoverArtworks(
+            limit: 10
+            excludeArtworkIds: ["artwork-1"]
+            likedArtworkIds: ["artwork-2"]
+            mltFields: ["materials", "tags", "medium"]
+            osWeights: [0.5, 0.5]
+            curatedPicksSize: 3
+          ) {
+            edges {
+              node {
+                id
+              }
+            }
+          }
+        }
+      `
+
+      const result = await runQuery(query, context)
+      expect(artworksDiscoveryLoader).toHaveBeenCalledWith({
+        limit: 10,
+        exclude_artwork_ids: ["artwork-1"],
+        liked_artwork_ids: ["artwork-2"],
+        mlt_fields: ["materials", "tags", "medium"],
+        os_weights: [0.5, 0.5],
+        curated_picks_size: 3,
+      })
+
+      expect(result).toMatchInlineSnapshot(`
+        {
+          "discoverArtworks": {
+            "edges": [
+              {
+                "node": {
+                  "id": "QXJ0d29yazphcnR3b3JrLTE=",
+                },
+              },
+              {
+                "node": {
+                  "id": "QXJ0d29yazphcnR3b3JrLTI=",
+                },
+              },
+            ],
+          },
+        }
+      `)
+    })
+  })
 })
