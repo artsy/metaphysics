7f97fb50d46fdce189f34f96707e686401aa4c1b
Ole <ole.richter91@gmail.com>
2025-01-31 13:46:50 +0100

    feat: Add Featured Fairs to home view quick links

--- src/schema/v2/homeView/sections/QuickLinks.ts
+++ src/schema/v2/homeView/sections/QuickLinks.ts
@@ -1,6 +1,7 @@
 import { ContextModule, OwnerType } from "@artsy/cohesion"
 import { HomeViewSection } from "."
 import { HomeViewSectionTypeNames } from "../sectionTypes/names"
+import { FeaturedFairs } from "./FeaturedFairs"
 
 export const QuickLinks: HomeViewSection = {
   id: "home-view-section-quick-links",
@@ -19,6 +20,10 @@ export interface QuickLink {
   ownerType: OwnerType
 }
 
+export const getSectionURL = (section: HomeViewSection) => {
+  return `home-view/sections/${section.id}?sectionType=${section.type}`
+}
+
 const QUICK_LINKS: Array<QuickLink> = [
   { title: "Follows", href: "/favorites", ownerType: OwnerType.follows },
   { title: "Auctions", href: "/auctions", ownerType: OwnerType.auctions },
@@ -34,4 +39,9 @@ const QUICK_LINKS: Array<QuickLink> = [
     ownerType: OwnerType.priceDatabase,
   },
   { title: "Editorial", href: "/news", ownerType: OwnerType.articles },
+  {
+    title: "Featured Fairs",
+    href: getSectionURL(FeaturedFairs),
+    ownerType: OwnerType.fairs,
+  },
 ]
