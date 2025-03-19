ae51b8df2c1a4e6cff7fc469fb1524a8432c8b6d
Erik <9088720+erikdstock@users.noreply.github.com>
2025-03-19 13:44:02 -0500

    respond to feedback & cleanup

--- src/schema/v2/order/setOrderFulfillmentOptionMutation.ts
+++ src/schema/v2/order/setOrderFulfillmentOptionMutation.ts
@@ -14,23 +14,21 @@ interface Input {
 }
 
 // Similar to `FulfillmentOptionTypeEnum` but for input: Expects an all-caps
-// enum and converts back to exchange's lower-cased enum for the API
+// enum and converts back to exchange's lower-cased enum for the API.
+// Does not permit the SHIPPING_TBD placeholder option (but maybe should allow null?)
 const FulfillmentOptionTypeInputEnum = new GraphQLEnumType({
   name: "FulfillmentOptionInputEnum",
   values: {
     DOMESTIC_FLAT: { value: "DOMESTIC_FLAT" },
-    DOMESTIC_EXPEDITED: { value: "DOMESTIC_EXPEDITED" },
-    DOMESTIC_PRIORITY: { value: "DOMESTIC_PRIORITY" },
-    INTERNATIONAL_STANDARD: { value: "INTERNATIONAL_STANDARD" },
-    INTERNATIONAL_EXPEDITED: { value: "INTERNATIONAL_EXPEDITED" },
+    INTERNATIONAL_FLAT: { value: "INTERNATIONAL_FLAT" },
     PICKUP: { value: "PICKUP" },
-    SHIPPING_TBD: { value: "SHIPPING_TBD" },
   },
 })
 
 const FulfillmentOptionInputType = new GraphQLInputObjectType({
   name: "FulfillmentOptionInput",
   fields: {
+    // TODO: Maybe allow null to unset the selected option
     type: { type: new GraphQLNonNull(FulfillmentOptionTypeInputEnum) },
   },
 })
@@ -63,13 +61,7 @@ export const setOrderFulfillmentOptionMutation = mutationWithClientMutationId<
     try {
       const { fulfillmentOption } = input
 
-      let type: string
-      switch (fulfillmentOption.type) {
-        case "SHIPPING_TBD":
-          throw new Error("can't set placeholder type")
-        default:
-          type = fulfillmentOption.type.toLowerCase()
-      }
+      const type = fulfillmentOption.type.toLowerCase()
 
       const payload = {
         type,
