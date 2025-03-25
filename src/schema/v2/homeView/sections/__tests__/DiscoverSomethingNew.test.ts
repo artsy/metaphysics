3c16b4aa8b67fcaee7b58a4cde5b8bc658b7bdd7
egdbear <egdfer@gmail.com>
2025-03-25 16:26:21 +0100

    chore: remove home-view-marketing-collection-categories feature flag from the homeView
    Co-authored-by: Janae Edwards <edwards.janae@gmail.com>
    Co-authored-by: Janae Hijaz Edwards <edwards.janae@gmail.com>
    Co-authored-by: Janae Hijaz Edwards <janae.edwards@artsymail.com>

--- src/schema/v2/homeView/sections/__tests__/DiscoverSomethingNew.test.ts
+++ src/schema/v2/homeView/sections/__tests__/DiscoverSomethingNew.test.ts
@@ -1,51 +1,36 @@
-import { isFeatureFlagEnabled } from "lib/featureFlags"
 import gql from "lib/gql"
 import { runQuery } from "schema/v2/test/utils"
 
-jest.mock("lib/featureFlags", () => ({
-  isFeatureFlagEnabled: jest.fn(() => true),
-}))
-
-const mockIsFeatureFlagEnabled = isFeatureFlagEnabled as jest.Mock
-
 describe("DiscoverSomethingNew", () => {
-  describe("when the feature flag is enabled", () => {
-    beforeEach(() => {
-      mockIsFeatureFlagEnabled.mockImplementation((flag: string) => {
-        if (flag === "diamond_home-view-marketing-collection-categories")
-          return true
-      })
-    })
-
-    it("returns the section's metadata", async () => {
-      const query = gql`
-        {
-          homeView {
-            section(id: "home-view-section-discover-something-new") {
-              __typename
-              internalID
-              contextModule
-              ownerType
-              component {
-                title
-                behaviors {
-                  viewAll {
-                    buttonText
-                    href
-                    ownerType
-                  }
+  it("returns the section's metadata", async () => {
+    const query = gql`
+      {
+        homeView {
+          section(id: "home-view-section-discover-something-new") {
+            __typename
+            internalID
+            contextModule
+            ownerType
+            component {
+              title
+              behaviors {
+                viewAll {
+                  buttonText
+                  href
+                  ownerType
                 }
               }
             }
           }
         }
-      `
+      }
+    `
 
-      const context = {}
+    const context = {}
 
-      const { homeView } = await runQuery(query, context)
+    const { homeView } = await runQuery(query, context)
 
-      expect(homeView.section).toMatchInlineSnapshot(`
+    expect(homeView.section).toMatchInlineSnapshot(`
         {
           "__typename": "HomeViewSectionCards",
           "component": {
@@ -57,22 +42,21 @@ describe("DiscoverSomethingNew", () => {
           "ownerType": null,
         }
       `)
-    })
+  })
 
-    it("returns the section's connection data", async () => {
-      const query = gql`
-        {
-          homeView {
-            section(id: "home-view-section-discover-something-new") {
-              ... on HomeViewSectionCards {
-                cardsConnection(first: 6) {
-                  edges {
-                    node {
-                      entityID
-                      title
-                      image {
-                        url
-                      }
+  it("returns the section's connection data", async () => {
+    const query = gql`
+      {
+        homeView {
+          section(id: "home-view-section-discover-something-new") {
+            ... on HomeViewSectionCards {
+              cardsConnection(first: 6) {
+                edges {
+                  node {
+                    entityID
+                    title
+                    image {
+                      url
                     }
                   }
                 }
@@ -80,32 +64,33 @@ describe("DiscoverSomethingNew", () => {
             }
           }
         }
-      `
-
-      const context = {
-        marketingCollectionsLoader: jest.fn().mockResolvedValue({
-          body: [
-            {
-              slug: "figurative-art",
-              id: "figurative-art",
-              title: "Figurative Art",
-              subtitle: "Our Picks",
-              thumbnail: "figurative-art.jpg",
-            },
-            {
-              slug: "new-from-leading-galleries",
-              id: "new-from-leading-galleries",
-              title: "New from Leading Galleries",
-              subtitle: "Our Picks",
-              thumbnail: "new-from-leading-galleries.jpg",
-            },
-          ],
-        }),
       }
+    `
 
-      const { homeView } = await runQuery(query, context)
+    const context = {
+      marketingCollectionsLoader: jest.fn().mockResolvedValue({
+        body: [
+          {
+            slug: "figurative-art",
+            id: "figurative-art",
+            title: "Figurative Art",
+            subtitle: "Our Picks",
+            thumbnail: "figurative-art.jpg",
+          },
+          {
+            slug: "new-from-leading-galleries",
+            id: "new-from-leading-galleries",
+            title: "New from Leading Galleries",
+            subtitle: "Our Picks",
+            thumbnail: "new-from-leading-galleries.jpg",
+          },
+        ],
+      }),
+    }
 
-      expect(homeView.section).toMatchInlineSnapshot(`
+    const { homeView } = await runQuery(query, context)
+
+    expect(homeView.section).toMatchInlineSnapshot(`
         {
           "cardsConnection": {
             "edges": [
@@ -131,34 +116,5 @@ describe("DiscoverSomethingNew", () => {
           },
         }
       `)
-    })
-  })
-
-  describe("when the feature flag is disabled", () => {
-    beforeEach(() => {
-      mockIsFeatureFlagEnabled.mockImplementation((flag: string) => {
-        if (flag === "diamond_home-view-marketing-collection-categories")
-          return false
-      })
-    })
-
-    it("throws an error when accessed by id", async () => {
-      const query = gql`
-        {
-          homeView {
-            section(id: "home-view-section-discover-something-new") {
-              __typename
-              internalID
-            }
-          }
-        }
-      `
-
-      const context = {}
-
-      await expect(runQuery(query, context)).rejects.toThrow(
-        "Section is not displayable"
-      )
-    })
   })
 })
