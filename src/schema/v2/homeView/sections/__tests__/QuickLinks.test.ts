33f212e783777b4e926314938d5650d09982c89b
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2025-04-28 16:57:37 -0400

    feat: implement QuickLinks v2

--- src/schema/v2/homeView/sections/__tests__/QuickLinks.test.ts
+++ src/schema/v2/homeView/sections/__tests__/QuickLinks.test.ts
@@ -2,6 +2,9 @@ import gql from "lib/gql"
 import { runQuery } from "schema/v2/test/utils"
 import { QUICK_LINKS } from "../QuickLinks"
 import { OwnerType } from "@artsy/cohesion"
+import { isFeatureFlagEnabled } from "lib/featureFlags"
+
+const mockIsFeatureFlagEnabled = isFeatureFlagEnabled as jest.Mock
 
 describe("QuickLinks", () => {
   const query = gql`
@@ -29,67 +32,148 @@ describe("QuickLinks", () => {
     accessToken: "424242",
   }
 
-  it("returns the section's data", async () => {
-    const { homeView } = await runQuery(query, context)
+  describe("when v2 is enabled", () => {
+    beforeEach(() => {
+      mockIsFeatureFlagEnabled.mockReturnValue(true)
+    })
 
-    expect(homeView.section).toMatchInlineSnapshot(`
-      {
-        "__typename": "HomeViewSectionNavigationPills",
-        "contextModule": "quickLinks",
-        "internalID": "home-view-section-quick-links",
-        "navigationPills": [
-          {
-            "href": "/favorites/saves",
-            "icon": "HeartStrokeIcon",
-            "ownerType": "saves",
-            "title": "Saves",
-          },
-          {
-            "href": "/auctions",
-            "icon": "GavelIcon",
-            "ownerType": "auctions",
-            "title": "Auctions",
-          },
-          {
-            "href": "/collection/new-this-week",
-            "icon": null,
-            "ownerType": "collection",
-            "title": "New This Week",
-          },
-          {
-            "href": "/articles",
-            "icon": "PublicationIcon",
-            "ownerType": "articles",
-            "title": "Editorial",
-          },
-          {
-            "href": "/collection/statement-pieces",
-            "icon": null,
-            "ownerType": "collection",
-            "title": "Statement Pieces",
-          },
-          {
-            "href": "/collections-by-category/Medium?homeViewSectionId=home-view-section-explore-by-category&entityID=Medium",
-            "icon": "ArtworkIcon",
-            "ownerType": "collectionsCategory",
-            "title": "Medium",
-          },
-          {
-            "href": "/shows-for-you",
-            "icon": null,
-            "ownerType": "shows",
-            "title": "Shows for You",
-          },
-          {
-            "href": "/featured-fairs",
-            "icon": "FairIcon",
-            "ownerType": "featuredFairs",
-            "title": "Featured Fairs",
-          },
-        ],
-        "ownerType": "quickLinks",
-      }
-    `)
+    it("returns the section's data", async () => {
+      const { homeView } = await runQuery(query, context)
+
+      expect(homeView.section).toMatchInlineSnapshot(`
+        {
+          "__typename": "HomeViewSectionNavigationPills",
+          "contextModule": "quickLinks",
+          "internalID": "home-view-section-quick-links",
+          "navigationPills": [
+            {
+              "href": "/infinite-discovery",
+              "icon": "ImageSetIcon",
+              "ownerType": "infiniteDiscovery",
+              "title": "Discover Daily",
+            },
+            {
+              "href": "/auctions",
+              "icon": "GavelIcon",
+              "ownerType": "auctions",
+              "title": "Auctions",
+            },
+            {
+              "href": "/collection/new-this-week",
+              "icon": null,
+              "ownerType": "collection",
+              "title": "New This Week",
+            },
+            {
+              "href": "/articles",
+              "icon": "PublicationIcon",
+              "ownerType": "articles",
+              "title": "Articles",
+            },
+            {
+              "href": "/collection/statement-pieces",
+              "icon": null,
+              "ownerType": "collection",
+              "title": "Statement Pieces",
+            },
+            {
+              "href": "/collection/paintings",
+              "icon": "ArtworkIcon",
+              "ownerType": "collection",
+              "title": "Paintings",
+            },
+            {
+              "href": "galleries-for-you",
+              "icon": "InstitutionIcon",
+              "ownerType": "galleriesForYou",
+              "title": "Galleries for You",
+            },
+            {
+              "href": "/shows-for-you",
+              "icon": null,
+              "ownerType": "shows",
+              "title": "Shows for You",
+            },
+            {
+              "href": "/featured-fairs",
+              "icon": "FairIcon",
+              "ownerType": "featuredFairs",
+              "title": "Featured Fairs",
+            },
+          ],
+          "ownerType": "quickLinks",
+        }
+      `)
+    })
+  })
+
+  describe("when v2 is not enabled", () => {
+    beforeEach(() => {
+      mockIsFeatureFlagEnabled.mockReturnValue(false)
+    })
+
+    it("returns the section's data", async () => {
+      const { homeView } = await runQuery(query, context)
+
+      expect(homeView.section).toMatchInlineSnapshot(`
+        {
+          "__typename": "HomeViewSectionNavigationPills",
+          "contextModule": "quickLinks",
+          "internalID": "home-view-section-quick-links",
+          "navigationPills": [
+            {
+              "href": "/favorites/saves",
+              "icon": "HeartStrokeIcon",
+              "ownerType": "saves",
+              "title": "Saves",
+            },
+            {
+              "href": "/auctions",
+              "icon": "GavelIcon",
+              "ownerType": "auctions",
+              "title": "Auctions",
+            },
+            {
+              "href": "/collection/new-this-week",
+              "icon": null,
+              "ownerType": "collection",
+              "title": "New This Week",
+            },
+            {
+              "href": "/articles",
+              "icon": "PublicationIcon",
+              "ownerType": "articles",
+              "title": "Editorial",
+            },
+            {
+              "href": "/collection/statement-pieces",
+              "icon": null,
+              "ownerType": "collection",
+              "title": "Statement Pieces",
+            },
+            {
+              "href": "/collections-by-category/Medium?homeViewSectionId=home-view-section-explore-by-category&entityID=Medium",
+              "icon": "ArtworkIcon",
+              "ownerType": "collectionsCategory",
+              "title": "Medium",
+            },
+            {
+              "href": "/shows-for-you",
+              "icon": null,
+              "ownerType": "shows",
+              "title": "Shows for You",
+            },
+            {
+              "href": "/featured-fairs",
+              "icon": "FairIcon",
+              "ownerType": "featuredFairs",
+              "title": "Featured Fairs",
+            },
+          ],
+          "ownerType": "quickLinks",
+        }
+      `)
+    })
   })
 
   describe("When a quick link specifies a minimum Eigen version", () => {
