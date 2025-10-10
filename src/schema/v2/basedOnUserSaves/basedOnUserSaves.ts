f06bfe2a181eb8497eab76e7663fe69c9b6c99eb
Nikita Skalkin <nikskalkin@gmail.com>
2025-10-10 14:29:32 +0200

    feat(onyx-1950): basedOnUserSaves field

---
+++ src/schema/v2/basedOnUserSaves/basedOnUserSaves.ts
@@ -0,0 +1,58 @@
+import { pageable } from "relay-cursor-paging"
+import { artworkConnection } from "../artwork"
+import { GraphQLFieldConfig } from "graphql"
+import { ResolverContext } from "types/graphql"
+import { paginationResolver } from "../fields/pagination"
+import { map } from "lodash"
+import { convertConnectionArgsToGravityArgs } from "lib/helpers"
+
+const SAVED_ARTWORKS_SIZE = 3
+const SIMILAR_ARTWORKS_SIZE = 10
+
+export const BasedOnUserSaves: GraphQLFieldConfig<void, ResolverContext> = {
+  description: "A connection of artwork recommendations, based on user saves",
+  type: artworkConnection.connectionType,
+  args: pageable(),
+  resolve: async (
+    _parent,
+    args,
+    { savedArtworksLoader, userID, xImpersonateUserID, similarArtworksLoader },
+    _info
+  ) => {
+    const userId = userID || xImpersonateUserID
+    if (!savedArtworksLoader || !userId) return null
+
+    const gravityArgs = convertConnectionArgsToGravityArgs(args)
+    const { page, size, offset } = gravityArgs
+
+    const { body: works } = await savedArtworksLoader({
+      size: SAVED_ARTWORKS_SIZE,
+      sort: "-position",
+      user_id: userId,
+      private: true,
+    })
+
+    console.log("[Debug] A!", works)
+
+    if (works.length === 0) return null
+
+    const artworks = await similarArtworksLoader({
+      artwork_id: map(works, "_id"),
+      for_sale: true,
+      size: size || SIMILAR_ARTWORKS_SIZE,
+      offset,
+      total_count: true,
+    })
+
+    console.log("[Debug] B!")
+
+    return paginationResolver({
+      totalCount: artworks.length,
+      offset,
+      page,
+      size,
+      body: artworks,
+      args,
+    })
+  },
+}
