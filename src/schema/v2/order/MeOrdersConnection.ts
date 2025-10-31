7e5d7cf6a08384ffd5531f4f6f8568daf5d4641f
Erik <9088720+erikdstock@users.noreply.github.com>
2025-10-31 16:21:49 -0500

    cleanup and align with exchange code

--- src/schema/v2/order/MeOrdersConnection.ts
+++ src/schema/v2/order/MeOrdersConnection.ts
@@ -4,9 +4,9 @@ import {
 } from "schema/v2/fields/pagination"
 import { convertConnectionArgsToGravityArgs } from "lib/helpers"
 import { OrderType } from "./types/OrderType"
+import { OrderBuyerStateEnum } from "./types/sharedOrderTypes"
 import { pageable } from "relay-cursor-paging"
 import {
-  GraphQLEnumType,
   GraphQLFieldConfig,
   GraphQLInt,
   GraphQLList,
@@ -18,56 +18,6 @@ export const MeOrdersConnectionType = connectionWithCursorInfo({
   name: "MeOrders",
   nodeType: OrderType,
 }).connectionType
-
-export const MeOrderBuyerStateEnum = new GraphQLEnumType({
-  name: "MeOrderBuyerStateEnum",
-  values: {
-    INCOMPLETE: {
-      value: "INCOMPLETE",
-      description: "Order is incomplete (pending or abandoned)",
-    },
-    SUBMITTED: { value: "SUBMITTED", description: "Order has been submitted" },
-    APPROVED: { value: "APPROVED", description: "Order has been approved" },
-    OFFER_RECEIVED: {
-      value: "OFFER_RECEIVED",
-      description: "Order is an offer awaiting response from the buyer",
-    },
-    PAYMENT_FAILED: {
-      value: "PAYMENT_FAILED",
-      description: "Payment has failed",
-    },
-    PROCESSING_PAYMENT: {
-      value: "PROCESSING_PAYMENT",
-      description: "Payment is processing",
-    },
-    PROCESSING_OFFLINE_PAYMENT: {
-      value: "PROCESSING_OFFLINE_PAYMENT",
-      description: "Processing offline payment",
-    },
-    ACTION_REQUIRED: {
-      value: "ACTION_REQUIRED",
-      description: "Action required from buyer",
-    },
-    IN_TRANSIT: { value: "IN_TRANSIT", description: "Order is in transit" },
-    SHIPPED: { value: "SHIPPED", description: "Order has been shipped" },
-    PURCHASED: {
-      value: "PURCHASED",
-      description: "Order has been successfully purchased",
-    },
-    COMPLETED: { value: "COMPLETED", description: "Order is completed" },
-    REFUNDED: { value: "REFUNDED", description: "Order has been refunded" },
-    DECLINED_BY_SELLER: {
-      value: "DECLINED_BY_SELLER",
-      description: "Order was declined by the seller",
-    },
-    DECLINED_BY_BUYER: {
-      value: "DECLINED_BY_BUYER",
-      description: "Order was declined by the buyer",
-    },
-    CANCELLED: { value: "CANCELLED", description: "Order has been cancelled" },
-    CANCELED: { value: "CANCELED", description: "Order has been canceled" },
-  },
-})
 
 export const MeOrdersConnection: GraphQLFieldConfig<any, ResolverContext> = {
   type: MeOrdersConnectionType,
@@ -83,7 +33,7 @@ export const MeOrdersConnection: GraphQLFieldConfig<any, ResolverContext> = {
       description: "Filter by edition set ID in line items",
     },
     buyerState: {
-      type: new GraphQLList(MeOrderBuyerStateEnum),
+      type: new GraphQLList(OrderBuyerStateEnum),
       description: "Filter by buyer states",
     },
   }),
