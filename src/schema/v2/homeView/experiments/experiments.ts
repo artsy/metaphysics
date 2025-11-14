eeb3bf4a810c9fb6815957ad517a78f4a757b5ed
Devon Blandin <dblandin@gmail.com>
2025-11-14 18:34:13 +0000

    chore: remove onyx_nwfy-price-reranking-test from running experiments
    We closed this experiment by pinning our experiment variant (variant-a) to 100%
    on October 29th (Slack).
    
    [Slack]: https://artsy.slack.com/archives/C05EQL4R5N0/p1761742919813479?thread_ts=1761317195.198219&cid=C05EQL4R5N0

--- src/schema/v2/homeView/experiments/experiments.ts
+++ src/schema/v2/homeView/experiments/experiments.ts
@@ -4,6 +4,4 @@ import { FeatureFlag } from "lib/featureFlags"
  * Provide here the names of the Unleash experiment feature flags that
  * should be exposed as part of the current home view response
  */
-export const CURRENTLY_RUNNING_EXPERIMENTS: FeatureFlag[] = [
-  "onyx_nwfy-price-reranking-test",
-]
+export const CURRENTLY_RUNNING_EXPERIMENTS: FeatureFlag[] = []
