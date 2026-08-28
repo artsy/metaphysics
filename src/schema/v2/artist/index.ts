46b50ce74356ca99576f6c90d75e25bc06e41a16
Jackie Potts <jacqueline.a.potts@gmail.com>
2026-08-28 11:58:44 -0400

    Simplify interface, fetch article data from the Artist Node

--- src/schema/v2/artist/index.ts
+++ src/schema/v2/artist/index.ts
@@ -440,24 +440,23 @@ export const ArtistType = new GraphQLObjectType<any, ResolverContext>({
           }).then((articles) => first(articles.results)),
       },
       latestArticle: {
-        type: Article.type,
-        description:
-          "The most recent published article featuring this artist, optionally limited to those published since a given date.",
-        args: {
-          publishedSince: {
-            type: GraphQLString,
-            description:
-              "Only return articles published since this ISO 8601 date.",
+        type: new GraphQLObjectType<any, ResolverContext>({
+          name: "ArtistLatestArticle",
+          fields: {
+            href: { type: GraphQLString },
           },
+        }),
+        description:
+          "The most recent editorial article featuring this artist, published in the last 12 months. ",
+        resolve: ({ latest_article_href, latest_article_published_at }) => {
+          if (!latest_article_href || !latest_article_published_at) return null
+
+          const oneYearAgo = new Date()
+          oneYearAgo.setFullYear(oneYearAgo.getFullYear() - 1)
+          if (new Date(latest_article_published_at) < oneYearAgo) return null
+
+          return { href: latest_article_href }
         },
-        resolve: ({ _id }, { publishedSince }, { articlesLoader }) =>
-          articlesLoader({
-            published: true,
-            artist_id: _id,
-            sort: "-published_at",
-            limit: 1,
-            ...(publishedSince && { published_since: publishedSince }),
-          }).then((articles) => first(articles.results)),
       },
       biographyBlurb: {
         args: {
